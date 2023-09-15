8b3dcc8879e0f288922811c8607c79c136dd9b00
siwon <muncool39@gmail.com>
2023-09-15 20:42:51 +0900

    mun: boj_스택 수열

--- September/1주차/boj_스택 수열/Main.java
+++ September/1주차/boj_스택 수열/Main.java
@@ -1,4 +1,28 @@
-
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder("");
+		int count = 1;
+		for(int i=0;i<n;i++){
+		    int num = Integer.parseInt(br.readLine());
+            if(num>=count){
+                while(count<=num){
+                    stack.add(count++);
+                    sb.append("+").append("\n");
+                }
+            }
+            if(stack.peek()!=num){
+                sb.setLength(0);
+                sb.append("NO");
+                break;
+            }
+            stack.pop();
+            sb.append("-").append("\n");
+		}
+		System.out.print(sb);
+	}
 }
