210b82ac0cf9aad58455509c180be7d4bfcf0c7a
siwon <muncool39@gmail.com>
2023-09-16 23:14:10 +0900

    mun: prgrms_자릿수 더하기

--- September/1주차/prgrms_자릿수 더하기/Solution.java
+++ September/1주차/prgrms_자릿수 더하기/Solution.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        while(n>0){
+            answer+=n%10;
+            n/=10;
+        }
         return answer;
     }
 }
