f0f5622933b387a5f29dcfd12842016b43149583
SeoYeon Min <tjdus553926@gmail.com>
2023-09-17 00:05:11 +0900

    min: prgrms_자릿수 더하기

--- September/1주차/prgrms_자릿수 더하기/Solution.java
+++ September/1주차/prgrms_자릿수 더하기/Solution.java
@@ -1,12 +1,15 @@
-import java.util.*;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+int solution(int n) {
+    int answer = 0;
+    
+    while(n!=0){
+        answer += n%10;
+        n = n / 10;
     }
+    
+  
+    return answer;
 }
