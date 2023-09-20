ee6b398ad5fac1ae2ed5a6d5a09ec96de54f6341
ChaeAg <dydkglel123@gmail.com>
2023-09-20 15:04:45 +0900

    lee: boj_스택 수열

--- September/1주차/boj_스택 수열/Main.java
+++ September/1주차/boj_스택 수열/Main.java
@@ -1,4 +1,48 @@
+import java.util.*;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int data = 1, popcount = 0, num;
+
+        Stack<Integer> stack = new Stack<>(); //int형 스택 선언
+        List<String> result = new ArrayList<>();
+
+        while(popcount != n) {
+            num = sc.nextInt();
+            if(!stack.empty()){
+                if(stack.peek() < num){
+                    while(stack.peek() != num){
+                        stack.push(data);
+                        data++;
+                        result.add("+");
+                    }
+                }
+                else if(stack.peek() > num){
+                    System.out.println("NO");
+                    System.exit(0);
+                }
+            }
+            else{
+                if(data <= num) {
+                    while(data <= num){
+                        stack.push(data);
+                        data++;
+                        result.add("+");
+                    }
+                }
+                else {
+                    System.out.println("NO");
+                    System.exit(0);
+                }
+            }
+            if(stack.peek() == num){
+                stack.pop();
+                result.add("-");
+                popcount++;
+            }
+        }
+        for(String s:result) System.out.println(s);
+    }
 }
