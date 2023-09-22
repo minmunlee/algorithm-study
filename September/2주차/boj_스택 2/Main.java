886476c5be47cc19fc030438dd43cb4e032411bb
siwon <muncool39@gmail.com>
2023-09-22 23:14:10 +0900

    mun: 2주차 문제 풀이

--- September/2주차/boj_스택 2/Main.java
+++ September/2주차/boj_스택 2/Main.java
@@ -1,3 +1,33 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder("");
+		for(int i=0;i<N;i++){
+		    StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		    int message = -2;
+		    switch(st.nextToken()) {
+                case "1":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "2":
+                    message = (!stack.isEmpty())?stack.pop():-1;
+                    break;
+                case "3":
+                    message = stack.size();
+                    break;
+                case "4":
+                    message = (stack.isEmpty())?1:0;
+                    break;
+                case "5":
+                    message = (!stack.isEmpty())?stack.peek():-1;
+                    break;
+            }
+            if(message>-2) sb.append(message).append("\n");
+		}
+		System.out.print(sb);
+	}
 }
--- September/2주차/boj_천재 수학자 성필/Main.java
+++ September/2주차/boj_천재 수학자 성필/Main.java
@@ -1,3 +1,29 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] susick = br.readLine().split("");
+		String cal = "+-*/";
+		Stack<Long> stack = new Stack<>();
+		for(String s : susick){
+		    if(cal.contains(s)){
+		        long a = stack.pop();
+		        long b = stack.pop();
+		        switch(s){
+		            case "+": stack.add(b+a);
+		            break;
+		            case "-": stack.add(b-a);
+		            break;
+		            case "*": stack.add(b*a);
+		            break;
+		            case "/": stack.add(b/a);
+		            break;
+		        }
+		    }else{
+		        stack.add(Long.parseLong(s));
+		    }
+		}
+		System.out.print(stack.pop());
+	}
 }
