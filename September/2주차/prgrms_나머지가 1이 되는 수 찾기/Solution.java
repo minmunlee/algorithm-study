310249d128ef5e46e06dbdde4d8cda45496160d9
SeoYeon Min <tjdus553926@gmail.com>
2023-09-25 01:21:40 +0900

    min: 2주차 문제 풀이
    - prgrms_나머지가 1이 되는 수 찾기

--- September/2주차/prgrms_나머지가 1이 되는 수 찾기/Solution.java
+++ September/2주차/prgrms_나머지가 1이 되는 수 찾기/Solution.java
@@ -1,3 +1,13 @@
-public class Solution {
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
     
+        for(int i = 1; i<n; i++){
+            if(n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+    return answer;
+    }
 }
