77e3eddc635155b31df1c641ba86c858fc4a17af
SeoYeon Min <tjdus553926@gmail.com>
2023-09-28 21:10:01 +0900

    min: 2주차 문제 풀이
    prgrms_자연수 뒤집어 배열로 만들기

--- September/2주차/prgrms_자연수 뒤집어 배열로 만들기/Solution.java
+++ September/2주차/prgrms_자연수 뒤집어 배열로 만들기/Solution.java
@@ -1,3 +1,24 @@
-public class Solution {
-    
+class Solution {
+    public int[] solution(long n) {
+        
+        String sArray = Long.toString(n); //  long 타입 변수 String 타입으로 변환
+        int[] arrNum = new int[sArray.length()]; // String 타입 배열 데이터 저장하는 정수형 배열
+        int[] answer = new int[sArray.length()]; // 뒤집힌 데이터 출력하는 배열
+        int len = arrNum.length - 1; // 맨 마지막 값
+        
+        
+        for(int i = 0; i < sArray.length(); i++){
+            arrNum[i] = sArray.charAt(i) - '0'; // String 타입 문자열을 한글자만 선택해서 char타입으로 변환
+        }
+        
+        while(len != -1){
+            for(int i = 0; i < arrNum.length; i++){
+                answer[i] =  arrNum[len]; // 맨 마지막 값 넣기
+                len--; // pop
+            }
+        }
+        
+        
+        return answer;
+    }
 }
