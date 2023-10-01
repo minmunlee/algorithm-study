6d7baa1c0d48e3f82a4d902f278e5d7f43b52634
SeoYeon Min <tjdus553926@gmail.com>
2023-10-02 00:39:08 +0900

    3주차 문제 풀이
    - prgrms_문자열 내 p와 y의 개수

--- September/3주차/prgrms_문자열 내 p와 y의 개수/Solution.java
+++ September/3주차/prgrms_문자열 내 p와 y의 개수/Solution.java
@@ -1,3 +1,26 @@
-public class Solution {
-    
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int pAc = 0;
+        int yAc = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P'){
+                pAc += 1;
+            }else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y'){
+                yAc += 1;
+            }
+        }
+        
+        if(pAc != yAc){
+            answer = false;
+        }else{
+            answer = true;
+        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println(answer);
+
+        return answer;
+    }
 }
