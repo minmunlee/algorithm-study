faaa3e6d34294f6bfa99f423bc388a0b95ef00e6
SeoYeon Min <tjdus553926@gmail.com>
2023-10-03 01:33:15 +0900

    min: 3주차 문제 풀이
    - prgrms_정수 제곱근 판별
    - boj_요세푸스 문제

--- September/3주차/boj_요세푸스 문제/Main.java
+++ September/3주차/boj_요세푸스 문제/Main.java
@@ -1,3 +1,37 @@
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt(); // 입력 받을 수의 개수
+        int n2 = sc.nextInt(); // 몇 번째 사람인지
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        int count = 1;
+
+        for(int i = 1; i <= n1; i++){ // 수 입력 받기
+            queue.offer(i);
+        }
+
+        while(!queue.isEmpty()){
+            if(count != n2){ // n2 번째가 나올 때 까지 돌림
+                queue.offer(queue.poll()); 
+                count++;
+            }else{
+                list.add(queue.poll()); // 뺄 값 list 에 넣음
+                count = 1;
+            }
+        }
+
+        System.out.print("<");
+        for(int i = 0; i < list.size(); i++){
+            if(i == list.size() - 1){
+                System.out.print(list.get(i) + ">");
+            }else{
+                System.out.print(list.get(i) + ",");
+            }
+        }
+        
+        sc.close();
+    }
 }
--- September/3주차/prgrms_정수 제곱근 판별/Solution.java
+++ September/3주차/prgrms_정수 제곱근 판별/Solution.java
@@ -1,3 +1,13 @@
-public class Solution {
-    
+import java.lang.Math.*;
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double x = Math.sqrt(n);
+        if(x % 1 == 0.0){
+            answer = new Double(Math.pow(x + 1, 2)).longValue();
+        }else{
+            answer = -1;
+        }
+        return answer;
+    }
 }
