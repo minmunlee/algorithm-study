b72379ca0b1dedf3b24dcd3d161d69faa57bfbf2
siwon <muncool39@gmail.com>
2023-10-04 20:46:11 +0900

    solve: boj_요세푸스 문제

--- September/3주차/boj_요세푸스 문제/Main.java
+++ September/3주차/boj_요세푸스 문제/Main.java
@@ -1,3 +1,23 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int i=1;i<=N;i++){
+		    arr.add(i);
+		}
+		StringBuilder sb = new StringBuilder("<");
+		int del = K-1;
+		for(int i=0;i<N-1;i++){
+		    sb.append(arr.remove(del)).append(", ");
+		    del+=K-1;
+		    del%=arr.size();
+		}
+		sb.append(arr.get(0)).append(">");
+		System.out.print(sb);
+	}
 }
