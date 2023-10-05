c6b74fffaeb2ecf9fb83f7eaa66031abeeae7dc3
siwon <muncool39@gmail.com>
2023-10-05 16:59:12 +0900

    solve: 3_prgrms_문자열 내 p와 y의 개수

--- September/3주차/prgrms_문자열 내 p와 y의 개수/Solution.java
+++ September/3주차/prgrms_문자열 내 p와 y의 개수/Solution.java
@@ -1,3 +1,8 @@
-public class Solution {
-    
+class Solution {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        int p = s.length()-s.replace("p","").length();
+        int y = s.length()-s.replace("y","").length();
+        return p==y;
+    }
 }
