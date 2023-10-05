3abba98f5476998160922e458d93ef8be6929c05
siwon <muncool39@gmail.com>
2023-10-05 16:58:44 +0900

    solve: 2_prgms_자연수 뒤집어 배열로 만들기

--- September/2주차/prgrms_자연수 뒤집어 배열로 만들기/Solution.java
+++ September/2주차/prgrms_자연수 뒤집어 배열로 만들기/Solution.java
@@ -1,3 +1,11 @@
-public class Solution {
-    
+import java.util.*;
+class Solution {
+    public ArrayList solution(long n) {
+        ArrayList<Long> arr = new ArrayList<>();
+        while(n>0){
+            arr.add(n%10);
+            n=n/10;
+        }
+        return arr;
+    }
 }
