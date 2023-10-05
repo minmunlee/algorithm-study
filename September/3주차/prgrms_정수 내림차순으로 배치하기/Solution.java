3fe85b535b61d2a3b934efc124008f901c0cddbc
siwon <muncool39@gmail.com>
2023-10-05 16:59:35 +0900

    solve: 3_prgrms_정수 내림차순으로 배치하기

--- September/3주차/prgrms_정수 내림차순으로 배치하기/Solution.java
+++ September/3주차/prgrms_정수 내림차순으로 배치하기/Solution.java
@@ -1,3 +1,12 @@
-public class Solution {
-    
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        String[] nums = String.valueOf(n).split("");
+        Arrays.sort(nums,Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder("");
+        for(String num : nums){
+            sb.append(num);
+        }
+        return Long.parseLong(sb.toString());
+    }
 }
