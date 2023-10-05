6c93f8d6096d0b15f9db52a99072b185e20aafb1
siwon <muncool39@gmail.com>
2023-10-05 16:54:10 +0900

    solve: 2_prgms_나머지가 1이 되는 수 찾기

--- September/2주차/prgrms_나머지가 1이 되는 수 찾기/Solution.java
+++ September/2주차/prgrms_나머지가 1이 되는 수 찾기/Solution.java
@@ -1,3 +1,12 @@
-public class Solution {
-    
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2;i<n;i++){
+            if(n%i==1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
 }
