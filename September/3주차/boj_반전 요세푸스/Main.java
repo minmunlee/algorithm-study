a35786d5d12fbdb90e35bfd68a32d499ed8952d2
SeoYeon Min <tjdus553926@gmail.com>
2023-10-05 18:31:01 +0900

    min: 3주차 문제 풀이
    - prgrms_정수 내림차순으로 배치하기
    - boj_반전 요세푸스

--- September/3주차/boj_반전 요세푸스/Main.java
+++ September/3주차/boj_반전 요세푸스/Main.java
@@ -1,3 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(str.nextToken()); // 입력 받을 데이터 개수
+        int K = Integer.parseInt(str.nextToken()); // 제거할 데이터 순서
+        int M = Integer.parseInt(str.nextToken()); // 반전되는 주기
+        int count = 1;
+        int num = 0;
+
+       Deque<Integer> deque = new ArrayDeque<>();
+
+       for(int i = 1; i <= N; i++){
+            deque.add(i);
+       }
+
+       while(!deque.isEmpty()){
+            if(count != K){
+                deque.add(deque.poll());
+                count++;
+            }else{
+                count = 1;
+                System.out.println(deque.poll());
+                num++;  
+            }
+            if(num == M){
+                if(count != K){
+                     deque.addLast(deque.pollLast());
+                     count++;
+                }else{
+                    count = 1;
+                    System.out.println(deque.pollLast());
+                }
+            }
+       }
+    }
 }
--- September/3주차/prgrms_정수 내림차순으로 배치하기/Solution.java
+++ September/3주차/prgrms_정수 내림차순으로 배치하기/Solution.java
@@ -1,3 +1,25 @@
-public class Solution {
-    
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        List<Long> longList = new ArrayList<Long>();
+        StringBuilder stb = new StringBuilder();
+        
+        while (n>0){
+            longList.add(n%10);
+            n /= 10;
+        }
+        
+        longList.sort(Comparator.reverseOrder());
+
+
+        for(Long i : longList){
+            stb.append(i);
+        }
+
+        answer = Long.parseLong(stb.toString());
+        
+        return answer;
+    }
 }
