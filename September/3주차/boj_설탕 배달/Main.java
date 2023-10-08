930ae46cc4fbe7bfd5826a0f4bbea86876a9482b
SeoYeon Min <tjdus553926@gmail.com>
2023-10-08 23:30:06 +0900

    solve: 3_boj_설탕 배달

--- September/3주차/boj_설탕 배달/Main.java
+++ September/3주차/boj_설탕 배달/Main.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 배달 할 설탕의 양
+        int count = 0; // 필요한 최소한의 봉지
+
+        while(true){
+            if(n % 5 == 0){ // 5의 배수인 경우
+                System.out.println(count += n/5);
+                break;
+            }else if(n < 0){ // 3, 5의 배수 둘 다 아닌 경우
+                System.out.println(-1);
+                break;
+            }
+            // 3의 배수인 경우
+            n -= 3;
+            count += 1;
+            
+        }
+
+        sc.close();
+
+    }
 }
