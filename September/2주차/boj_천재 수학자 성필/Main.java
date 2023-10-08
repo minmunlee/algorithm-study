639077924a9dc33c47300d63b32416f9a265ef44
이채은 <dydkglel123@gmail.com>
2023-10-08 19:18:00 +0900

    solve: 2_boj_천재 수학자 성필

--- September/2주차/boj_천재 수학자 성필/Main.java
+++ September/2주차/boj_천재 수학자 성필/Main.java
@@ -1,3 +1,33 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        char[] array = br.readLine().toCharArray();
+        
+        for(char c: array){
+            if(c < 48 || c > 58) { // 숫자가 아니면
+                int b = stack.pop();
+                int a = stack.pop();
+                switch(c) {
+                    case '+':
+                        stack.push(a+b);
+                        break;
+                    case '-':
+                        stack.push(a-b);
+                        break;
+                    case '*':
+                        stack.push(a*b);
+                        break;
+                    case '/':
+                        stack.push(a/b);
+                        break;
+                }
+            }
+            else stack.push(c - '0'); // 숫자가 맞으면
+        }
+        System.out.println(stack.pop());
+    }
 }
