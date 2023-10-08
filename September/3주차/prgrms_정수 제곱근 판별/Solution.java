03e1bf5f69bdd6b30e69a06b806dfebd9c6da445
이채은 <dydkglel123@gmail.com>
2023-10-08 19:19:30 +0900

    solve: 3_prgrms_정수 제곱근 판별

--- September/3주차/prgrms_정수 제곱근 판별/Solution.java
+++ September/3주차/prgrms_정수 제곱근 판별/Solution.java
@@ -1,3 +1,8 @@
-public class Solution {
-    
+class Solution {
+    public long solution(long n) {
+        double num = Math.sqrt((double)n);
+        
+        if(num % 1 == 0.0) return (long)Math.pow(++num, 2);
+        else return -1;
+    }
 }
