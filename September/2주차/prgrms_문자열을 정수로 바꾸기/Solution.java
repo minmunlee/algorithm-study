f066aa4a081acdb2db93bc9cc819ea5d75e10734
이채은 <dydkglel123@gmail.com>
2023-10-08 19:19:02 +0900

    solve: 2_prgrms_문자열을 정수로 바꾸기

--- September/2주차/prgrms_문자열을 정수로 바꾸기/Solution.java
+++ September/2주차/prgrms_문자열을 정수로 바꾸기/Solution.java
@@ -1,3 +1,18 @@
-public class Solution {
-    
+class Solution {
+    public int solution(String s) {
+        int answer;
+        
+        switch(s.charAt(0)){
+            case '+':
+                answer = Integer.valueOf(s.substring(1));
+                break;
+            case '-':
+                answer = -(Integer.valueOf(s.substring(1)));
+                break;
+            default:
+                answer = Integer.valueOf(s);
+        }
+        
+        return answer;
+    }
 }
