7709dd464fc0cc96c8f2357d85418978dc14ad4f
이채은 <dydkglel123@gmail.com>
2023-10-10 19:57:07 +0900

    solve: 3_boj_설탕 배달

--- September/3주차/boj_설탕 배달/Main.java
+++ September/3주차/boj_설탕 배달/Main.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int kg = 0, count = 0, three_count = 0;
+
+		while(kg <= N) {
+			if((N - kg) % 5 == 0) {
+				count += (N - kg) / 5 + three_count;
+				break;
+			}
+			kg += 3;
+			three_count++;
+		}
+		if(count == 0) System.out.println("-1");
+		else System.out.println(count);
+	}
 }
