4dec3d42af15c74715a0874aac5909fe94723659
siwon <muncool39@gmail.com>
2023-10-19 03:03:14 +0900

    solve: prgrms_콜라츠 추측

--- October/1주차/prgrms_콜라츠 추측/Solution.java
+++ October/1주차/prgrms_콜라츠 추측/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        return answer;
+        int count = 1;
+        if(num==1) return 0;
+        long n = num;
+        while(count<=500){
+            if(n%2==0) n/=2;
+            else n = (n*3)+1;
+            if(n==1) break;
+            count++;
+        }
+        return (count>500)?-1:count;
     }
 }
