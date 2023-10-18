e3c34d77493ceb105b90250c40616ee4a380a871
SeoYeon Min <tjdus553926@gmail.com>
2023-10-19 02:49:43 +0900

    solve: 비밀번호 찾기

--- October/1주차/boj_비밀번호 찾기/Main.java
+++ October/1주차/boj_비밀번호 찾기/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(str.nextToken()); // 저장된 사이트 주소의 수
+        int M = Integer.parseInt(str.nextToken()); // 비밀번호를 찾으려는 사이트 주소의 수
+        HashMap<String, String> hm = new HashMap<String, String>();
+
+        for(int i = 0; i < N; i++){
+            str = new StringTokenizer(br.readLine()); // 값을 입력 받을 변수
+            hm.put(str.nextToken(), str.nextToken()); // 사이트 비밀번호 정보 저장
+        }
+
+        for(int i = 0; i < M; i++){
+            sb.append(hm.get(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
 }
