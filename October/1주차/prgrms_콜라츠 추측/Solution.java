ce441a79e766301e6393a6add7a32469dae7c6fb
ChaeAg <dydkglel123@gmail.com>
2023-10-18 17:47:48 +0900

    solve: prgrms_콜라츠 추측

--- October/1주차/prgrms_콜라츠 추측/Solution.java
+++ October/1주차/prgrms_콜라츠 추측/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        return answer;
+        int count = 0;      
+        Long n = Long.valueOf(num);
+        
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n = n * 3 + 1;
+            }
+            count++;
+            if(count > 500) return -1;
+        }
+        
+        return count;
     }
 }
