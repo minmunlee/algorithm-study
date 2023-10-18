0846669e31818c6c9e74572d7fd20ec5d39bb761
siwon <muncool39@gmail.com>
2023-10-19 03:07:46 +0900

    solve: prgrms_서울에서 김서방 찾기

--- October/1주차/prgrms_서울에서 김서방 찾기/Solution.java
+++ October/1주차/prgrms_서울에서 김서방 찾기/Solution.java
@@ -1,6 +1,9 @@
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals("Kim")) return String.format("김서방은 %d에 있다",i);
+        }
         return answer;
     }
 }
