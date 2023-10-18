f7d5cc39f7b03a00903c15368c19ab90c9c8a111
SeoYeon Min <tjdus553926@gmail.com>
2023-10-19 01:33:07 +0900

    solve: prgrms_콜라츠 추측

--- October/1주차/prgrms_콜라츠 추측/Solution.java
+++ October/1주차/prgrms_콜라츠 추측/Solution.java
@@ -1,6 +1,20 @@
 class Solution {
-    public int solution(int num) {
+    public int solution(long num) {
         int answer = 0;
+        while(num != 1){
+            if(num % 2 == 0){
+                num /= 2;
+                answer++;
+            }else{
+                num = num * 3 + 1;
+                answer++;
+            }
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
