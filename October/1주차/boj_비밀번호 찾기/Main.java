82576f7271b9a4c3193084644f17a037e2c8a24a
ChaeAg <dydkglel123@gmail.com>
2023-10-18 17:47:10 +0900

    solve: boj_비밀번호 찾기

--- October/1주차/boj_비밀번호 찾기/Main.java
+++ October/1주차/boj_비밀번호 찾기/Main.java
@@ -1,3 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		String str;
+		while((str = br.readLine()) != null) {
+			String [] a = str.split(" ");
+			if(a.length == 2) {
+				map.put(a[0], a[1]);
+			}
+			else {
+				sb.append(map.get(a[0])).append("\n");
+			}
+		}
+
+		System.out.print(sb);
+	}
 }
