8a2334ff1d94456ae959b813f88ad00bec98e3de
siwon <muncool39@gmail.com>
2023-10-19 16:32:51 +0900

    solve: prgrms_두 정수 사이의 합

--- October/1주차/prgrms_두 정수 사이의 합/Solution.java
+++ October/1주차/prgrms_두 정수 사이의 합/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
     public long solution(int a, int b) {
         long answer = 0;
+        int front = (a<b)?a:b;
+        int back = (front==a)?b:a;
+        for(int i=front;i<=back;i++){
+            answer+=i;
+        }
         return answer;
     }
 }
