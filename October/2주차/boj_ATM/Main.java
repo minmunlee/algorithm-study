84b794e4c6933b28c1e272543d621bf6954bfd0b
gitseoyeon <tjdus553926@gmail.com>
2023-10-22 01:50:48 +0900

    min: 10월 2주차 문제 풀이
    solve: boj_ATM

--- October/2주차/boj_ATM/Main.java
+++ October/2주차/boj_ATM/Main.java
@@ -1,3 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 입력할 데이터 개수
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        List<Integer> list  = new ArrayList<Integer>();
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            list.add(Integer.parseInt(str.nextToken()));
+        }
+
+        Collections.sort(list); // 오름차순으로 정렬
+
+        for(int i = 0; i<list.size(); i++){
+            sum += list.get(i);
+            if(i != 0){
+                for(int j = i-1; j >= 0; j--){
+                    sum += list.get(j);
+                }
+            }
+        }
+
+        System.out.println(sum);
+
+    }
 }
