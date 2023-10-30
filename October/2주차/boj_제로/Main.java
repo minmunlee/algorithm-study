6a2cebcc1f9ebeb4e6cbe1f62def0ec1dc543683
ChaeAg <dydkglel123@gmail.com>
2023-10-31 02:24:51 +0900

    solve: boj_제로

--- October/2주차/boj_제로/Main.java
+++ October/2주차/boj_제로/Main.java
@@ -1,5 +1,24 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> numbers = new ArrayList<>();
+
+		for(int i=0; i<N; i++) {
+			int number = Integer.parseInt(br.readLine());
+			if(number == 0) {
+				numbers.remove(numbers.size()-1);
+				continue;
+			}
+			numbers.add(number);
+		}
+
+		System.out.print(numbers.stream().mapToInt(Integer::intValue).sum());
+	}
 }
