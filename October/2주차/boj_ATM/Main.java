7ab177c735102d8beba639d2f921d79a92c160ac
siwon <muncool39@gmail.com>
2023-11-02 17:07:21 +0900

    solve: boj_ATM

--- October/2주차/boj_ATM/Main.java
+++ October/2주차/boj_ATM/Main.java
@@ -1,3 +1,20 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = Arrays.stream(br.readLine().split(" "))
+		            .mapToInt(Integer::parseInt).sorted().toArray();
+		int temp = 0;
+		for(int i=0;i<arr.length;i++){
+		    temp=temp+arr[i];
+		    arr[i]=temp;
+		}
+		int sum = 0;
+		for(int p : arr){
+		    sum+=p;
+		}
+		System.out.print(sum);
+	}
 }
