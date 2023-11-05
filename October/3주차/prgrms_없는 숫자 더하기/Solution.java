f5f00a2801101e6a8aaab5a53ce6411b7bd3bb57
gitseoyeon <tjdus553926@gmail.com>
2023-11-05 22:48:56 +0900

    solve: prgrms_없는 숫자 더하기

--- October/3주차/prgrms_없는 숫자 더하기/Solution.java
+++ October/3주차/prgrms_없는 숫자 더하기/Solution.java
@@ -1,3 +1,12 @@
-public class Solution {
-    
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        
+        answer = 45 - sum;
+        return answer;
+    }
 }
