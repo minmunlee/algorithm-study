bd85cdfd582e3ce427e24d03f3b05354746efdce
gitseoyeon <tjdus553926@gmail.com>
2023-11-05 22:49:23 +0900

    solve: prgrms_나누어 떨어지는 배열

--- October/3주차/prgrms_나누어 떨어지는 배열/Solution.java
+++ October/3주차/prgrms_나누어 떨어지는 배열/Solution.java
@@ -1,3 +1,29 @@
-public class Solution {
-    
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            }
+        }
+        
+        if(list.size() == 0){
+            answer = new int[1];
+            answer[0] = -1;
+        }else{
+            Collections.sort(list);
+            answer = new int[list.size()];
+            
+            for(int i = 0; i < list.size(); i++){
+                answer[i] = list.get(i);
+            }
+            
+        }
+
+        return answer;
+    }
 }
