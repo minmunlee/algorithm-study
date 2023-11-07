c610b1992014b1dff8304add76e9aac644766436
ChaeAg <dydkglel123@gmail.com>
2023-11-08 04:17:17 +0900

    solve: prgrms_없는 숫자 더하기

--- October/3주차/prgrms_없는 숫자 더하기/Solution.java
+++ October/3주차/prgrms_없는 숫자 더하기/Solution.java
@@ -1,3 +1,12 @@
-public class Solution {
-    
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+        for(int num : numbers) {
+            if(list.contains(num)) {
+				list.remove(list.indexOf(num));
+			}
+        }
+        return list.stream().mapToInt(Integer::intValue).sum();
+    }
 }
