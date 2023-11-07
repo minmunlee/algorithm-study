1f20de1e6bce87c4bb2b368fef82bc18c5d607b2
ChaeAg <dydkglel123@gmail.com>
2023-11-08 04:17:26 +0900

    solve: prgrms_나누어 떨어지는 배열

--- October/3주차/prgrms_나누어 떨어지는 배열/Solution.java
+++ October/3주차/prgrms_나누어 떨어지는 배열/Solution.java
@@ -1,3 +1,14 @@
-public class Solution {
-    
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public List<Integer> solution(int[] arr, int divisor) {
+        List<Integer> list = Arrays.stream(arr)
+                                .filter(i -> i % divisor == 0)
+                                .sorted()
+                                .boxed()
+                                .collect(Collectors.toList());
+
+        return list.isEmpty() ? Collections.singletonList(-1) : list;
+    }
 }
