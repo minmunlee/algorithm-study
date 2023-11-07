45bce79a3ff49e38e2d6288cd52d178159caaa12
ChaeAg <dydkglel123@gmail.com>
2023-11-08 03:35:50 +0900

    solve: boj_단어 나누기

--- October/3주차/boj_단어 나누기/Main.java
+++ October/3주차/boj_단어 나누기/Main.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<String> resultList = new ArrayList<>();
+		String input = br.readLine();
+		for(int i=1; i<input.length()-1; i++) {
+			for(int k=i+1; k<input.length(); k++) {
+				StringBuffer sb1 = new StringBuffer(input.substring(0, i));
+				StringBuffer sb2 = new StringBuffer(input.substring(i, k));
+				StringBuffer sb3 = new StringBuffer(input.substring(k));
+				resultList.add(sb1.reverse().toString() + sb2.reverse() + sb3.reverse());
+			}
+		}
+		Collections.sort(resultList);
+        System.out.println(resultList.get(0));
+	}
 }
