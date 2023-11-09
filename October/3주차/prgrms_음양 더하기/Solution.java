6e0b9409de8e4cb3ab365006a714306937867017
siwon <muncool39@gmail.com>
2023-11-09 13:14:11 +0900

    solve: prgrms_음양 더하기

--- October/3주차/prgrms_음양 더하기/Solution.java
+++ October/3주차/prgrms_음양 더하기/Solution.java
@@ -1,3 +1,10 @@
-public class Solution {
-    
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<absolutes.length;i++){
+            int number = (signs[i])?absolutes[i]:absolutes[i]*-1;
+            answer+=number;
+        }
+        return answer;
+    }
 }
