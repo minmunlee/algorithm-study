988f451817bff0707656c3fe38224dac2aebe020
siwon <muncool39@gmail.com>
2023-11-13 21:57:05 +0900

    solve: prgrms_제일 작은 수 제거하기

--- November/1주차/prgrms_제일 작은 수 제거하기/Solution.java
+++ November/1주차/prgrms_제일 작은 수 제거하기/Solution.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        if(arr.length==1) return new int[]{-1};
+        int[] answer = new int[arr.length-1];
+        int min = Arrays.stream(arr).min().getAsInt();
+        int i=0;
+        int arrcount = 0;
+        while(i<answer.length){
+            if(arr[arrcount]==min){
+                arrcount++;
+                continue;
+            }
+            answer[i] = arr[arrcount];
+            arrcount++;
+            i++;
+        }
         return answer;
     }
 }
