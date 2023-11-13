190b80b8704341de8dd66c476b8132a8224f0d3b
siwon <muncool39@gmail.com>
2023-11-13 21:56:39 +0900

    solve: prgrms_수박수박수박수박수

--- November/1주차/prgrms_수박수박수박수박수박수/Solution.java
+++ November/1주차/prgrms_수박수박수박수박수박수/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        return answer;
+        String subak = "수박";
+        StringBuilder subaks = new StringBuilder("");
+        for(int i=0;i<n/2;i++){
+            subaks.append(subak);
+        }
+        if(n%2!=0) subaks.append("수");
+        return subaks.toString();
     }
 }
