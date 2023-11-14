a122e5ea43597b99c4835b0eec7635e026940b49
ChaeAg <dydkglel123@gmail.com>
2023-11-14 18:18:20 +0900

    solve: boj_집합

--- November/1주차/boj_집합/Main.java
+++ November/1주차/boj_집합/Main.java
@@ -1,3 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
-    
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<M; i++) {
+            String[] arr = br.readLine().split(" ");
+            switch (arr[0]) {
+                case "add":
+                    if(!list.contains(arr[1])) list.add(arr[1]);
+                    break;
+                case "remove":
+                    if(list.contains(arr[1])) list.remove(arr[1]);
+                    break;
+                case "check":
+                    sb.append(list.contains(arr[1]) ? 1 : 0).append("\n");
+                    break;
+                case "toggle":
+                    if(list.contains(arr[1])) list.remove(arr[1]);
+                    else list.add(arr[1]);
+                    break;
+                case "all":
+                    list.clear();
+                    list = IntStream.rangeClosed(1, 20)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.toList());
+                    break;
+                case "empty":
+                    list.clear();
+                    break;
+            }
+        }
+        System.out.print(sb);
+	}
 }
