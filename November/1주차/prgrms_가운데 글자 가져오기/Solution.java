7a59b782c0a3e914858ee4c9199605af56206431
ChaeAg <dydkglel123@gmail.com>
2023-11-14 18:18:33 +0900

    solve: prgrms_가운데 글자 가져오기

--- November/1주차/prgrms_가운데 글자 가져오기/Solution.java
+++ November/1주차/prgrms_가운데 글자 가져오기/Solution.java
@@ -1,6 +1,10 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        if(s.length() % 2 == 0) {
+            return s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        }
+        else {
+            return s.substring(s.length() / 2, s.length() / 2 + 1);
+        }
     }
 }
