464e89913c4e4330ad41527e929e54500533454f
ChaeAg <dydkglel123@gmail.com>
2023-11-14 18:18:58 +0900

    solve: prgrms_제일 작은 수 제거하기

--- November/1주차/prgrms_제일 작은 수 제거하기/Solution.java
+++ November/1주차/prgrms_제일 작은 수 제거하기/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
-    public int[] solution(int[] arr) {
-        int[] answer = {};
-        return answer;
+    public List<Integer> solution(int[] arr) {
+        if(arr.length < 2) return List.of(-1);
+        List<Integer> list = Arrays.stream(arr)
+            .boxed()
+            .collect(Collectors.toList());
+        list.remove(list.indexOf(Arrays.stream(arr)
+                                 .min()
+                                 .getAsInt()));
+        return list;
     }
 }
