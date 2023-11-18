0ff2579cf12ff756707153f1456ceab072041251
gitseoyeon <tjdus553926@gmail.com>
2023-11-18 21:27:23 +0900

    solve: prgrms_약수의 개수와 덧셈

--- November/2주차/prgrms_약수의 개수와 덧셈/Solution.java
+++ November/2주차/prgrms_약수의 개수와 덧셈/Solution.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 class Solution {
-    public String solution(String s) {
-        String answer = "";
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i = left; i <= right; i++){
+            List<Integer> list = new ArrayList<Integer>();
+            for(int j = 1; j <= i; j ++){
+                if(i % j == 0){
+                    list.add(j);
+                }
+            }
+            if(list.size() % 2 == 0){
+                answer += i;
+            }else{
+                answer -= i;
+            }
+        }
         return answer;
     }
 }
