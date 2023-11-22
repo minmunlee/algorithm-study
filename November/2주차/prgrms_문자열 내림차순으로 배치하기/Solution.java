c6accc6e0c834a02961af9e85f93475165eb1f91
siwon <muncool39@gmail.com>
2023-11-22 19:26:17 +0900

    solve: prgrms_문자열 내림차순으로 배치하기

--- November/2주차/prgrms_문자열 내림차순으로 배치하기/Solution.java
+++ November/2주차/prgrms_문자열 내림차순으로 배치하기/Solution.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        String[] word = s.split("");
+        Arrays.sort(word,Collections.reverseOrder());
+        StringBuilder answer = new StringBuilder("");
+        for(String w : word){
+            answer.append(w);
+        }
+        return answer.toString();
     }
 }
