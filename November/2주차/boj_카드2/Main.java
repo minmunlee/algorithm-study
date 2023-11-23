42dd6c7b0fe510217d5dd434c41542d2006b127f
siwon <muncool39@gmail.com>
2023-11-23 16:45:40 +0900

    solve: boj_카드2

--- November/2주차/boj_카드2/Main.java
+++ November/2주차/boj_카드2/Main.java
@@ -1,4 +1,17 @@
-
-public class Main {
-    
+import java.util.*;
+import java.io.*;
+public class Main{
+	public static void main(String[] args)throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+		Deque<Integer> deq = new LinkedList<>();
+		for(int i=0;i<N;i++){
+		    deq.add(i+1);
+		}
+		while(deq.size()>1){
+		    deq.remove();
+		    deq.add(deq.remove());
+		}
+		System.out.print(deq.getFirst());
+	}
 }
