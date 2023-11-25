57433f4be7a6448c2c6e2216e2fb6a8d5e0c2f04
gitseoyeon <tjdus553926@gmail.com>
2023-11-25 23:12:26 +0900

    solve: prgrms_3진법 뒤집기

--- November/3주차/prgrms_3진법 뒤집기/Solution.java
+++ November/3주차/prgrms_3진법 뒤집기/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        StringBuffer sb = new StringBuffer(Integer.toString(n, 3));
+        String reverse = sb.reverse().toString();
+        
+        answer = Integer.parseInt(reverse, 3);
+        
+        
         return answer;
     }
 }
