f057cb700fcc74f0354b3bb6b345827c889dbd5d
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:52:10 +0900

    solve: boj_ A->B

--- November/3주차/boj_A-B/Main.java
+++ November/3주차/boj_A-B/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                int count = 0;
+
+                while(A < B) {
+                        if(B % 10 == 1) {
+                                B /= 10;
+                                count++;
+                        }
+                        else if(B % 2 == 0) {
+                                B /= 2;
+                                count++;
+                        }
+                        else break;
+                }
+                if(A == B) System.out.print(count+1);
+                else System.out.print("-1");
+        }
 }
