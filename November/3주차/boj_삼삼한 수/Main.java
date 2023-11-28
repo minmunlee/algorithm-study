da297bbfe9bafe2b216dae16494de48bf2ea4766
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:52:21 +0900

    solve: boj_삼삼한 수

--- November/3주차/boj_삼삼한 수/Main.java
+++ November/3주차/boj_삼삼한 수/Main.java
@@ -1,3 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                int N = Integer.parseInt(br.readLine());
+                if(Integer.toString(N, 3).contains("2") || N == 0) System.out.println("NO");
+                else System.out.println("YES");
+        }
 }
