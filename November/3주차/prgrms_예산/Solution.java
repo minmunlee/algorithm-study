059d044b851265d16c4d54d266560d9fd86aef75
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:53:05 +0900

    solve: prgrms_예산

--- November/3주차/prgrms_예산/Solution.java
+++ November/3주차/prgrms_예산/Solution.java
@@ -1,6 +1,15 @@
+import java.util.*;
 class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
-        return answer;
+    public int solution(int[] d, int budget) {        
+        int[] sortArray = Arrays.stream(d).sorted().toArray();
+        int sum = 0;
+        int count = 0;
+        for(int i : sortArray) {
+            if(sum + i > budget) break;
+            sum += i;
+            count++;
+        }
+        
+        return count;
     }
 }
