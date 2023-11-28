d611958626360c807ebb1b25c188983aca8f202a
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:52:56 +0900

    solve: prgrms_같은 숫자는 싫어

--- November/3주차/prgrms_같은 숫자는 싫어/Solution.java
+++ November/3주차/prgrms_같은 숫자는 싫어/Solution.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        int[] answer = {};
+    public List<Integer> solution(int []arr) {
+        List<Integer> list = new ArrayList();
+        list.add(arr[0]);
         
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        Arrays.stream(arr)
+            .filter(i -> list.get(list.size()-1) != i)
+            .forEach(i -> list.add(i));
+        
+        return list;
     }
 }
