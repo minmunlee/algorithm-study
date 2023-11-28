530fa55f2ad2e0d20a9880884d3c3fa27c2b7955
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:52:47 +0900

    solve: prgrms_3진법 뒤집기

--- November/3주차/prgrms_3진법 뒤집기/Solution.java
+++ November/3주차/prgrms_3진법 뒤집기/Solution.java
@@ -1,6 +1,12 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        return answer;
+    public int solution(int n) {        
+        String[] arr = Integer.toString(n, 3).split("");
+        String a = "";
+        
+        for(int i=arr.length-1; i>=0; i--) {
+            a += arr[i];
+        }
+        
+        return Integer.parseInt(a, 3);
     }
 }
