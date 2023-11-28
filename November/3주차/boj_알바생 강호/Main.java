bf3b022f1ddca5369da3d629e97f982498e8719e
ChaeAg <dydkglel123@gmail.com>
2023-11-29 01:52:28 +0900

    solve: boj_알바생 강호

--- November/3주차/boj_알바생 강호/Main.java
+++ November/3주차/boj_알바생 강호/Main.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                int N = Integer.parseInt(br.readLine());
+                List<Integer> list = new ArrayList<>();
+                for(int i=0; i<N; i++) {
+                        list.add(Integer.parseInt(br.readLine()));
+                }
+                list.sort(Comparator.reverseOrder());
+                Long tip = 0L;
+                for(int i=0; i<list.size(); i++) {
+                        if(list.get(i) - (i+1 -1) <= 0) break;
+                        tip += list.get(i) - (i+1 -1);
+                }
+                System.out.print(tip);
+        }
 }
