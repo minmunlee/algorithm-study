57763fbf8010c515df86dc35257290740817b836
gitseoyeon <tjdus553926@gmail.com>
2023-11-29 15:58:19 +0900

    solve: boj_삼삼한 수

--- November/3주차/boj_삼삼한 수/Main.java
+++ November/3주차/boj_삼삼한 수/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        if(X == 0){
+            System.out.println("NO");
+            System.exit(0);
+        }
+        while (X != 0){
+            if(X % 3 > 1){
+                    System.out.println("NO");
+                    System.exit(0);
+            }
+            X /= 3;
+        }
+
+        System.out.println("YES");
+    }
+
+
 }
+    
+
