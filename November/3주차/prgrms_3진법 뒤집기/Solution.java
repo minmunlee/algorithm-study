d8e716858a32ab59379fffee22f6c3a2bf246608
siwon <muncool39@gmail.com>
2023-11-30 12:51:39 +0900

    solve: prgrms_3진법 뒤집기

--- November/3주차/prgrms_3진법 뒤집기/Solution.java
+++ November/3주차/prgrms_3진법 뒤집기/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
+        String three = sb.reverse().toString();
+        int answer = Integer.parseInt(three, 3);
         return answer;
     }
 }
