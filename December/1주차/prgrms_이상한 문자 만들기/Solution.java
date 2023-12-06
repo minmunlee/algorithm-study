c534cf69fd1c039f7ae08044899c4179daf575e2
siwon <muncool39@gmail.com>
2023-12-06 17:52:54 +0900

    solve: prgrms_이상한 문자 만들기

--- December/1주차/prgrms_이상한 문자 만들기/Solution.java
+++ December/1주차/prgrms_이상한 문자 만들기/Solution.java
@@ -1,6 +1,15 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        s = s.toUpperCase();
+        String[] words = s.split(" ",-1);
+        for(int i=0;i<words.length;i++){
+            String[] wordChar = words[i].split("");
+            for(int j=1;j<wordChar.length;j+=2){
+                wordChar[j] = wordChar[j].toLowerCase();
+            }
+            words[i] = String.join("",wordChar);
+        }
+        String answer = String.join(" ",words);
         return answer;
     }
 }
