60f50cc44d6cc7b63b33a38f4e76c571539df454
ChaeAg <dydkglel123@gmail.com>
2023-12-07 01:02:38 +0900

    solve: boj_숫자 카드 2

--- December/1주차/boj_숫자 카드 2/main.java
+++ December/1주차/boj_숫자 카드 2/main.java
@@ -1,3 +1,28 @@
-public class main {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                StringBuilder sb = new StringBuilder();
+                StringTokenizer st;
+                Map<Integer, Integer> map = new HashMap();
+                
+                int N = Integer.parseInt(br.readLine());
+                st = new StringTokenizer(br.readLine());
+                for(int i=0; i<N; i++) {
+                        int number = Integer.parseInt(st.nextToken());
+                        map.put(number, map.getOrDefault(number,0) + 1);
+                }
+                
+                int M = Integer.parseInt(br.readLine());
+                st = new StringTokenizer(br.readLine());
+                for(int i=0; i<M; i++) {
+                        int number = Integer.parseInt(st.nextToken());
+                        sb.append(map.getOrDefault(number, 0)).append(" ");
+                }
+                System.out.print(sb);
+        }
 }
