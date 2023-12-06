d81859d72df56776e2bcb027fe8e92db2592a9b6
ChaeAg <dydkglel123@gmail.com>
2023-12-07 01:03:21 +0900

    solve: prgrms_시저암호

--- December/1주차/prgrms_시저암호/Solution.java
+++ December/1주차/prgrms_시저암호/Solution.java
@@ -1,6 +1,27 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        return answer;
+        String result = "";
+        char[] arr = s.toCharArray();
+        
+        for(int i=0; i<arr.length; i++) {
+            int ask = (int)arr[i] + n;
+            if(arr[i] == ' ') { // 공백일 때
+                result += " ";
+                continue;
+            }
+            else if(ask >= 65+n && ask <= 90+n) { // 대문자일 때
+                if(ask > 90) {
+                    ask = 64 + ask - 90;
+                }
+            }
+            else { // 소문자일 때
+                if(ask > 122) {
+                    ask = 96 + ask - 122;
+                }
+            }
+            result += (char)ask;
+        }
+        
+        return result;
     }
 }
