6ee1d5f3331eeade7914af721abb3691bc025520
ChaeAg <dydkglel123@gmail.com>
2023-12-07 01:03:38 +0900

    solve: prgrms_최소직사각형

--- December/1주차/prgrms_최소직사각형/Solution.java
+++ December/1주차/prgrms_최소직사각형/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
+        int colMax = 0, lowMax = 0;
+        
+        for(int i=0; i<sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            if(colMax < sizes[i][0]) colMax = sizes[i][0];
+            if(lowMax < sizes[i][1]) lowMax = sizes[i][1];
+        }
+        
+        return colMax * lowMax;
     }
 }
