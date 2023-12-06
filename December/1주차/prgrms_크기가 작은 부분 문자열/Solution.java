c648c585c8d430d0aea372c29925e0749dac644d
siwon <muncool39@gmail.com>
2023-12-06 17:53:50 +0900

    solve: prgrms_크기가 작은 부분 문자열

--- December/1주차/prgrms_크기가 작은 부분 문자열/Solution.java
+++ December/1주차/prgrms_크기가 작은 부분 문자열/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(String t, String p) {
-        int answer = 0;
+        int size = p.length();
+        long num = Long.parseLong(p);
+        int count = 0;
+        for(int now=size;now<=t.length();now++){
+            long subNum = Long.parseLong(t.substring(now-size, now));
+            if(subNum<=num) count++;
+        }
+        int answer = count;
         return answer;
     }
 }
