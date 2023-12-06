dec1d89f7cad0b60454c296020433762ed183f29
siwon <muncool39@gmail.com>
2023-12-06 17:53:29 +0900

    solve: prgrms_최소직사각형

--- December/1주차/prgrms_최소직사각형/Solution.java
+++ December/1주차/prgrms_최소직사각형/Solution.java
@@ -1,6 +1,22 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
+        int[] garo = new int[sizes.length];
+        int[] sero = new int[sizes.length];
+        for(int i=0;i<sizes.length;i++){
+            int w = sizes[i][0];
+            int h = sizes[i][1];
+            if(w>h){
+                garo[i] = w;
+                sero[i] = h;
+            }else{
+                garo[i] = h;
+                sero[i] = w;
+            }
+        }
+        int garoMax = Arrays.stream(garo).max().getAsInt();
+		int seroMax = Arrays.stream(sero).max().getAsInt();
+        int answer = garoMax * seroMax;
         return answer;
     }
 }
