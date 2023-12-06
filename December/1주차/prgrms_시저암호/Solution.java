ccf56d5f72a2c3cbc6d72577894adfd00dfd0863
siwon <muncool39@gmail.com>
2023-12-06 17:52:20 +0900

    solve: prgrms_시저암호

--- December/1주차/prgrms_시저암호/Solution.java
+++ December/1주차/prgrms_시저암호/Solution.java
@@ -1,6 +1,29 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            int asc = (int) s.charAt(i);
+            if(asc==32){
+                answer.append(" ");
+                continue;
+            }
+            int calNum = asc + n;
+            int checkNum = (asc>=97)?rowCaseOverCheck(calNum):upCaseOverCheck(calNum);
+            answer.append((char)checkNum);
+        } 
+        return answer.toString();
+    }
+    public int upCaseOverCheck(int calNum){
+        if(calNum>90){
+            return 64+(calNum-90);
+        }
+        return calNum;
+    }
+    
+    public int rowCaseOverCheck(int calNum){
+        if(calNum>122){
+            return 96+(calNum-122);
+        }
+        return calNum;
     }
 }
