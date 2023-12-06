117c63999a541202dda16ac26d7f0591bf640d6d
ChaeAg <dydkglel123@gmail.com>
2023-12-07 01:03:29 +0900

    solve: prgrms_이상한 문자 만들기

--- December/1주차/prgrms_이상한 문자 만들기/Solution.java
+++ December/1주차/prgrms_이상한 문자 만들기/Solution.java
@@ -1,6 +1,20 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        int idxCount = 0;
+        String[] str = s.split("");
+        String result = "";
+        for(int i=0; i<str.length; i++) {
+            if(str[i].equals(" ")) {
+                result += " ";
+                idxCount = 0;
+            }
+            else {
+                if(idxCount % 2 == 0) result += str[i].toUpperCase();
+                else result += str[i].toLowerCase();
+                idxCount++;
+            }
+        }
+        
+        return result;
     }
 }
