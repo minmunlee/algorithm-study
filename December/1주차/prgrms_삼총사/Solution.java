4c3f4d262954069ecebe386e28937d2728a20be4
siwon <muncool39@gmail.com>
2023-12-06 17:52:04 +0900

    solve: prgrms_삼총사

--- December/1주차/prgrms_삼총사/Solution.java
+++ December/1주차/prgrms_삼총사/Solution.java
@@ -1,6 +1,19 @@
+import java.util.*;
 class Solution {
+    static int count = 0;
+    static int[] numbers;
     public int solution(int[] number) {
-        int answer = 0;
-        return answer;
+        numbers = number;
+        dfs(0, 0, 1);
+        return count;
     }
+    public void dfs(int start, int sum, int deep){
+        if(deep>3){
+            if(sum==0) count++;
+            return;
+        }
+        for(int idx=start;idx<numbers.length;idx++){
+            dfs(idx+1, sum+numbers[idx], deep+1);
+        }
+    }   
 }
