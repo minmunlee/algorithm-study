252c821f05899e712820c516e7e506d70b0f7db9
siwon <muncool39@gmail.com>
2023-12-20 16:32:55 +0900

    solve: prgrms_문자열 내 마음대로 정렬하기

--- December/2주차/prgrms_문자열 내 마음대로 정렬하기/Solution.java
+++ December/2주차/prgrms_문자열 내 마음대로 정렬하기/Solution.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        return answer;
+        Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+                String s1IndexS = s1.substring(n,n+1);
+                String s2IndexS = s2.substring(n,n+1);
+                if(s1IndexS.equals(s2IndexS)){
+                    return s1.compareTo(s2);
+                }
+				return s1IndexS.compareTo(s2IndexS);
+			}
+		});
+        return strings;
     }
 }
