298cde487a20caf9dc3be5c4a0ff6a4b94fe303f
siwon <muncool39@gmail.com>
2023-12-20 16:32:29 +0900

    solve: prgrms_가장 가까운 글자

--- December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
+++ December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        int[] answer = new int[s.length()];
+        HashMap<String,Integer> count = new HashMap<>();
+        String[] cList = s.split("");
+        for(int i=0;i<answer.length;i++){
+            String c = cList[i];
+            int close = i - count.getOrDefault(c, i+1);
+            answer[i] = close;
+            count.putIfAbsent(c, i);
+            count.replace(c, i);
+        }
         return answer;
     }
 }
