81ceab9e35e8d62a5b6e54c896f7edc04628ccdc
ChaeAg <dydkglel123@gmail.com>
2023-12-21 15:24:22 +0900

    solve: boj_여우는 어떻게 울지

--- December/2주차/boj_여우는 어떻게 울지/Main.java
+++ December/2주차/boj_여우는 어떻게 울지/Main.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                int T = Integer.parseInt(br.readLine());
+
+                for(int i=0; i<T; i++) {
+                        List<String> list = new ArrayList<>(List.of(br.readLine().split(" ")));
+                        while(true) {
+                                String line = br.readLine();
+                                if(line.equals("what does the fox say?")) {
+                                        list.forEach(str -> System.out.print(str + " "));
+                                        break;
+                                }
+                                while(list.contains(line.split(" ")[2])) {
+                                        list.remove(line.split(" ")[2]);
+                                }
+                        }
+                }
+        }
 }
