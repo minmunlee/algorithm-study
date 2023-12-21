9cdaf454558bfe4c3b645ce15262c2b88ee97310
gitseoyeon <tjdus553926@gmail.com>
2023-12-21 15:53:03 +0900

    solve: prgrms_가장 가까운 같은 글자

--- December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
+++ December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(String s) {
-        int[] answer = {};
+    public List<Integer> solution(String s) {
+        List<Integer> answer = new ArrayList<Integer>();
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            answer.add(stack.search(c)); // search: 같은 문자열중 가장 가까운 위치 반환
+            stack.push(c);
+            
+        }
+        
+        
         return answer;
     }
 }
