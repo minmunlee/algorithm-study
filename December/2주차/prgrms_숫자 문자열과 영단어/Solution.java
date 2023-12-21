cf25378ff18cedddc9399d4e096ef1a0e0e458e0
gitseoyeon <tjdus553926@gmail.com>
2023-12-21 15:53:28 +0900

    solve: prgrms_숫자 문자열과 영단어

--- December/2주차/prgrms_숫자 문자열과 영단어/Solution.java
+++ December/2주차/prgrms_숫자 문자열과 영단어/Solution.java
@@ -1,6 +1,50 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        String str = "";
+        String temp = "";
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){ // 숫자인지 확인
+                str += c;
+            }else{
+                temp += c;
+            }
+            
+            // 문자열 숫자로 변환
+            if(temp.equals("one")){
+                str += "1";
+                temp = "";
+            }else if(temp.equals("two")){
+                str += "2";
+                temp = "";
+            }else if(temp.equals("three")){
+                str += "3";
+                temp = "";
+            }else if(temp.equals("four")){
+                str += "4";
+                temp = "";
+            }else if(temp.equals("five")){
+                str += "5";
+                temp = "";
+            }else if(temp.equals("six")){
+                str += "6";
+                temp = "";
+            }else if(temp.equals("seven")){
+                str += "7";
+                temp = "";
+            }else if(temp.equals("eight")){
+                str += "8";
+                temp = "";
+            }else if(temp.equals("nine")){
+                str += "9";
+                temp = "";
+            }else if(temp.equals("zero")){
+                str += "0";
+                temp = "";
+            }
+        }
+        answer = Integer.parseInt(str);
         return answer;
     }
 }
