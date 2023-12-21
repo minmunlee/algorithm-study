43fb573c8e5cc10742d67cb97b52867881a06677
ChaeAg <dydkglel123@gmail.com>
2023-12-21 15:24:33 +0900

    solve: prgrms_가장 가까운 같은 글자

--- December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
+++ December/2주차/prgrms_가장 가까운 같은 글자/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        int[] answer = new int[s.length()];
+        String str = "";
+        int idx = 0;
+        for(String word : s.split("")) {
+            if(str.lastIndexOf(word) != -1) {
+                answer[idx] = idx - str.lastIndexOf(word);
+            }
+            else {
+                answer[idx] = -1;
+            }
+            str += word;
+            idx++;
+        }
         return answer;
     }
 }
