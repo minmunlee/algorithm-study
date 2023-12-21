0b1409955a5e5835acfbb0246b31b8d5db22d6ef
ChaeAg <dydkglel123@gmail.com>
2023-12-21 15:24:53 +0900

    solve: prgrms_숫자 문자열과 영단어

--- December/2주차/prgrms_숫자 문자열과 영단어/Solution.java
+++ December/2주차/prgrms_숫자 문자열과 영단어/Solution.java
@@ -1,6 +1,21 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        return answer;
+        List<String> list = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        String result = "";
+        String word = "";
+        for(String str : s.split("")) {
+            if(str.matches("[-+]?\\d*\\.?\\d+")) {
+                result += Integer.parseInt(str);
+            }
+            else {
+               word += str;
+            }
+            if(list.contains(word)) {
+                 result += list.indexOf(word);
+                 word = "";
+            }
+        }
+        return Integer.parseInt(result);
     }
 }
