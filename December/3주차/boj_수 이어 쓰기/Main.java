09a45ca9507d1506fc8711e5c812b109f698a8c4
ChaeAg <dydkglel123@gmail.com>
2023-12-26 14:11:41 +0900

    solve: boj_수 이어 쓰기

--- December/3주차/boj_수 이어 쓰기/Main.java
+++ December/3주차/boj_수 이어 쓰기/Main.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+        public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String num = br.readLine(); // 예) 1234, 234092
+                String i = "0"; // 증가하면서 N 구하는 수
+                while(num.length() > 0) {
+                        i = String.valueOf(Integer.parseInt(i)+1);
+                        String number = i;
+                        while(number.length() > 0 && num.length() > 0) {
+                                if(num.charAt(0) == number.charAt(0)) {
+                                        num = num.substring(1);
+                                }
+                                number = number.substring(1);
+                        }
+                }
+                System.out.print(i);
+        }
 }
