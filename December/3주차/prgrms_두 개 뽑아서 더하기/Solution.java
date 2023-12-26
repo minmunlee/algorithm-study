5b3007e102b7c9106962088cb4f8e15d57939c61
ChaeAg <dydkglel123@gmail.com>
2023-12-26 14:12:26 +0900

    solve: prgrms_두 개 뽑아서 더하기

--- December/3주차/prgrms_두 개 뽑아서 더하기/Solution.java
+++ December/3주차/prgrms_두 개 뽑아서 더하기/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        return answer;
+        Set<Long> sumSet = new HashSet();
+        for(int i=0; i<numbers.length - 1; i++) {
+            for(int k=i+1; k<numbers.length; k++) {
+                sumSet.add(Long.valueOf(numbers[i]) + Long.valueOf(numbers[k]));
+            }
+        }
+        
+        return sumSet.stream().mapToInt(Long::intValue).sorted().toArray();
     }
 }
