425d4744051768008d1f26ba9f67f7edfffc1019
ChaeAg <dydkglel123@gmail.com>
2023-12-26 14:12:18 +0900

    solve: prgrms_K번째 수

--- December/3주차/prgrms_K번째 수/Solution.java
+++ December/3주차/prgrms_K번째 수/Solution.java
@@ -1,6 +1,17 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++) {
+            List<Integer> arr = new ArrayList();
+            for(int k=commands[i][0]-1; k<commands[i][1]; k++) {
+                arr.add(array[k]);
+            }
+            Collections.sort(arr);
+            answer[i] = arr.get(commands[i][2] - 1);
+        }
+        
         return answer;
     }
 }
