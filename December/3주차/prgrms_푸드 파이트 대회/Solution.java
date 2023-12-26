e4cac9150aa982d9a6209fa56fc7cfb9e29d47c8
ChaeAg <dydkglel123@gmail.com>
2023-12-26 14:12:34 +0900

    solve: prgrms_푸드 파이트 대회

--- December/3주차/prgrms_푸드 파이트 대회/Solution.java
+++ December/3주차/prgrms_푸드 파이트 대회/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++) {
+            for(int k=0; k<(food[i] / 2); k++) {
+                sb.append(i);
+            }
+        }
+        String reverse = sb.reverse().toString();
+        sb.reverse().append("0").append(reverse);
+        
+        return sb.toString();
     }
 }
