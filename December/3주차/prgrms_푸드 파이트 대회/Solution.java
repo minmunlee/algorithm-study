963e9a39e415ca1e0aad90f3f98c698a72df19f2
gitseoyeon <tjdus553926@gmail.com>
2023-12-28 01:35:35 +0900

    solve: prgrms_ 푸드 파이트 대회

--- December/3주차/prgrms_푸드 파이트 대회/Solution.java
+++ December/3주차/prgrms_푸드 파이트 대회/Solution.java
@@ -1,6 +1,24 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
+        String str1 = "";
+        String str2 = "";
+        
+        for(int i = 1; i < food.length; i++){
+            int n = 0;
+            int cal = food[i] / 2;
+            while(n != cal){
+                str1 += String.valueOf(i);
+                n++;
+            }
+        } 
+
+        for(int i = str1.length() - 1; i >= 0; i--){
+            str2 += str1.charAt(i);
+        }
+        
+        answer = str1 + '0' + str2;
+        
         return answer;
     }
 }
