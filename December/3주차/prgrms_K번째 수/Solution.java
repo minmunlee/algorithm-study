a10560a3cd9912d233f005daf948612994ba268c
gitseoyeon <tjdus553926@gmail.com>
2023-12-28 01:35:03 +0900

    solve: prgrms_K 번째 수

--- December/3주차/prgrms_K번째 수/Solution.java
+++ December/3주차/prgrms_K번째 수/Solution.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+        int[] tmp = {};
+        
+        for(int i = 0; i < commands.length; i++){
+            tmp = IntStream.range(commands[i][0] - 1, commands[i][1]).map(num -> array[num]).toArray();
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2] - 1];
+
+        }
+        
         return answer;
     }
 }
