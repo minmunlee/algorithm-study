45435cef8008ad348e51dc6e0c36b8e06dc4bfb6
gitseoyeon <tjdus553926@gmail.com>
2023-12-28 01:35:19 +0900

    solve: prgrms_두 개 뽑아서 더하기

--- December/3주차/prgrms_두 개 뽑아서 더하기/Solution.java
+++ December/3주차/prgrms_두 개 뽑아서 더하기/Solution.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
+
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = {};
+    public List<Integer> solution(int[] numbers) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                list.add(numbers[i] + numbers[j]);
+            }
+        }
+        List<Integer> answer = list.stream().distinct().collect(Collectors.toList());
+        
+        Collections.sort(answer);
+        
         return answer;
     }
 }
