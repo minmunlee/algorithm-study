d29962f6f322ac5a5986783d80ec016b77d82b24
siwon <muncool39@gmail.com>
2023-12-27 20:48:07 +0900

    solve: prgrms_K번째 수

--- December/3주차/prgrms_K번째 수/Solution.java
+++ December/3주차/prgrms_K번째 수/Solution.java
@@ -1,6 +1,15 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+        for(int t=0;t<commands.length;t++){
+            int i = commands[t][0];
+            int j = commands[t][1];
+            int k = commands[t][2];
+            int[] rangeArray = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(rangeArray);
+            answer[t] = rangeArray[k-1];
+        }
         return answer;
     }
 }
