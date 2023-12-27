87d93ebf1ef34d9608d7d64ed8980f88d4427d28
siwon <muncool39@gmail.com>
2023-12-27 20:47:52 +0900

    solve: prgrms_[1차] 비밀지도

--- December/3주차/prgrms_[1차] 비밀지도/Solution.java
+++ December/3주차/prgrms_[1차] 비밀지도/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+        String format = "%" + n + "s";
+        for(int i=0;i<n;i++){
+            String[] map1 = String.format(format, Integer.toBinaryString(arr1[i])).split("");
+            String[] map2 = String.format(format, Integer.toBinaryString(arr2[i])).split("");
+            StringBuilder sb = new StringBuilder("");
+            for(int j=0;j<n;j++){
+                if(map1[j].equals("1") || map2[j].equals("1")) sb.append("#");
+                else sb.append(" ");
+            }
+            answer[i] = sb.toString();
+        }
         return answer;
     }
 }
