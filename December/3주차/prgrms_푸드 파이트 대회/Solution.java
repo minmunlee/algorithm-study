677d10ad0b152af9c60a81ba77c237ac20639269
siwon <muncool39@gmail.com>
2023-12-27 20:48:38 +0900

    solve: prgrms_푸드 파이트 대회

--- December/3주차/prgrms_푸드 파이트 대회/Solution.java
+++ December/3주차/prgrms_푸드 파이트 대회/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder("");
+        for(int i=1;i<food.length;i++){
+            for(int j=0;j<food[i]/2;j++){
+                sb.append(i);
+            }
+        }
+        String left = sb.toString();
+        sb.append("0").append(new StringBuilder(left).reverse().toString());
+        return sb.toString();
     }
 }
