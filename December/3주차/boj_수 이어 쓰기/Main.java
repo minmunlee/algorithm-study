9cf98c6ba42bcb625c2e47d97c6715c143adfb4f
gitseoyeon <tjdus553926@gmail.com>
2023-12-28 01:34:11 +0900

    solve: boj_수 이어 쓰기

--- December/3주차/boj_수 이어 쓰기/Main.java
+++ December/3주차/boj_수 이어 쓰기/Main.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String number = br.readLine(); // 슷자 입력
+        int cnt = 0;
+        
+        while(number.length() != 0){
+            cnt++;
+            String n = String.valueOf(cnt); // 증가 하는 값 체크
+
+            while(number.length()> 0 && n.length() > 0){
+                if(number.charAt(0) == n.charAt(0)){ // n이 늘어나다가 입력한 맨 앞 숫자와 같아질 경우
+                    number = number.substring(1); // 맨 앞 숫자 지우기
+                }
+                n = n.substring(1); // n 값 지우기
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
     
 }
