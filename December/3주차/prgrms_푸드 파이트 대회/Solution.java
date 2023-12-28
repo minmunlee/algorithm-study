4e866c978a9c1bcdcc6c3f2b09eeb4ddd62772d3
sejyom <akxxkd01@gmail.com>
2023-12-28 11:41:09 +0900

    slove: prgrms_푸드 파이트 대회

--- December/3주차/prgrms_푸드 파이트 대회/Solution.java
+++ December/3주차/prgrms_푸드 파이트 대회/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
-    public String solution(int[] food) {
-        String answer = "";
-        return answer;
+    public String solution(int [] food) {
+        StringBuffer sb = new StringBuffer();
+        StringBuffer answer = new StringBuffer();
+        for (int i = 1; i < food.length; i++)
+            sb.append(String.valueOf(i).repeat(food[i]/2));
+
+        answer.append(sb);
+        answer.append(sb.reverse());
+        answer.insert(answer.length() / 2, "0");
+        return answer.toString();
     }
 }
