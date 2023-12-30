908fa958a4240890a34f43de50efd127057ba933
ChaeAg <dydkglel123@gmail.com>
2023-12-30 12:59:31 +0900

    solve: prgrms_카드 뭉치

--- January/1주차/prgrms_카드 뭉치/Solution.java
+++ January/1주차/prgrms_카드 뭉치/Solution.java
@@ -1,6 +1,25 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
+        String answer = "Yes";
+        int cards1Idx = 0;
+        int cards2Idx = 0;
+        int goalIdx = 0;
+        
+        while(goalIdx < goal.length) {
+            if(cards1Idx < cards1.length && goal[goalIdx].equals(cards1[cards1Idx])) {
+                goalIdx++;
+                cards1Idx++;
+            }
+            else if(cards2Idx < cards2.length && goal[goalIdx].equals(cards2[cards2Idx])) {
+                goalIdx++;
+                cards2Idx++;
+            }
+            else {
+                answer = "No";
+                break;
+            }
+        }
+        
         return answer;
     }
 }
