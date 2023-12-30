1897c6cdd21ddd1b0ddf5848c5b754485dffd9d5
ChaeAg <dydkglel123@gmail.com>
2023-12-30 12:59:21 +0900

    solve: prgrms_추억 점수

--- January/1주차/prgrms_추억 점수/Solution.java
+++ January/1주차/prgrms_추억 점수/Solution.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
-        return answer;
+        List<Integer> answer = new ArrayList();
+        Map<String, Integer> memory = new HashMap();
+        
+        for(int i=0; i<name.length; i++) {
+            memory.put(name[i], yearning[i]);
+        }
+        
+        for(int i=0; i<photo.length; i++) {
+            int sum = 0;
+            for(int j=0; j<photo[i].length; j++) {
+                sum += memory.getOrDefault(photo[i][j], 0);
+            }
+            answer.add(sum);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
