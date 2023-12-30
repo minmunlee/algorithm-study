60eb8bb9003e076d116e55afb02c6923e61bb8c6
ChaeAg <dydkglel123@gmail.com>
2023-12-30 12:59:02 +0900

    solve: boj_중복 빼고 정렬하기

--- January/1주차/boj_중복 빼고 정렬하기/Main.java
+++ January/1주차/boj_중복 빼고 정렬하기/Main.java
@@ -1,3 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for(int num : Arrays.stream(br.readLine().split(" "))
+            .distinct()
+            .mapToInt(Integer::parseInt)
+            .sorted().toArray()) {
+            sb.append(num).append(" ");
+        }
+        System.out.print(sb);
+    }
 }
