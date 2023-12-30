943552531f1eb11e2df9e5f0a6c5f6e6bcf89f8f
ChaeAg <dydkglel123@gmail.com>
2023-12-30 12:59:40 +0900

    solve: prgrms_콜라 문제

--- January/1주차/prgrms_콜라 문제/Solution.java
+++ January/1주차/prgrms_콜라 문제/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
+        
+        while(n >= a) {
+            int jibulCola = (n / a) * a;
+            int getCola = (n / a) * b;
+            n = n - jibulCola + getCola;
+            answer += getCola;
+        }
+        
         return answer;
     }
 }
