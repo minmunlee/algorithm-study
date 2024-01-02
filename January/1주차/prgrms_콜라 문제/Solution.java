e108f048b12cbf79d7e9caf8937461d6faae4f5e
siwon <muncool39@gmail.com>
2024-01-02 22:55:07 +0900

    solve: prgrms_콜라 문제

--- January/1주차/prgrms_콜라 문제/Solution.java
+++ January/1주차/prgrms_콜라 문제/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
+        int emptyBottle = n;
+        while(emptyBottle>=a){
+            int returnCola = (emptyBottle/a)*b;
+            answer += returnCola;
+            emptyBottle%=a;
+            emptyBottle+=returnCola;
+        }
         return answer;
     }
 }
