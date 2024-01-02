18735c5c632e8026cad5a40e9f9255e362bb506d
gitseoyeon <tjdus553926@gmail.com>
2024-01-03 03:01:45 +0900

    solve: prgrms_콜라 문제

--- January/1주차/prgrms_콜라 문제/Solution.java
+++ January/1주차/prgrms_콜라 문제/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
+        int re = 0; // 받은 개수
+        
+        while(n >= a){
+            re = n / a * b;
+            answer += re;
+            n = re + n % a;
+        }
         return answer;
     }
 }
