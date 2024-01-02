6622509fbd6b9b446f3def32f477983f7071b8fa
siwon <muncool39@gmail.com>
2024-01-02 22:54:34 +0900

    solve: prgrms_추억 점수

--- January/1주차/prgrms_추억 점수/Solution.java
+++ January/1주차/prgrms_추억 점수/Solution.java
@@ -1,6 +1,18 @@
+import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
+        int[] answer = new int[photo.length];
+        List<String> nameArray = Arrays.asList(name);
+        for(int i=0;i<answer.length;i++){
+            String[] photohuman = photo[i];
+            int memoryScore = 0;
+            for(String human : photohuman){
+                int nameIndex = nameArray.indexOf(human);
+                if(nameIndex<0) continue;
+                memoryScore+=yearning[nameIndex];
+            }
+            answer[i] = memoryScore;
+        }
         return answer;
     }
 }
