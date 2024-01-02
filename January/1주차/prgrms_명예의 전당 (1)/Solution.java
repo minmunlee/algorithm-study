63197912cc7b760dd3f584d2560cee13a90e55c9
siwon <muncool39@gmail.com>
2024-01-02 22:54:10 +0900

    solve: prgrms_명예의 전당 (1)

--- January/1주차/prgrms_명예의 전당 (1)/Solution.java
+++ January/1주차/prgrms_명예의 전당 (1)/Solution.java
@@ -1,6 +1,23 @@
+import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = {};
+        int[] answer = new int[score.length];
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(score[0]);
+        answer[0] = score[0];
+        for(int i=1;i<score.length;i++){
+            int lastIndex = (i<k)?arr.size()-1:k-1;
+            int nextScore = score[i];
+            if(arr.get(lastIndex)<=nextScore){
+                for(int navi=0;navi<=lastIndex;navi++){
+                    if(arr.get(navi)>nextScore) continue;
+                    arr.add(navi, nextScore);
+                    break;
+                }
+            }else arr.add(nextScore);
+            if(i<k) lastIndex++;
+            answer[i] = arr.get(lastIndex);
+        }
         return answer;
     }
 }
