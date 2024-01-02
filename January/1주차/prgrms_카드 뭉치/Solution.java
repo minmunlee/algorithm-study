4d755e854927fe8331221810e5745e3c2f8805dd
siwon <muncool39@gmail.com>
2024-01-02 22:54:53 +0900

    solve: prgrms_카드 뭉치

--- January/1주차/prgrms_카드 뭉치/Solution.java
+++ January/1주차/prgrms_카드 뭉치/Solution.java
@@ -1,6 +1,20 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        return answer;
+        int lastIndex1=0, lastIndex2=0, count=0;
+        for(int i=0;i<goal.length;i++){
+            String goalString = goal[i];
+            if(lastIndex1<cards1.length && cards1[lastIndex1].equals(goalString)){
+                lastIndex1++;
+                count++;
+                continue;
+            }
+            if(lastIndex2<cards2.length && cards2[lastIndex2].equals(goalString)){
+                lastIndex2++;
+                count++;
+                continue;
+            }
+            break;
+        }
+        return (count<goal.length)?"No":"Yes";
     }
 }
