03ecf66b6cd47f4589fce08c2d874c6f61fffe97
gitseoyeon <tjdus553926@gmail.com>
2024-01-03 03:00:58 +0900

    solve: prgrms_명예의 전당

--- January/1주차/prgrms_명예의 전당 (1)/Solution.java
+++ January/1주차/prgrms_명예의 전당 (1)/Solution.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = {};
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++){
+            if(list.size() < k){ // 명예의 전당 자리가 남아 있는 경우
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i] = list.get(0);
+             }else if(list.size() == k){ // 자리가 가득 찬 경우
+                list.add(score[i]);
+                Collections.sort(list);
+                list.remove(list.get(0));
+                answer[i] = list.get(0);
+            }
+
+        }
         return answer;
     }
 }
