ea4fb3e4a831238a198f811eae8d934f079bf6f1
gitseoyeon <tjdus553926@gmail.com>
2024-01-03 03:00:42 +0900

    solve: boj_중복 빼고 정렬하기

--- January/1주차/boj_중복 빼고 정렬하기/Main.java
+++ January/1주차/boj_중복 빼고 정렬하기/Main.java
@@ -1,3 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        Set<Integer> set = new TreeSet<Integer>();
+
+        for(int i = 0; i < N; i++){
+            set.add(Integer.parseInt(str.nextToken()));
+        }
+
+        for(int i : set){
+            System.out.print(i + " ");
+        }
+        
+
+    }
     
 }
