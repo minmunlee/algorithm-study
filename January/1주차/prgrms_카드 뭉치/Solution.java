4967878ad473bb1bbdabcf845ea25d3ea9415ffa
gitseoyeon <tjdus553926@gmail.com>
2024-01-03 03:01:30 +0900

    solve: prgrms_카드 뭉치

--- January/1주차/prgrms_카드 뭉치/Solution.java
+++ January/1주차/prgrms_카드 뭉치/Solution.java
@@ -1,6 +1,22 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
-        return answer;
+        int n1 = 0;
+        int n2 = 0;
+        
+        for(int i = 0; i < goal.length; i++){
+            if(n1 < cards1.length && cards1[n1].equals(goal[i])){
+                n1++;
+                continue;
+            }
+            if(n2 < cards2.length && cards2[n2].equals(goal[i])){
+                n2++;
+                continue;
+            }
+            
+            return answer = "No";
+        }
+        
+        return answer = "Yes";
     }
 }
