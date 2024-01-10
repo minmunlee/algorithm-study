95691a415fca9d06b9a102b2df702b4baef363c4
siwon <muncool39@gmail.com>
2024-01-10 20:54:58 +0900

    solve: prgrms_폰켓몬

--- January/2주차/prgrms_폰켓몬/Solution.java
+++ January/2주차/prgrms_폰켓몬/Solution.java
@@ -1,6 +1,12 @@
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
+        int max = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        int answer = (set.size()>max)?max:set.size();
         return answer;
     }
 }
