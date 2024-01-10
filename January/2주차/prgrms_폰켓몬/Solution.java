41a27512390b3e4c227d47579da781a6cc44f1f2
gitseoyeon <tjdus553926@gmail.com>
2024-01-11 02:53:25 +0900

    solve: prgrms_폰켓몬

--- January/2주차/prgrms_폰켓몬/Solution.java
+++ January/2주차/prgrms_폰켓몬/Solution.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        return answer;
+        int choice = nums.length / 2; // 선택 할 수 있는 포켓몬의 개수
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!list.contains(nums[i])){ // nums 에서 이미 중복되는 값들을 제거한 리스트
+                list.add(nums[i]);
+            }
+        }
+        
+        if(choice <= list.size()){
+            return answer = choice;
+        }else{
+            return answer = list.size();
+        }
     }
 }
