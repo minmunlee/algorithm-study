cfe9c4e036d4ba7c81536f1384d3f3d1e4f549c2
ChaeAg <dydkglel123@gmail.com>
2024-01-11 16:27:03 +0900

    lee: solve prgrms_폰켓몬

--- January/2주차/prgrms_폰켓몬/Solution.java
+++ January/2주차/prgrms_폰켓몬/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        return answer;
+        Map<Integer, Integer> numMap = new HashMap();
+        
+        for(int num : nums) {
+            numMap.put(num, numMap.getOrDefault(num, 0) + 1);
+        }
+        
+        return numMap.size() <= nums.length / 2 ? numMap.size() : nums.length / 2 ;
     }
 }
