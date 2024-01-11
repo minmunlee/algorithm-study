338b0758e8406cad72007c7bfa5660c50da5e9b7
ChaeAg <dydkglel123@gmail.com>
2024-01-11 16:26:29 +0900

    lee: solve boj_2016년

--- January/2주차/prgrms_2016년/Solution.java
+++ January/2주차/prgrms_2016년/Solution.java
@@ -1,6 +1,12 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
-        return answer;
+        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        
+        for(int i=0; i<a-1; i++) {
+            b += date[i];
+        }
+        
+        return days[b % 7];
     }
 }
