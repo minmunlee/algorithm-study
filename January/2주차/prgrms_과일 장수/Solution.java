3f0fa281aa4110a52ee56c214fdc078ea0305d6b
sejyom <akxxkd01@gmail.com>
2024-01-11 10:50:48 +0900

    solve: prgrms_과일 장수

--- January/2주차/prgrms_과일 장수/Solution.java
+++ January/2주차/prgrms_과일 장수/Solution.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
-    public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        return answer;
+    public static int solution(int k, int m, int[] score) {
+        score = Arrays.stream(score)
+                .boxed()
+                .sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        int res = 0;
+        int pointer = m - 1;
+
+        while (pointer < score.length) {
+            res += score[pointer] * m;
+            pointer += m;
+        }
+
+        return res;
     }
 }
