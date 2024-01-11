d8caf04853396381684c3b142d2cf39f3306a0ab
ChaeAg <dydkglel123@gmail.com>
2024-01-11 16:26:46 +0900

    lee: solve prgrms_과일 장수

--- January/2주차/prgrms_과일 장수/Solution.java
+++ January/2주차/prgrms_과일 장수/Solution.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+        score = Arrays.stream(score)
+            .boxed()
+            .sorted(Comparator.reverseOrder())
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
+        for(int i=0; i<score.length-(score.length % m); i++) {
+            if(i % m == (m-1)) {
+                answer += score[i] * m;
+            }
+        }
+        
         return answer;
     }
 }
