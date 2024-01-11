e801ce9b610fdcab527c4e6ca6431ec9aafab3eb
siwon <muncool39@gmail.com>
2024-01-11 17:06:18 +0900

    solve: prgrms_2016년

--- January/2주차/prgrms_2016년/Solution.java
+++ January/2주차/prgrms_2016년/Solution.java
@@ -1,6 +1,10 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
-        return answer;
+        String[] day = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+        LocalDate date = LocalDate.of(2016, a, b);
+        int dayIndex = date.getDayOfWeek().getValue()-1;
+        return day[dayIndex];
     }
 }
