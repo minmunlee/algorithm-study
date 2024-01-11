dca66f4da4a2f2bb491af8179aecb5cf7db8d166
ChaeAg <dydkglel123@gmail.com>
2024-01-11 16:25:58 +0900

    lee: solve boj_문자열 암호화

--- January/2주차/boj_문자열 암호화/Main.java
+++ January/2주차/boj_문자열 암호화/Main.java
@@ -1,3 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            int N = Integer.parseInt(br.readLine());
+            if(N == 0) break;
+            String[] arr = br.readLine().replace(" ", "").split("");
+            String[] result = new String[arr.length];
+            int startIdx = 0;
+            int idx = startIdx;
+            int arrIdx = 0;
+            while(arrIdx < arr.length) {
+                result[idx] = arr[arrIdx++];
+                if(idx + N > arr.length - 1) {
+                    startIdx++;
+                    idx = startIdx;
+                }
+                else {
+                    idx += N;
+                }
+            }
+            sb.append(String.join("", result).toUpperCase()).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
