0edd5098312b3fa811c4213c2888dc1643a7e87a
sejyom <akxxkd01@gmail.com>
2024-01-11 10:50:36 +0900

    solve: prgrms_2016년

--- January/2주차/prgrms_2016년/Solution.java
+++ January/2주차/prgrms_2016년/Solution.java
@@ -1,6 +1,12 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public String solution(int a, int b) {
-        String answer = "";
-        return answer;
+    public static String solution(int a, int b) {
+        LocalDate localDate = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        return dayOfWeek.toString().substring(0, 3);
     }
 }
