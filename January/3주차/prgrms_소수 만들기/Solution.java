161c172192eec53255fa425b8f45340606f83d21
siwon <muncool39@gmail.com>
2024-01-17 15:05:26 +0900

    solve: prgrms_소수 만들기

--- January/3주차/prgrms_소수 만들기/Solution.java
+++ January/3주차/prgrms_소수 만들기/Solution.java
@@ -1,10 +1,29 @@
 class Solution {
+    int count = 0;
+    int[] numbers;
+    boolean[] check = new boolean[50000];
     public int solution(int[] nums) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        numbers = nums;
+        check();
+        dfs(0, 0, 1);
+        return count;
+    }
+    public void check(){
+        for(int i=2;i<50000;i++){
+            if(!check[i]){
+                for(int j=i+i;j<50000;j+=i){
+                    check[j] = true;
+                }
+            }
+        }
+    }
+    public void dfs(int start, int sum, int deep){
+        if(deep>3){
+            if(!check[sum]) count++;
+            return;
+        }
+        for(int idx=start;idx<numbers.length;idx++){
+            dfs(idx+1, sum+numbers[idx], deep+1);
+        }
     }
 }
