9dd523824365331f63810e35f05d88537ea4025a
siwon <muncool39@gmail.com>
2024-01-17 15:05:19 +0900

    solve: prgrms_덧칠하기

--- January/3주차/prgrms_덧칠하기/Solution.java
+++ January/3주차/prgrms_덧칠하기/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        int end = 0;
+        for(int i=0;i<section.length;i++){
+            if(section[i]>end){
+                count++;
+                end=section[i]+m-1;
+            }
+        }
+        return count;
     }
 }
