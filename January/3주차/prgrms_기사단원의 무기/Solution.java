d64fc4ee9461724c59a8cdd70cfc7d30cb8cacdb
siwon <muncool39@gmail.com>
2024-01-17 15:05:12 +0900

    solve: prgrms_기사단원의 무기

--- January/3주차/prgrms_기사단원의 무기/Solution.java
+++ January/3주차/prgrms_기사단원의 무기/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        return answer;
+        int sum = 1;
+        int[] yackcount = new int[number+1];
+        for(int i=2;i<=number;i++){
+            yackcount[i]++;
+            for(int j=i;j<=number;j+=i){
+                yackcount[j]++;
+            }
+            sum+=(yackcount[i]>limit)?power:yackcount[i];
+        }
+        return sum;
     }
 }
