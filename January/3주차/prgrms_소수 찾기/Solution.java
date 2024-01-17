493d8cd1876a7b1c1ca29ac22083f02ecd1db8af
siwon <muncool39@gmail.com>
2024-01-17 15:05:34 +0900

    solve: prgrms_소수 찾기

--- January/3주차/prgrms_소수 찾기/Solution.java
+++ January/3주차/prgrms_소수 찾기/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        boolean[] notsosu = new boolean[n+1];
+        notsosu[1]=true;
+        for(int i=2;i<=n;i++){
+            if(notsosu[i]==false){
+                count++;
+                for(int j=i;j<=n;j=j+i){
+                    notsosu[j]=true;
+                }
+            }
+        }
+        return count;
     }
 }
