3f8df6281953f0fc53702d51aee032e185467ffa
gitseoyeon <tjdus553926@gmail.com>
2024-01-18 23:58:09 +0900

    solve: boj_지뢰찾기

--- January/3주차/boj_지뢰찾기/Main.java
+++ January/3주차/boj_지뢰찾기/Main.java
@@ -1,3 +1,54 @@
-public class Main {
-    
-}
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.StringTokenizer;
+
+    public class Main {
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer str;
+
+            while(true){
+                str = new StringTokenizer(br.readLine());
+                int R = Integer.parseInt(str.nextToken());
+                int C = Integer.parseInt(str.nextToken());
+                if(R == 0 && C == 0){
+                    System.exit(1);
+                }
+                String[][] map = new String[R][C];
+        
+                for(int i = 0; i < R; i++){
+                    String s = br.readLine();
+                    for(int j = 0; j < C; j++){
+                        map[i][j] = String.valueOf(s.charAt(j));
+                    }
+                }
+        
+                
+                for(int i = 0; i < R; i++){
+                    for(int j = 0; j < C; j++){
+                    if(map[i][j].equals(".")){
+                            int cnt = 0;
+                            // 현재 위치에서 근처 값들을 보고 *인지 판별. 단 근처 값들의 위치가 -이면 안됨
+                            for (int dx = -1; dx <= 1; dx++) {
+                                for (int dy = -1; dy <= 1; dy++) {
+                                    int ni = i + dx;
+                                    int nj = j + dy;
+                                    if (ni >= 0 && ni < R && nj >= 0 && nj < C && map[ni][nj].equals("*")) {
+                                        cnt++;
+                                    }
+                                }
+                            }
+                            map[i][j] = Integer.toString(cnt);
+                    } 
+                    }
+                }
+                for(int i = 0; i < R; i++){
+                    for(int j = 0; j < C; j++){
+                        System.out.print(map[i][j]);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
