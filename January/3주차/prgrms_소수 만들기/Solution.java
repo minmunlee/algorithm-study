71e2b8eaabbc6f829a3179016b64bb535ed2e927
gitseoyeon <tjdus553926@gmail.com>
2024-01-18 23:59:17 +0900

    solve: prgrms_소수만들기

--- January/3주차/prgrms_소수 만들기/Solution.java
+++ January/3주차/prgrms_소수 만들기/Solution.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        int answer = 0;
+        List<Integer> sumList = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    sumList.add(nums[i] + nums[j] + nums[k]);
+                }
+            }
+        }
+         
+        
+        for(int i = 0; i < sumList.size(); i++){
+            int cnt = 0;
+            int num = sumList.get(i);
+            for(int j = 2; j < num; j++){
+                if(sumList.get(i) % j == 0){
+                    cnt++;  
+                }   
+            }
+            if(cnt == 0){
+                answer += 1;
+            }
+        }
         return answer;
     }
 }
