770972e29289e3d7ede4e78b63fcc9f80d9f8003
gitseoyeon <tjdus553926@gmail.com>
2024-01-18 23:59:00 +0900

    solve: prgrms_덧칠하기

--- January/3주차/prgrms_덧칠하기/Solution.java
+++ January/3주차/prgrms_덧칠하기/Solution.java
@@ -1,6 +1,32 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        Boolean[] wall = new Boolean[n + 1];
+        
+        for(int i = 0; i <= n; i++){
+            wall[i] = true;
+        }
+        
+        for(int i : section){
+            wall[i] = false;
+        }
+        
+        for(int i = 1; i < n - m + 2; i++){
+            if(wall[i] == false){
+                for(int j = i; j < i + m; j++){
+                    wall[j] = true;
+                }
+                answer++;
+            }
+        }
+        
+        for(int i = n - m + 2; i <= n; i++){
+            if(wall[i] == false){
+                answer++;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
