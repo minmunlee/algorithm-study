815b801189266af5a6aab579292ae5e2e22cff72
gitseoyeon <tjdus553926@gmail.com>
2024-01-18 23:59:44 +0900

    solve: prgrms_소수찾기

--- January/3주차/prgrms_소수 찾기/Solution.java
+++ January/3주차/prgrms_소수 찾기/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
+        for(int i = 3; i <= n; i += 2){
+            boolean isPrime = true;
+            int sqrt = (int) Math.sqrt(i);
+            for(int j = 2; j <= sqrt; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                answer++;
+            }
+        }
         return answer;
     }
 }
