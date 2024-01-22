176037f99ed9058184c6fde9fa9e09cbaebcf254
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:52:11 +0900

    solve: prgrms_실패율

--- January/3주차/prgrms_실패율/Solution.java
+++ January/3주차/prgrms_실패율/Solution.java
@@ -1,6 +1,33 @@
+import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+        int[] answer = new int[N];
+        Map<Integer, Float> failRate = new HashMap<>();
+        for(int i=1; i<=N; i++) {
+            float same = 0.0f;
+            float dodal = 0.0f;
+            for(int j=0; j<stages.length; j++) {
+                if(stages[j] == i) {
+                    dodal++;
+                    same++;
+                }
+                else if(stages[j] > i) dodal++;
+            }
+            if(same != 0 || dodal != 0) failRate.put(i, same/dodal);
+            else failRate.put(i, 0.0f);
+        }
+        List<Integer> keys = new ArrayList<>(failRate.keySet());
+		Collections.sort(keys, (v1, v2) -> (failRate.get(v2).compareTo(failRate.get(v1))));
+        int idx = 0;
+        for(int key : keys) {
+            answer[idx++] = key;
+        }
+        for(int i=1; i<=N; i++) {
+            if(!keys.contains(i)) {
+                answer[idx++] = i;
+            }
+        }
+        
         return answer;
     }
 }
