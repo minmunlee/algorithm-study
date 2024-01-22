3e134391b8a156946fa8eaad01cd3b3e2b683e2f
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:51:58 +0900

    solve: prgrms_소수 만들기

--- January/3주차/prgrms_소수 만들기/Solution.java
+++ January/3주차/prgrms_소수 만들기/Solution.java
@@ -1,9 +1,22 @@
 class Solution {
     public int solution(int[] nums) {
-        int answer = -1;
+        int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        for(int i=0; i<nums.length-2; i++) {
+            for(int j=i+1; j<nums.length-1; j++) {
+                for(int k=j+1; k<nums.length; k++) {
+                    int sum = nums[i] +nums[j] + nums[k];
+                    int count = 0;
+                    for(int a=2; a<=sum/2; a++) {
+                        if(sum % a == 0) {
+                            count++;
+                            break;
+                        }
+                    }
+                    if(count == 0) answer++;
+                }
+            }
+        }
 
         return answer;
     }
