7462794e4c4aabd5967255c926d56f3134d40c28
ChaeAg <dydkglel123@gmail.com>
2024-01-23 00:46:04 +0900

    solve: boj_로프

--- January/3주차/boj_로프/Main.java
+++ January/3주차/boj_로프/Main.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] rope = new int[N];
+        for(int i=0; i<N; i++) {
+            rope[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(rope); // 오름차순 정렬
+        int maxWeight = 0;
+        for(int i=N-1; i>=0; i--) {
+            if(maxWeight < rope[i] * (N-i)) {
+                maxWeight = rope[i] * (N-i);
+            }
+        }
+        System.out.print(maxWeight);
+    }
 }
