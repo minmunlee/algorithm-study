ddf0fd1f9fa7b0ea1bc2d2df8efe53e6e8591c1b
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:51:50 +0900

    solve: prgrms_덧칠하기

--- January/3주차/prgrms_덧칠하기/Solution.java
+++ January/3주차/prgrms_덧칠하기/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        int lastIdx = 0;
+        for(int i=0; i<section.length; i++) {
+            if(section[i] < lastIdx) continue;
+            answer++;
+            lastIdx = section[i] + m;
+        }
+        
         return answer;
     }
 }
