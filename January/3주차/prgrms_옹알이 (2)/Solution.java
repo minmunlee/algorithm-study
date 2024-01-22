adb6af28929a584a1b4f302a0380ec36106b472e
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:52:21 +0900

    solve: prgrms_옹알이 (2)

--- January/3주차/prgrms_옹알이 (2)/Solution.java
+++ January/3주차/prgrms_옹알이 (2)/Solution.java
@@ -1,6 +1,20 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        
+        for(String word : babbling) {
+            int len = word.length();
+            word = word.replaceAll("ayaaya", "?")
+                .replaceAll("yeye", "?")
+                .replaceAll("woowoo", "?")
+                .replaceAll("mama", "?"); 
+            word = word.replaceAll("aya", "---")
+                .replaceAll("ye", "--")
+                .replaceAll("woo", "---")
+                .replaceAll("ma", "--");
+            if(!word.contains("?") && word.equals("-".repeat(len))) answer++;
+        }
+        
         return answer;
     }
 }
