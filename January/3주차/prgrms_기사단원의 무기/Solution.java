53c17d41b414744c874b32653be188924ddbddc9
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:51:41 +0900

    solve: prgrms_기사단원의 무기

--- January/3주차/prgrms_기사단원의 무기/Solution.java
+++ January/3주차/prgrms_기사단원의 무기/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        
+        for(int i=1; i<=number; i++) {
+            int divisorCount = 0;
+            for(int j=1; j*j<=i; j++) {
+                if(j * j == i) divisorCount++;
+                else if(i % j == 0) divisorCount += 2;
+            }
+            if(divisorCount > limit) answer += power;
+            else answer += divisorCount;
+        }
+        
         return answer;
     }
 }
