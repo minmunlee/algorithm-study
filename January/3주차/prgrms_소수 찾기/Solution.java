e7684c1a2d7ff0c8ce53f0bdfb20f35f01743fe8
ChaeAg <dydkglel123@gmail.com>
2024-01-23 05:52:05 +0900

    solve: prgrms_소수 찾기

--- January/3주차/prgrms_소수 찾기/Solution.java
+++ January/3주차/prgrms_소수 찾기/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
+        for(int i=2; i<=n; i++) {
+            int count = 0;
+            for(int j=2; j*j<=i; j++) {
+                if(i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if(count == 0) answer++;
+        }
+        
         return answer;
     }
 }
