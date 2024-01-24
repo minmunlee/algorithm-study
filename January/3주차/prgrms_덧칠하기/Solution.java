98cc9e90f2327fc57804edbc055cd68ce25f5448
gitseoyeon <tjdus553926@gmail.com>
2024-01-25 02:47:38 +0900

    add: prgrms_덧칠하기 주석 추가

--- January/3주차/prgrms_덧칠하기/Solution.java
+++ January/3주차/prgrms_덧칠하기/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        Boolean[] wall = new Boolean[n + 1];
+        Boolean[] wall = new Boolean[n + 1]; // 처음 위치를 1로 두기 위해서
         
         for(int i = 0; i <= n; i++){
             wall[i] = true;
@@ -13,7 +13,7 @@ class Solution {
         
         for(int i = 1; i < n - m + 2; i++){
             if(wall[i] == false){
-                for(int j = i; j < i + m; j++){
+                for(int j = i; j < i + m; j++){ // 색칠 안한 구역부터 롤러의 길이 만큼 색칠
                     wall[j] = true;
                 }
                 answer++;
