38d0f6b9b62d37a750cf51400c9bba80588f1501
siwon <muncool39@gmail.com>
2024-01-24 22:07:08 +0900

    solve: prgrms_옹알이 (2)

--- January/3주차/prgrms_옹알이 (2)/Solution.java
+++ January/3주차/prgrms_옹알이 (2)/Solution.java
@@ -1,6 +1,31 @@
 class Solution {
-    public int solution(String[] babbling) {
-        int answer = 0;
-        return answer;
+    String[] Pronunciation = {"aya", "ye", "woo", "ma"};
+    public int solution(String[] babbling) { 
+        int count = 0;
+        for(String b : babbling){
+            if(check(b)) count++;
+        }
+        return count;
+    }
+    public boolean check(String babblingWord){
+        for(String p : Pronunciation){
+            babblingWord = deletePronunciation(p, babblingWord);
+            if(babblingWord.replace(" ","").length()==0) return true;
+        }
+        return false;
+    }
+    
+    public String deletePronunciation(String pronunciation, String word) {
+        int startIndex = 0;
+        int size = pronunciation.length();
+        while(startIndex+size<=word.length()){
+            String subString = word.substring(startIndex, startIndex+size);
+            if(subString.equals(pronunciation)){
+                word = word.replaceFirst(pronunciation, " ");
+                startIndex++;
+            }
+            startIndex++;
+        }
+        return word;
     }
 }
