eacf500e256da8405df0aaf76725bdc5ce4bb099
gitseoyeon <tjdus553926@gmail.com>
2024-02-01 00:17:15 +0900

    solve: boj_토너먼트

--- February/1주차/boj_토너먼트/Main.java
+++ February/1주차/boj_토너먼트/Main.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(str.nextToken());
+        int jimin = Integer.parseInt(str.nextToken());
+        int hansu = Integer.parseInt(str.nextToken());
+        int round = 0;
+
+        while(jimin != hansu){
+            jimin -= jimin / 2;
+            hansu -= hansu / 2;
+            round++;
+        }
+
+        System.out.println(round);
+    }
     
 }
