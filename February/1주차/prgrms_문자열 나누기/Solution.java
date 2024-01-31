4adcd5f75124e55d17e23d5dcdbd4f58fdaedd88
gitseoyeon <tjdus553926@gmail.com>
2024-02-01 00:46:25 +0900

    solve: prgrms_문자열 나누기

--- February/1주차/prgrms_문자열 나누기/Solution.java
+++ February/1주차/prgrms_문자열 나누기/Solution.java
@@ -1,6 +1,28 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
+        int answer = 1;
+        int first_word_cnt = 1; // 첫번째 문자 같은 개수
+        int other_word_cnt = 0; // 다른 문자 개수
+        int fIdx = 0; // 첫번째 문자 위치
+        int lIdx = 1; // 다른 문자 위치
+        
+        while(lIdx < s.length()){
+            if(first_word_cnt == other_word_cnt){ // 첫번째 문자 개수와 다른 문자 개수 같을 경우
+                fIdx = lIdx;
+                lIdx += 1;
+                first_word_cnt = 1;
+                other_word_cnt = 0;
+                answer++;
+            }else{
+                if(s.charAt(fIdx) == s.charAt(lIdx)){ // 첫번째 문자와 다른 위치의 문자가 같을 경우
+                    first_word_cnt += 1;
+                }else{
+                    other_word_cnt += 1;
+                }
+                lIdx++;
+            }   
+        }
+               
         return answer;
     }
 }
