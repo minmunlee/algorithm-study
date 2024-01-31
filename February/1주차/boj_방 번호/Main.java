b6fc2fd2294fd581e211e6a730b9b2d789055cba
gitseoyeon <tjdus553926@gmail.com>
2024-02-01 00:16:24 +0900

    solve: boj_방 번호

--- February/1주차/boj_방 번호/Main.java
+++ February/1주차/boj_방 번호/Main.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String[] n = str.replaceAll("9", "6").split(""); // 9를 6으로 대체
+
+        int[] arr = new int[9]; // 등장 횟수 저장
+        for (String s : n) 
+            arr[Integer.parseInt(s)]++;
+
+        if (arr[6] % 2 != 0){ // 6의 횟수가 홀수 일 경우
+            arr[6] = arr[6] / 2 + 1; // 한세트가 더 필요함
+        } else{
+            arr[6] /= 2;
+
+        } 
+        Arrays.sort(arr);
+        System.out.print(arr[8]);
+    }
 }
