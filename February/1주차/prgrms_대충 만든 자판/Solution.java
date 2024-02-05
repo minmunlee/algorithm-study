ad40dc5d81e6ad4ece9c9a345723c761d7373699
ChaeAg <dydkglel123@gmail.com>
2024-02-05 18:14:44 +0900

    solve: prgrms_대충 만든 자판

--- February/1주차/prgrms_대충 만든 자판/Solution.java
+++ February/1주차/prgrms_대충 만든 자판/Solution.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = {};
+        int[] answer = new int[targets.length];
+        
+        for(int i=0; i<targets.length; i++) {
+            String[] word = targets[i].split("");
+            int count = 0;
+            for(String w : word) {
+                int minCount = 101;
+                for(String key : keymap) {
+                    if(key.contains(w) && key.indexOf(w) + 1 < minCount) {
+                        minCount = key.indexOf(w) + 1;
+                    }
+                }
+                if(minCount == 101) {
+                    answer[i] = -1;
+                    break;
+                }
+                answer[i] += minCount;
+            }
+        }
+        
         return answer;
     }
 }
