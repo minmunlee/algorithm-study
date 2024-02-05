cd482173fcd73c553d842f40420d866241f3e7d3
ChaeAg <dydkglel123@gmail.com>
2024-02-05 18:14:58 +0900

    solve: prgrms_문자열 나누기

--- February/1주차/prgrms_문자열 나누기/Solution.java
+++ February/1주차/prgrms_문자열 나누기/Solution.java
@@ -1,6 +1,24 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        return answer;
+        int sameCount = 0;
+        int differentCount = 0;
+        char first = s.charAt(0);
+        
+        for(int i=0; i<s.length(); i++) {
+            if(sameCount == 0 && differentCount == 0) {
+                first = s.charAt(i);
+            }
+            if(s.charAt(i) == first) sameCount++;
+            else differentCount++;
+            
+            if(sameCount == differentCount) {
+                answer++;
+                sameCount = 0;
+                differentCount = 0;
+            }
+        }
+        
+        return sameCount == 0 ? answer : answer + 1;
     }
 }
