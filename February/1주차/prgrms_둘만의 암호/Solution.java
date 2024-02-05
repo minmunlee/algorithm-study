5009698335b41acabaf098db4e5c0bfc1d274305
ChaeAg <dydkglel123@gmail.com>
2024-02-05 18:14:51 +0900

    solve: prgrms_둘만의 암호

--- February/1주차/prgrms_둘만의 암호/Solution.java
+++ February/1주차/prgrms_둘만의 암호/Solution.java
@@ -1,6 +1,27 @@
+import java.util.*;
+import java.util.stream.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
+        char[] sWord = s.toCharArray();
+        List<String> skipWord = Arrays.stream(skip.split(""))
+            .collect(Collectors.toList());
+        
+        for(char word : sWord) {
+            int count = 0;
+            while(count < index) {
+                count++;
+                word++;
+                if(word > 'z') { // z를 넘어간 경우
+                    word = 'a';
+                }
+                if(skipWord.contains(String.valueOf(word))) { // skip에 있는 알파벳인 경우
+                    count--;
+                }
+            }
+            answer += word;
+        }
+        
         return answer;
     }
 }
