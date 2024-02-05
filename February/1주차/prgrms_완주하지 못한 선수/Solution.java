e4d07a788aa8e6509ced443a9bdc3f3696d61fa2
ChaeAg <dydkglel123@gmail.com>
2024-02-05 18:15:28 +0900

    solve: prgrms_완주하지 못한 선수

--- February/1주차/prgrms_완주하지 못한 선수/Solution.java
+++ February/1주차/prgrms_완주하지 못한 선수/Solution.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        Map<String, Integer> nameMap = new HashMap();
+        
+        for(String name : participant) {
+            nameMap.put(name, nameMap.getOrDefault(name, 0) + 1);
+        }
+        
+        for(String name : completion) {
+            nameMap.put(name, nameMap.get(name) - 1);
+        }
+        
+        for(String name : nameMap.keySet()) {
+            if(nameMap.get(name) != 0) answer = name;
+        }
+        
         return answer;
     }
 }
