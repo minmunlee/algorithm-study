8acea49243c0427cf65bf6e8d6566777db1b1d83
siwon <muncool39@gmail.com>
2024-02-06 23:09:11 +0900

    solve: prgrms_문자열 나누기

--- February/1주차/prgrms_문자열 나누기/Solution.java
+++ February/1주차/prgrms_문자열 나누기/Solution.java
@@ -1,6 +1,25 @@
 class Solution {
+    String[] list;
     public int solution(String s) {
-        int answer = 0;
-        return answer;
+        list = s.split("");
+        int count = 0;
+        int subIndex = 0;
+        while(subIndex < list.length) {
+            subIndex = cutting(subIndex) + 1;
+            count++;
+        }
+        return count;
+    }
+    
+    private int cutting(int startIndex) {
+        String x = list[startIndex];
+        int xCount = 1;
+        int oCount = 0;
+        for(int i=startIndex+1;i<list.length;i++) {
+            if(list[i].equals(x)) xCount++;
+            else oCount++;
+            if(xCount == oCount) return i;
+        }
+        return list.length;
     }
 }
