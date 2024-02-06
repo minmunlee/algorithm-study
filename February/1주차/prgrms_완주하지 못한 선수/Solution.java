fbaaae5402fb6246970b4aaf2e16aa2e483da5bc
gitseoyeon <tjdus553926@gmail.com>
2024-02-07 02:07:33 +0900

    solve: prgrms_완주하지  못한 선수

--- February/1주차/prgrms_완주하지 못한 선수/Solution.java
+++ February/1주차/prgrms_완주하지 못한 선수/Solution.java
@@ -1,6 +1,23 @@
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(String s : participant){
+            map.put(s, map.getOrDefault(s, 0) + 1); // 참가 선수 정보 넣기
+        }
+        
+        for(String s : completion){
+            map.put(s, map.get(s) - 1); // 완주 한 선수는 value 값을 지우기
+        }
+        
+        for(String s : map.keySet()){
+            if(map.get(s) != 0){ // 완주 하지 못한 선수
+                answer += s;
+            }
+        }
         return answer;
     }
 }
