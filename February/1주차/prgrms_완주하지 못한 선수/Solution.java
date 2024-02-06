dd92c2ea023688270011ac116d8d75960bfbc95b
sejyom <akxxkd01@gmail.com>
2024-02-06 13:27:36 +0900

    solve: prgrms_완주하지 못한 선수

--- February/1주차/prgrms_완주하지 못한 선수/Solution.java
+++ February/1주차/prgrms_완주하지 못한 선수/Solution.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        return answer;
+        List<String> p = Arrays.asList(participant)
+                .stream().sorted().collect(Collectors.toList());;
+        List<String> c = Arrays.asList(completion)
+                .stream().sorted().collect(Collectors.toList());
+
+        for (int i = 0; i < c.size(); i++)
+            if (!(p.get(i).equals(c.get(i))))
+                return p.get(i);
+
+        return p.get(p.size() - 1);
     }
 }
