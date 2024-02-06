b1cac2c362137b6672e4abec3bf7649a226b47e0
siwon <muncool39@gmail.com>
2024-02-06 23:09:39 +0900

    solve: prgrms_체육복

--- February/1주차/prgrms_체육복/Solution.java
+++ February/1주차/prgrms_체육복/Solution.java
@@ -1,6 +1,38 @@
 class Solution {
+    boolean[] lostClothes;
+    boolean[] reserveClothes;
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        lostClothes = new boolean[n];
+        lostClothesCheck(lost);
+        reserveClothes = new boolean[n];
+        reserveClothesCheck(reserve);
+        for(int i=0;i<n;i++){
+            if(!reserveClothes[i]) continue;
+            if(i>0 && lostClothes[i-1]) {
+                lostClothes[i-1] = false;
+                continue;
+            }
+            if(i < n - 1 && lostClothes[i+1]) lostClothes[i+1] = false;
+        }
+        int answer = n;
+        for(boolean l : lostClothes) {
+            if(l) answer--;
+        }
         return answer;
+    }
+    
+    private void lostClothesCheck(int[] lost) {
+        for(int l : lost) {
+            lostClothes[l-1] = true;
+        }
+    }
+    private void reserveClothesCheck(int[] reserve) {
+        for(int r : reserve) {
+            if(lostClothes[r-1]) {
+                lostClothes[r-1] = false;
+                continue;
+            }
+            reserveClothes[r-1] = true;
+        }
     }
 }
