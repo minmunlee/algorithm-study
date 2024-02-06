2b66fb73788afbadc9f5e3cda62030831f043b5c
siwon <muncool39@gmail.com>
2024-02-06 23:08:44 +0900

    solve: prgrms_[PCCE 기출문제] 9번 이웃한 칸

--- February/1주차/prgrms_[PCCE 기출문제] 9번 이웃한 칸/Solution.java
+++ February/1주차/prgrms_[PCCE 기출문제] 9번 이웃한 칸/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
+    int[] dh = {0, 1, -1, 0};
+    int[] dw = {1, 0, 0, -1};
     public int solution(String[][] board, int h, int w) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        int size = board.length;
+        String color = board[h][w];
+        for(int i=0;i<4;i++) {
+            int nextH = h + dh[i];
+            int nextW = w + dw[i];
+            if(nextH >= size || nextW >=size || nextH<0 || nextW<0) continue;
+            if(board[nextH][nextW].equals(color)) count++;
+        }
+        return count;
     }
 }
