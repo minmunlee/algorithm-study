bd9e7f39d1f17ad6ed5a945da24605d9c03b62a0
siwon <muncool39@gmail.com>
2024-02-06 23:09:30 +0900

    solve: prgrms_완주하지 못한 선수

--- February/1주차/prgrms_완주하지 못한 선수/Solution.java
+++ February/1주차/prgrms_완주하지 못한 선수/Solution.java
@@ -1,6 +1,21 @@
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String p : participant) {
+            int count = map.getOrDefault(p, 0);
+            map.put(p, ++count);
+        }
+        for(String c : completion) {
+            int count = map.get(c);
+            map.replace(c, --count);
+            if(count==0) map.remove(c);
+        }
         String answer = "";
+        Set<String> keySet = map.keySet();
+        for(String key : keySet) {
+            answer = key;
+        }
         return answer;
     }
 }
