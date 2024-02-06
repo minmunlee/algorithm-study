fb94f37ac6ad37f606fff93913c1194f95aaaf93
gitseoyeon <tjdus553926@gmail.com>
2024-02-07 02:06:47 +0900

    solve: prgrms_대충 만든 자판

--- February/1주차/prgrms_대충 만든 자판/Solution.java
+++ February/1주차/prgrms_대충 만든 자판/Solution.java
@@ -4,23 +4,21 @@ public class Solution {
         int[] answer = new int[targets.length];
 
         for (int i = 0; i < targets.length; i++) {
-            int times = 0; // 현재 단어에 대한 누른 키의 총 횟수를 초기화
+            int times = 0; // 현재 단어에 대한 누른 키의 총 횟수
 
             for (char ch : targets[i].toCharArray()) {
                 boolean flag = false; // 문자를 찾을 수 있는지 여부
-                int time = 101; // keymap의 원소의 길이가 최대 100이기 때문에 101로 초기화
+                int time = 101; // 타겟 문자열의 문자 하나당 찾는 횟수
 
-                // keymap에 있는 모든 원소를 반복하면서 가장 적게 누를 수 있는 char를 찾음
                 for (String key : keymap) {
-                    if (key.indexOf(ch) != -1) { // key안에 char가 존재하면
-                        time = Math.min(key.indexOf(ch) + 1, time);// 현재 문자를 찾은 경우, 해당 문자가 나오는 위치를 통해 누른 키의 횟수를 갱신
-                        flag = true; // 문자를 찾을 수 있음을 표시
+                    if (key.indexOf(ch) != -1) {
+                        time = Math.min(key.indexOf(ch) + 1, time);// 문자를 찾은 경우, 해당 문자가 나오는 위치를 통해 누른 키의 횟수를 갱신
+                        flag = true;
                     }
                 }
 
-                // 문자를 찾지 못한 경우
                 if (!flag) {
-                    times = -1; // 현재 단어에 대한 누른 키를 계산할 수 없음을 나타냄
+                    times = -1;
                     break;
                 }
 
