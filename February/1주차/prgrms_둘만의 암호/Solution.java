ceb2a821b19b3708aab501488d38e4b92f91c75c
gitseoyeon <tjdus553926@gmail.com>
2024-02-07 02:07:03 +0900

    solve: prgrms_둘만의 암호

--- February/1주차/prgrms_둘만의 암호/Solution.java
+++ February/1주차/prgrms_둘만의 암호/Solution.java
@@ -1,6 +1,19 @@
-class Solution {
-    public String solution(String s, String skip, int index) {
-        String answer = "";
-        return answer;
+public class Solution {
+    public static String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // skip 문자열에 포함된 문자들을 알파벳 문자열에서 제외
+        for (char sk : skip.toCharArray()) {
+            alphabet = alphabet.replace(Character.toString(sk), "");
+        }
+
+        for (char a : s.toCharArray()) {
+            // 각 문자를 주어진 index만큼 오른쪽으로 이동시켜 새로운 문자열을 생성. 알파벳 배열 넘어 갈 경우 길이 나눠서 나머지 값 넣음
+            int idx = (alphabet.indexOf(a) + index) % alphabet.length(); 
+            answer.append(alphabet.charAt(idx));
+        }
+
+        return answer.toString();
     }
 }
