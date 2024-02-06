69034f5548c03146ad7d19d445832e3c28f161b5
gitseoyeon <tjdus553926@gmail.com>
2024-02-07 02:06:31 +0900

    solve: prgrms_9번 이웃한 칸

--- February/1주차/prgrms_[PCCE 기출문제] 9번 이웃한 칸/Solution.java
+++ February/1주차/prgrms_[PCCE 기출문제] 9번 이웃한 칸/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
-        int answer = 0;
-        return answer;
+        int n = board.length;
+        int count = 0;
+        int[][] direc = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        
+        for(int[] d : direc){
+            int h_check = h + d[0];
+            int w_check = w + d[1];
+            
+            if(h_check >= 0 && h_check < n && w_check >= 0 && w_check < n){
+                if(board[h][w].equals(board[h_check][w_check])){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
