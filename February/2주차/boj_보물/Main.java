373dc18b98bdcdd9d80c90a5b5aa350df6e00820
ChaeAg <dydkglel123@gmail.com>
2024-02-13 14:30:02 +0900

    solve: boj_보물

--- February/2주차/boj_보물/Main.java
+++ February/2주차/boj_보물/Main.java
@@ -1,3 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] B = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int result = S(N, A, B);
+        System.out.print(result);
+    }
+
+    private static int S(int N, int[] A, int[] B) {
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[N - i - 1];
+        }
+
+        return sum;
+    }
 }
