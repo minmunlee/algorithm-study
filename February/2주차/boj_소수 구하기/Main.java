daf9a04af06d441403cbaec1f6cb17e5a6d8bf84
ChaeAg <dydkglel123@gmail.com>
2024-02-13 14:30:11 +0900

    solve: boj_소수 구하기

--- February/2주차/boj_소수 구하기/Main.java
+++ February/2주차/boj_소수 구하기/Main.java
@@ -1,3 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] inputs = br.readLine().split(" ");
+        int M = Integer.parseInt(inputs[0]);
+        int N = Integer.parseInt(inputs[1]);
+
+        for(int i=M; i<=N; i++) {
+            if(i == 1) continue;
+            if(isSosu(i))
+                sb.append(i).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    public static boolean isSosu(int num) {
+        for(int i=2; i*i<=num; i++) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
 }
