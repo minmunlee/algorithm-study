5878191687d4b8832a88eb8240d4e2388b868598
sejyom <akxxkd01@gmail.com>
2024-02-15 05:09:03 +0900

    solve: boj_베스트셀러

--- February/2주차/boj_베스트셀러/Main.java
+++ February/2주차/boj_베스트셀러/Main.java
@@ -1,25 +1,20 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Map<String, Integer> hm = new HashMap<>();
-        String[] key = new String[n];
         int max = 0;
         ArrayList<String> res = new ArrayList<>();
-        String[] s;
+        String str;
 
-        for (int i = 0; i < n; i++)
-            key[i] = br.readLine();
-
-        for (String str : key) {
-            if (hm.containsKey(str))
-                hm.put(str, hm.get(str)+1);
-            else
-                hm.put(str, 1);
+        for (int i = 0; i < n; i++) {
+            str = br.readLine();
+            hm.put(str, hm.getOrDefault(str, 0) + 1);
         }
 
         for (Integer i : hm.values()) {
@@ -33,7 +28,7 @@ public class Main {
         }
 
         Collections.sort(res);
-        System.out.println(res.get(0));
+        System.out.println(res.getFirst());
 
         br.close();
     }
