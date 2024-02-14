08c6c6eb8d7510ecd77fc3c0da0deee532872b1f
siwon <muncool39@gmail.com>
2024-02-14 22:08:11 +0900

    solve: boj_보물

--- February/2주차/boj_보물/Main.java
+++ February/2주차/boj_보물/Main.java
@@ -1,3 +1,17 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    int[] A = Arrays.stream(br.readLine().split(" "))
+	                    .mapToInt(Integer::parseInt).sorted().toArray();
+	    int[] B = Arrays.stream(br.readLine().split(" "))
+	                    .mapToInt(Integer::parseInt).sorted().toArray();
+	    long S = 0;
+	    for(int i=0;i<N;i++){
+	        S+= (A[i]*B[N-i-1]);
+	    }
+	    System.out.print(S);
+	}
 }
