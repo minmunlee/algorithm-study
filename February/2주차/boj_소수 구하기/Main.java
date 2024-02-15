86184cf48c723a5c1856af7798a9b09f36285b10
gitseoyeon <tjdus553926@gmail.com>
2024-02-15 16:29:14 +0900

    solve: boj_소수 구하기

--- February/2주차/boj_소수 구하기/Main.java
+++ February/2주차/boj_소수 구하기/Main.java
@@ -1,3 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str= new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(str.nextToken());
+        int N = Integer.parseInt(str.nextToken());
+
+        for(int i = M; i <= N; i++){
+            is_prime(i);
+        }
+
+    }
+
+    // 소수 구하는 메서드
+    public static void is_prime(int num){
+        if(num < 2){
+            return;
+        }else if(num == 2){
+            System.out.println(num);
+            return;
+        }else{
+            for(int i = 2; i < Math.sqrt(num); i++){
+                if(num % i == 0){
+                    return;
+                }
+            } 
+            System.out.println(num);
+            return;
+        }
+    }
     
 }
