7dbc3fbfbc50510180c5df4c1794516da11710c4
siwon <muncool39@gmail.com>
2024-02-22 09:48:39 +0900

    [UPDATE] 2024년 2월 3주차 추가 문제

---
+++ February/3주차/prgrms_[PCCP 기출문제] 1번 붕대 감기/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ February/3주차/prgrms_가장 많이 받은 선물/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        return answer;
+    }
+}
