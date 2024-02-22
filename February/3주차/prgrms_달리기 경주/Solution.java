fafa552cc8abd3a458ffed7cd7aa2dc2f92d6b2b
ChaeAg <dydkglel123@gmail.com>
2024-02-23 01:08:19 +0900

    solve: prgrms_달리기 경주

--- February/3주차/prgrms_달리기 경주/Solution.java
+++ February/3주차/prgrms_달리기 경주/Solution.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-        return answer;
+        Map<String, Integer> playerMap = new HashMap<>();
+        
+        for(int i=0; i<players.length; i++) {
+            playerMap.put(players[i], i);
+        }
+        
+        for(String call : callings) {
+            int ranking = playerMap.get(call);
+            playerMap.replace(players[ranking - 1], ranking);
+            playerMap.replace(call, ranking - 1);
+            players[ranking] = players[ranking - 1];
+            players[ranking - 1] = call;
+        }
+        
+        return players;
     }
 }
