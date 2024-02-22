add89f678514f4ae7f19923976f28b2e3c686eed
ChaeAg <dydkglel123@gmail.com>
2024-02-23 01:07:07 +0900

    solve: boj_수 찾기

--- February/3주차/boj_수 찾기/Main.java
+++ February/3주차/boj_수 찾기/Main.java
@@ -1,3 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static int[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        A = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        int M = Integer.parseInt(br.readLine());
+
+        for(String s : br.readLine().split(" ")) {
+            sb.append(binarySearch(Integer.parseInt(s), 0, N-1)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static String binarySearch(int key, int low, int high) {
+        int mid;
+
+        while(low <= high) {
+            mid = (low + high) / 2;
+            if(A[mid] == key) {
+                return "1";
+            }
+            else if(A[mid] > key) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return "0";
+    }
 }
