07da1b0fc94a75ddfa200d57ffe91c577b321ab8
ChaeAg <dydkglel123@gmail.com>
2024-02-27 23:24:56 +0900

    solve: prgrms_[PCCP 기출문제] 1번 붕대 감기

--- February/3주차/prgrms_[PCCP 기출문제] 1번 붕대 감기/Solution.java
+++ February/3주차/prgrms_[PCCP 기출문제] 1번 붕대 감기/Solution.java
@@ -1,6 +1,30 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = 0;
-        return answer;
+        int time = 0, attackIdx = 0, gauge = 0, maxHealth = health;
+        
+        while(time <= attacks[attacks.length-1][0]) {
+            // 몬스터가 공격할 때
+            if(attacks[attackIdx][0] == time) {
+                health -= attacks[attackIdx++][1];
+                gauge = 0;
+                if(health <= 0) {
+                    return -1;
+                }
+            }
+            else { // 몬스터가 공격 안 할 때
+                gauge++;
+                health += bandage[1];
+                if(gauge % bandage[0] == 0) {
+                    health += bandage[2];
+                    gauge = 0;
+                }
+                if(health > maxHealth) {
+                    health = maxHealth;
+                }
+            }
+            time++;
+        }
+        
+        return health;
     }
 }
