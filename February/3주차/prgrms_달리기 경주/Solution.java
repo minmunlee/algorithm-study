c55fd6f851c672c2222852491466f792dc686936
siwon <muncool39@gmail.com>
2024-02-28 20:34:48 +0900

    solve: prgrms_달리기 경주

--- February/3주차/prgrms_달리기 경주/Solution.java
+++ February/3주차/prgrms_달리기 경주/Solution.java
@@ -1,6 +1,28 @@
+import java.util.*;
 class Solution {
+    HashMap<String, Integer> playerRank = new HashMap<>();
+    TreeMap<Integer, String> rankMap = new TreeMap<>();
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
+        for(int i=0;i<players.length;i++) {
+            playerRank.put(players[i], i+1);
+            rankMap.put(i+1, players[i]);
+        }
+        for(String callingPlayer : callings) {
+            int callingPlayerRank = playerRank.get(callingPlayer);
+            String frontPlayer = rankMap.get(callingPlayerRank-1);
+            
+            rankMap.replace(callingPlayerRank-1, callingPlayer);
+            rankMap.replace(callingPlayerRank, frontPlayer);
+            
+            playerRank.replace(callingPlayer, callingPlayerRank-1);
+            playerRank.replace(frontPlayer, callingPlayerRank);
+        }
+        String[] answer = new String[players.length];
+        int i=0;
+        for(Map.Entry<Integer, String> entry : rankMap.entrySet()) {
+            answer[i] = entry.getValue();
+            i++;
+        }
         return answer;
     }
 }
