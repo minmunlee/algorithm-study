bd943c450306ee786946cb7bbd0e30b0c993e680
siwon <muncool39@gmail.com>
2024-02-29 20:46:43 +0900

    solve: boj_한수

--- February/3주차/boj_한수/Main.java
+++ February/3주차/boj_한수/Main.java
@@ -1,3 +1,33 @@
-public class Main {
-    
+import java.io.*;
+import java.util.Arrays;
+public class Main{
+    static int cnt=99;
+	public static void main(String[] args)throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    if(N<100){
+	        cnt=N;
+	    }else{
+	        for(int i=100;i<N+1;i++){
+	            count(Integer.toString(i));
+	        }
+	    }
+	    System.out.print(cnt);
+	}
+	static void count(String x){
+	    int[] num = Arrays.stream(x.split(""))
+	                .mapToInt(Integer::parseInt).toArray();
+	    int[] arr = new int[num.length-1];
+	    for(int i=0;i<arr.length;i++){
+	        arr[i]=num[i+1]-num[i];
+	    }
+	    if(arr[0]==arr[1]){
+	        if(arr.length>2){
+	            if(arr[0]==arr[3])
+	                cnt+=1;
+	        }else{
+	            cnt+=1;
+	        }
+	    }
+	}
 }
