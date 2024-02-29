30d7c31f0763f1e242f3a9695218fdb69574c8f0
siwon <muncool39@gmail.com>
2024-02-29 20:46:35 +0900

    solve: boj_수 찾기

--- February/3주차/boj_수 찾기/Main.java
+++ February/3주차/boj_수 찾기/Main.java
@@ -1,3 +1,32 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args)throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    br.readLine();
+	    int A[] = Arrays.stream(br.readLine().split(" "))
+	                .mapToInt(Integer::parseInt).sorted().toArray();
+		br.readLine();
+		int num[] = Arrays.stream(br.readLine().split(" "))
+		            .mapToInt(Integer::parseInt).toArray();
+		for(int i : num){
+		    System.out.println(find(A,i));
+		}
+	}
+	static int find(int A[],int a){
+	    int bottom = 0; int top = A.length-1; int mid;
+	    while(bottom<=top){
+	        mid = (bottom+top)/2;
+	        if(A[mid]>a){
+	            top = mid-1;
+	        }
+	        else if(A[mid]<a){
+	            bottom = mid+1;
+	        }
+	        else if(A[mid]==a){
+	            return 1;
+	        }
+	    }
+	    return 0;
+	}
 }
