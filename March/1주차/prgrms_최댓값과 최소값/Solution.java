c6f1182861ce255e4840a5669ef497c7a1bf0654
ChaeAg <dydkglel123@gmail.com>
2024-03-06 14:41:25 +0900

    solve: prgrms_최댓값과 최솟값

--- March/1주차/prgrms_최댓값과 최소값/Solution.java
+++ March/1주차/prgrms_최댓값과 최소값/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        String[] strs = s.split(" ");
+        int min = Integer.parseInt(strs[0]);
+        int max = Integer.parseInt(strs[0]);
+    
+        for(String str : strs) {
+            int num = Integer.parseInt(str);
+            if(num < min) 
+                min = num;
+            if(num > max)
+                max = num;
+        }
+        
+        return min + " " + max;
     }
 }
