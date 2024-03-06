7353ed66a1cef266d9ae693d49f29a7e0e302e20
ChaeAg <dydkglel123@gmail.com>
2024-03-06 14:41:38 +0900

    solve: prgrms_최솟값 만들기

--- March/1주차/prgrms_최솟값 만들기/Solution.java
+++ March/1주차/prgrms_최솟값 만들기/Solution.java
@@ -1,6 +1,18 @@
-class Solution {
-    public String solution(String s) {
-        String answer = "";
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        // A, B 배열 오름차순 정렬
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int size = B.length - 1;
+        
+        for(int i=0; i<A.length; i++)
+            answer += A[i] * B[size-i];
+
         return answer;
     }
 }
