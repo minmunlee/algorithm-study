d24afa11076104cd4f48d9459ae596c422618953
ChaeAg <dydkglel123@gmail.com>
2024-03-06 14:41:16 +0900

    solve: prgrms_JadenCase 문자열 만들기

--- March/1주차/prgrms_JadenCase 문자열 만들기/Solution.java
+++ March/1주차/prgrms_JadenCase 문자열 만들기/Solution.java
@@ -1,12 +1,20 @@
-class Solution
-{
-    public int solution(int []A, int []B)
-    {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+class Solution {
+    public String solution(String s) {
+        s = s.toLowerCase();
+        String[] strs = s.split("");
+        boolean isFirst = true;
+                
+        for(int i=0; i<strs.length; i++) {
+            if(strs[i].equals(" ")) {
+                isFirst = true;
+                continue;
+            }
+            if(isFirst) {
+                strs[i] = strs[i].toUpperCase();
+                isFirst = false;
+            }
+        }
+        
+        return String.join("", strs);
     }
 }
