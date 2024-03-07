f32705b1845d448b9d54987f21bec284ff99f47c
gitseoyeon <tjdus553926@gmail.com>
2024-03-08 02:59:36 +0900

    solve: prgrms_최댓값과 최소값

--- March/1주차/prgrms_최댓값과 최소값/Solution.java
+++ March/1주차/prgrms_최댓값과 최소값/Solution.java
@@ -1,6 +1,19 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
+        StringTokenizer str = new StringTokenizer(s);
+        List<Integer> list = new ArrayList<>();
+        
+        while(str.hasMoreTokens()){
+            list.add(Integer.parseInt(str.nextToken()));
+        }
+        
+        String max = Integer.toString(Collections.max(list));
+        String min = Integer.toString(Collections.min(list));
+        
+        answer = min + " " + max;
         return answer;
     }
 }
