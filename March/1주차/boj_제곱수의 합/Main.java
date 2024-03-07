6240379e7ea50e9ad0042fd7396c90ec605d0427
gitseoyeon <tjdus553926@gmail.com>
2024-03-08 02:58:47 +0900

    solve: boj_제곱수의 합

--- March/1주차/boj_제곱수의 합/Main.java
+++ March/1주차/boj_제곱수의 합/Main.java
@@ -1,3 +1,23 @@
-public class Main {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ze {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[100001];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= N; i++){
+            dp[i] = i;
+            for(int j = 1; j * j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
 }
