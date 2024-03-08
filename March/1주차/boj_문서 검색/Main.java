024a204abd1edca1d532f2907138f62d95a03be2
siwon <muncool39@gmail.com>
2024-03-08 15:16:39 +0900

    solve: boj_문서 검색

--- March/1주차/boj_문서 검색/Main.java
+++ March/1주차/boj_문서 검색/Main.java
@@ -1,3 +1,9 @@
-public class Main {
-    
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String document = br.readLine();
+	    String word = br.readLine();
+		System.out.print((document.length() - document.replace(word, "").length())/word.length());
+	}
 }
