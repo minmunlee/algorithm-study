b830fffe2e07fd207fd62524eb00e1f8335e20e5
ChaeAg <dydkglel123@gmail.com>
2024-03-15 15:11:25 +0900

    solve: prgrms_숫자의 표현

--- March/2주차/prgrms_숫자의 표현/Solution.java
+++ March/2주차/prgrms_숫자의 표현/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int sum = 0;
+        int start = 1;
+        int idx = 1;
+        while(start <= n) {
+            sum += idx++;
+            if(sum >= n) {
+                if(sum == n) answer++;
+                idx = ++start;
+                sum = 0;
+            }
+        }
+        
         return answer;
     }
 }
