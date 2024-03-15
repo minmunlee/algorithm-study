94dc3f5946a341174bad697ac6252cb35bc9a295
gitseoyeon <tjdus553926@gmail.com>
2024-03-15 10:06:58 +0900

    solve: prgrms_올바른 괄호

--- March/2주차/prgrms_올바른 괄호/Solution.java
+++ March/2주차/prgrms_올바른 괄호/Solution.java
@@ -1,10 +1,32 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        
+        String[] str = s.split("");
+        int o = 0;
+        int c = 0;
+        
+        if(str[0].equals(")") || str[str.length - 1].equals("(")){
+            return answer = false;
+        }else{
+            for(int i = 0; i < str.length; i++){
+                if(str[i].equals("(")){
+                   o++; 
+                }else{
+                    c++;
+                }
+                
+                if(o < c){
+                    return answer = false;
+                }
+            }
+            
+            if(o == c){
+                return answer;
+            }else{
+                return answer = false;
+            }
+            
+        }
     }
 }
