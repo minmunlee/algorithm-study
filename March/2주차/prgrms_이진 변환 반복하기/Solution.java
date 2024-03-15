4440f371cb5598acac06fd67dc083f955ddfd8e6
ChaeAg <dydkglel123@gmail.com>
2024-03-15 15:11:53 +0900

    solve: prgrms_이진 변환 반복하기

--- March/2주차/prgrms_이진 변환 반복하기/Solution.java
+++ March/2주차/prgrms_이진 변환 반복하기/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        
+        while(!s.equals("1")) {
+            int cut_s_len = s.replaceAll("0", "").length();
+            answer[1] += s.length() - cut_s_len;
+            s = Integer.toBinaryString(cut_s_len);
+            answer[0]++;
+        }
+        
         return answer;
     }
 }
