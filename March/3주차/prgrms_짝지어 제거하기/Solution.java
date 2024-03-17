679f50e138d7041d621c8acd7ffb910a427567de
siwon <muncool39@gmail.com>
2024-03-17 14:48:45 +0900

    [UPDATE] 2024년 3월 3주차 문제

---
+++ March/3주차/prgrms_짝지어 제거하기/Solution.java
@@ -0,0 +1,10 @@
+class Solution{
+    public int solution(String s) {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
