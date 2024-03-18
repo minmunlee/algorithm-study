752ebdd71a8b917be7525b2eb07616add98b04f7
ChaeAg <dydkglel123@gmail.com>
2024-03-18 23:51:36 +0900

    solve: boj_수들의 합

--- March/3주차/boj_수들의 합/Main.java
+++ March/3주차/boj_수들의 합/Main.java
@@ -1,3 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long S = Long.parseLong(br.readLine());
+        System.out.print(greedy(S));
+    }
+
+    static long greedy(long num) {
+        if(num == 2 || num == 1) return 1;
+        long sum = 0, count = 0;
+        for(long i=1; i<=num/2; i++) {
+            sum += i;
+            count++;
+            if(sum + i + i + 3 >= num) {
+                return sum + i + i + 3 == num ? count + 2 : count + 1;
+            }
+        }
+        return 0;
+    }
 }
