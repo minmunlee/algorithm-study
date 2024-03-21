8e4a290d0deed3bf775034012cddf7e8a19ec590
siwon <muncool39@gmail.com>
2024-03-21 17:31:22 +0900

    solve: prgrms_다음 큰 숫자

--- March/3주차/prgrms_다음 큰 숫자/Solution.java
+++ March/3주차/prgrms_다음 큰 숫자/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        String binaryN = Integer.toBinaryString(n);
+        int oneLength = binaryN.length() - binaryN.replace("1", "").length();
+        int nextNumber = n + 1;
+        while(true) {
+            String binaryNext = Integer.toBinaryString(nextNumber);
+            int nextLength = binaryNext.length() - binaryNext.replace("1", "").length();
+            if(oneLength == nextLength) return nextNumber;
+            nextNumber++;
+        }
     }
 }
