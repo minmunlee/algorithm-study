bc79b4b0308572939111d79073ee85d758ce5914
siwon <muncool39@gmail.com>
2024-03-21 17:31:34 +0900

    solve: prgrms_짝지어 제거하기

--- March/3주차/prgrms_짝지어 제거하기/Solution.java
+++ March/3주차/prgrms_짝지어 제거하기/Solution.java
@@ -1,10 +1,16 @@
+import java.util.*;
 class Solution{
     public int solution(String s) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i=1;i<s.length();i++) {
+            char munza = s.charAt(i);
+            if(stack.isEmpty() || stack.peek()!=munza) {
+                stack.push(munza);
+                continue;
+            }
+            stack.pop();
+        }
+        return (stack.isEmpty())?1:0;
     }
 }
