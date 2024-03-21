8cefe7ce5d6ed6c76b8f2a75f8e806f8c7dc6f46
ChaeAg <dydkglel123@gmail.com>
2024-03-21 13:07:23 +0900

    solve: prgrms_피보나치 수

--- March/3주차/prgrms_피보나치 수/Solution.java
+++ March/3주차/prgrms_피보나치 수/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
+    static int[] nums;
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        nums = new int[n+1];
+        nums[1] = 1;
+        return fibo(n);
+    }
+    
+    /*
+    이렇게 메모이제이션을 하면 시간복잡도가 O(N)이 된다.
+    */
+    public int fibo(int n) {
+        if(n == 0) return 0;
+        if(nums[n] == 0) {
+            nums[n] = (fibo(n-1) + fibo(n-2)) % 1234567;
+        }
+        return nums[n];
     }
 }
