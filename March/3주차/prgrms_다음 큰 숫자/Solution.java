24496dcfdccd104b446903c0cb36c0ad9ca001e9
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-03-22 12:14:32 +0900

    solve: prgrms_다음 큰 숫자

--- March/3주차/prgrms_다음 큰 숫자/Solution.java
+++ March/3주차/prgrms_다음 큰 숫자/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        return answer;
+        
+        int a = Integer.bitCount(n); // 이진수로 변환했을 때의 1의 개수
+               
+        while(true){
+            int i = n + 1;
+            int b = Integer.bitCount(i);
+            if(a == b){
+                return answer = i;
+            }
+            n++;
+        }
     }
 }
