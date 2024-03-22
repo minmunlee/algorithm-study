820381c4ae991802c939b0269c5715ed11dc65c4
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-03-22 12:14:49 +0900

    solve: prgrms_피보나치 수

--- March/3주차/prgrms_피보나치 수/Solution.java
+++ March/3주차/prgrms_피보나치 수/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        return answer;
+        int[] F = new int[100001];
+        F[0] = 0;
+        F[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            F[i] = (F[i - 1] + F[i - 2]) % 1234567;
+        }
+
+        return answer = F[n];
     }
 }
