ac83daccfc8ed65f7076d91aa7d0b1c656173f2f
siwon <muncool39@gmail.com>
2024-03-29 05:00:34 +0900

    fail: prgrms_점프와 순간 이동

--- March/4주차/prgrms_점프와 순간 이동/Solution.java
+++ March/4주차/prgrms_점프와 순간 이동/Solution.java
@@ -1,10 +1,30 @@
+import java.util.*;
+// 메모리 초과
 public class Solution {
+    int[] map;
+    boolean[] visited;
     public int solution(int n) {
-        int ans = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return ans;
+        map = new int[n+1];
+        visited = new boolean[n+1];
+        bfs(0,0,n);
+        return map[n];
+    }
+    public void bfs(int start, int battery, int n) {
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{start, battery});
+        while(!que.isEmpty()) {
+            int[] now = que.poll();
+            int loc = now[0];
+            int b = now[1];
+            if(visited[loc] && map[loc]<=b) continue;
+            map[loc] = b;
+            if(loc < n) {
+                que.add(new int[]{loc+1,map[loc]+1});
+            }
+            if(loc <= n/2) {
+                que.add(new int[]{loc*2,map[loc]});
+            }
+            visited[loc] = true;
+        }
     }
 }
