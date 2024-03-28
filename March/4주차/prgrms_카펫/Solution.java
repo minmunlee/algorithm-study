7119a9c92fce7c9ef6179fefeb1cb2c9bc0e0e41
siwon <muncool39@gmail.com>
2024-03-29 04:59:48 +0900

    solve: prgrms_카펫

--- March/4주차/prgrms_카펫/Solution.java
+++ March/4주차/prgrms_카펫/Solution.java
@@ -1,6 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        return answer;
+        for(int i=6;i<brown;i+=2) {
+            int top = i/2;
+            int side = ((brown-i)/2)+2;
+            int middle = (top-2)*(side-2);
+            if(middle == yellow) {
+                return new int[]{side, top};
+            }
+        }
+        return new int[]{0, 0};
     }
 }
