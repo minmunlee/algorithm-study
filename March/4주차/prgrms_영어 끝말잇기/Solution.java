acb25e4d07b4fd2fe1680265e535d7ab2eb9c878
siwon <muncool39@gmail.com>
2024-03-29 04:59:41 +0900

    solve: prgrms_영어 끝말잇기

--- March/4주차/prgrms_영어 끝말잇기/Solution.java
+++ March/4주차/prgrms_영어 끝말잇기/Solution.java
@@ -1,10 +1,17 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
-        System.out.println("Hello Java");
-
-        return answer;
+        HashSet<String> wordSet = new HashSet<>();
+        wordSet.add(words[0]);
+        char last = words[0].charAt(words[0].length()-1);
+        for(int i=1;i<words.length;i++) {
+            String word = words[i];
+            if(word.charAt(0)!=last || wordSet.contains(word)) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
+            last = word.charAt(words[i].length()-1);
+            wordSet.add(word);
+        }
+        return new int[]{0,0};
     }
 }
