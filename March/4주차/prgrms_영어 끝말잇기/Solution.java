469327424c96f4fc69f09007e0259536ba743deb
ChaeAg <dydkglel123@gmail.com>
2024-03-29 14:54:54 +0900

    solve: prgrms_영어 끝말잇기

--- March/4주차/prgrms_영어 끝말잇기/Solution.java
+++ March/4주차/prgrms_영어 끝말잇기/Solution.java
@@ -1,10 +1,32 @@
-class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = {};
+    import java.util.*;
+    class Solution {
+        public int[] solution(int n, String[] words) {
+            int[] answer = new int[2];
+            Stack<String> word_memory = new Stack<>();
+            Map<Integer, Integer> players = new HashMap<>();
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
-        System.out.println("Hello Java");
+            for(int i=0; i<=n; i++) {
+                players.put(i, 1);
+            }
 
-        return answer;
+            for(int i=0; i<words.length; i++) {
+                int player = i % n;
+                // 이미 있는 단어를 말할 경우
+                // 직전 단어의 마지막 문자로 시작하는 단어를 말하지 않았을 경우
+                if(i != 0) {
+                    String last_word = word_memory.peek();
+                    if(word_memory.contains(words[i]) ||
+                       last_word.charAt(last_word.length()-1) != words[i].charAt(0)) {
+                        answer[0] = player + 1;
+                        answer[1] = players.get(player);
+                        break;
+                    }
+                }
+
+                word_memory.push(words[i]);
+                players.replace(player, players.get(player) + 1);
+            }
+
+            return answer;
+        }
     }
-}
