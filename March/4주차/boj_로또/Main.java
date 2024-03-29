49d2f137aac4bde544e9227b5553e82016a2d512
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-03-29 14:12:28 +0900

    solve: boj_로또

--- March/4주차/boj_로또/Main.java
+++ March/4주차/boj_로또/Main.java
@@ -1,3 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+
+    public static int k;
+    public static int[] lotto;
+    public static boolean[] check;
+    public static int[] tmp;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true){
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            k = Integer.parseInt(str.nextToken()); // 입력 받는 수 개수
+            tmp = new int[6];
+
+            if(k == 0) break;
+
+            lotto = new int[k];
+            check = new boolean[k];
+
+            for(int i = 0; i < lotto.length; i++){
+                lotto[i] = Integer.parseInt(str.nextToken());
+            }
+            
+            dfs(0, 0);
+        }
+    }
+
+    public static void dfs(int n, int l){
+        if(l == 6){
+            for(int i = 0; i < 6; i++){
+                System.out.print(tmp[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = n; i < k; i++){
+            if(check[i] == false){
+                check[i] = true;
+                tmp[l] = lotto[i];
+                dfs(i + 1, l + 1);
+                check[i] = false;
+            }
+        }
+    }
+
 }
