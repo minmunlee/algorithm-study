ecae581a2b33f83c5f32ff0147df2e5f48c18253
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-03-29 14:13:32 +0900

    solve: prgrms_카펫

--- March/4주차/prgrms_카펫/Solution.java
+++ March/4주차/prgrms_카펫/Solution.java
@@ -1,6 +1,26 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        
+        int carpet = brown + yellow; // 카펫의 크기
+        
+        int row = 0;
+        int col = 0;
+        
+        for(int i = 1; i <= carpet; i++){
+            row = i;
+            col = carpet / row;
+            
+            if(row > col){ // 열이 행보다 길 경우
+                continue;
+            }
+            
+            if((row - 2) * (col - 2) == yellow){ // 노란 격자는 전체 카펫 행과 열에서 각각 2씩 뺀 행과 열의 길이와 같다
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            }
+        }
         return answer;
     }
 }
