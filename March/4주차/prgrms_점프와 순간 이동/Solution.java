a3e3db5bd52b61e163be7b4d0a82d2482c5a4f3d
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-03-29 14:13:19 +0900

    solve: prgrms_점푸와 순간이동

--- March/4주차/prgrms_점프와 순간 이동/Solution.java
+++ March/4주차/prgrms_점프와 순간 이동/Solution.java
@@ -1,10 +1,15 @@
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        
+        while(n > 0){
+            if(n % 2 == 0){
+                n /= 2;
+            }else{
+                n--;
+                ans++;
+            }    
+        }
         return ans;
     }
 }
