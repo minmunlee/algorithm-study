38dc3ae72d5ada32a3fbd8e72623285d049b0b22
siwon <muncool39@gmail.com>
2024-04-04 22:35:38 +0900

    solve: prgrms_구명보트

--- April/1주차/prgrms_구명보트/Solution.java
+++ April/1주차/prgrms_구명보트/Solution.java
@@ -1,6 +1,15 @@
+import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(people);
+        int min = 0, first = 0, second = people.length-1;
+        while(first<=second) {
+            min++;
+            if(people[first]+people[second]<=limit) {
+                first++;
+            }
+            second--;
+        }
+        return min;
     }
 }
