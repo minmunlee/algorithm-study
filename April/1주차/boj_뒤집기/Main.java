70f06ff58b873bb7f18f56869e907dac0f941907
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-05 11:14:17 +0900

    solve: boj_뒤집기

--- April/1주차/boj_뒤집기/Main.java
+++ April/1주차/boj_뒤집기/Main.java
@@ -1,3 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static int oCnt;
+    public static int zCnt;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] str = s.split("");
+        oCnt = 0;
+        zCnt = 0;
+        String tmp = str[0];
+
+        cnt(tmp);
+
+        for(int i = 0; i < str.length; i++){
+            if(!str[i].equals(tmp)){
+                tmp = str[i];
+                cnt(tmp);
+            }
+        }
+
+        if(oCnt >= zCnt){
+            System.out.println(zCnt);
+        }else{
+            System.out.println(oCnt);
+        }
+ 
+    }
+
+    public static void cnt(String st){
+        if(st.equals("0")){
+            zCnt++;
+        }else{
+            oCnt++;
+        }
+    }
     
 }
