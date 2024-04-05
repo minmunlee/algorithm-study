e79603153799d694d4249dfff7932b0c596a3ea1
ChaeAg <dydkglel123@gmail.com>
2024-04-05 14:08:48 +0900

    solve: prgrms_멀리 뛰기

--- April/1주차/prgrms_멀리 뛰기/Solution.java
+++ April/1주차/prgrms_멀리 뛰기/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
     public long solution(int n) {
-        long answer = 0;
-        return answer;
+        long[] dp = new long[n];
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        long num1 = 1;
+        long num2 = 2;
+        long temp;
+        
+        for(int i=3; i<=n; i++) {
+            temp = num2;
+            num2 = (num1 + num2) % 1234567;
+            num1 = temp;
+        }
+        
+        return num2;
     }
 }
