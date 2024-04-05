f33f645befdd8379cf0e4df4324daca974df8fde
ChaeAg <dydkglel123@gmail.com>
2024-04-05 14:08:42 +0900

    solve: prgrms_구명보트

--- April/1주차/prgrms_구명보트/Solution.java
+++ April/1주차/prgrms_구명보트/Solution.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        Arrays.sort(people);
+        int start = 0, end = people.length - 1;
+
+        while(start <= end) {
+            if(people[end] + people[start] <= limit) {
+                start++;
+                end--;
+                answer++;
+            }
+            else if(people[end] <= limit) {
+                end--;
+                answer++;
+            }
+            else if(people[start] <= limit) {
+                start++;
+                answer++;
+            }
+        }
+        
         return answer;
     }
 }
