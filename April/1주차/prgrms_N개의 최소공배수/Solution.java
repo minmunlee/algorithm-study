3d79753a76b3c1acd99224a495b624ddf6a470af
ChaeAg <dydkglel123@gmail.com>
2024-04-05 14:08:36 +0900

    solve: prgrms_N개의 최소공배수

--- April/1주차/prgrms_N개의 최소공배수/Solution.java
+++ April/1주차/prgrms_N개의 최소공배수/Solution.java
@@ -1,6 +1,25 @@
+import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
+        
+        Arrays.sort(arr);
+        int start = arr[0] * arr[arr.length-1];
+        
+        for(int i=start; i<2147483647; i++) {
+            boolean isGongbeasu = true;
+            for(int j=0; j<arr.length; j++) {
+                if(i % arr[j] != 0) {
+                    isGongbeasu = false;
+                    break;
+                }
+            }
+            if(isGongbeasu) {
+                answer = i;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
