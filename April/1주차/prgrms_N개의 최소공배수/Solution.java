5fd8be44537b5262ecec0bb9d189fd4e5506893b
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-05 11:14:33 +0900

    solve: boj_N개의 최소공배수

--- April/1주차/prgrms_N개의 최소공배수/Solution.java
+++ April/1주차/prgrms_N개의 최소공배수/Solution.java
@@ -1,6 +1,30 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
+        int answer = arr[0];
+        
+        for(int i = 1; i < arr.length; i++){
+            answer = lcm(answer, arr[i]);
+        }
         return answer;
+    }
+    
+    public int gdc(int n1, int n2){
+        if(n1 < n2){
+            int tmp = n1;
+            n1 = n2;
+            n2 = tmp;
+        }
+        
+        while(n2 != 0){
+            int n = n1 % n2;
+            n1 = n2;
+            n2 = n;
+        }
+        
+        return n1;
+    }
+    
+    public int lcm(int n1, int n2){
+        return n1 * n2 / gdc(n1, n2);
     }
 }
