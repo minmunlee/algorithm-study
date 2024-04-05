d667af9f84266e33207c65e52f22c3c02f7b69af
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-05 11:15:00 +0900

    solve: prgrms_구명보트

--- April/1주차/prgrms_구명보트/Solution.java
+++ April/1주차/prgrms_구명보트/Solution.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        int light = 0;
+        int heavy = people.length - 1;
+        
+        Arrays.sort(people);
+        
+        while(light <= heavy){
+            answer++;
+            if(people[light] + people[heavy] <= limit){
+                light++;
+            }
+            heavy--;
+        }
         return answer;
     }
 }
