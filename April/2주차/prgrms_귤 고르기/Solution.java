02062580ff0c1899de53b5c52d1303b6da31bba6
siwon <muncool39@gmail.com>
2024-04-07 22:50:39 +0900

    [UPDATE] 2024년 4년 2주차 문제

---
+++ April/2주차/prgrms_귤 고르기/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ April/2주차/prgrms_예상 대진표/Solution.java
@@ -0,0 +1,8 @@
+class Solution{
+    public int solution(int n, int a, int b){
+        int answer = 0;
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
