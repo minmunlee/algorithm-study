441acc8063c3ee615e189eed42efa4cdc3e81a1b
siwon <muncool39@gmail.com>
2024-04-10 23:00:57 +0900

    solve: boj_나이순 정렬

--- April/2주차/boj_나이순 정렬/Main.java
+++ April/2주차/boj_나이순 정렬/Main.java
@@ -1,3 +1,24 @@
-public class Main {
-    
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder[] lst = new StringBuilder[201];
+		for(int i=0;i<lst.length;i++){
+		    lst[i] = new StringBuilder("");
+		}
+		for(int i=0;i<N;i++){
+		    StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		    int age = Integer.parseInt(st.nextToken());
+		    String name = st.nextToken();
+		    lst[age].append(age).append(" ").append(name).append("\n");
+		}
+		StringBuilder sb = new StringBuilder("");
+		for(StringBuilder str : lst){
+		    sb.append(str);
+		}
+		System.out.print(sb);
+	}
 }
