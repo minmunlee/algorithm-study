552a95d265ae6aedb129bff304aeaf9e5fb95d4b
siwon <muncool39@gmail.com>
2024-04-10 23:36:42 +0900

    solve: prgrms_귤 고르기

--- April/2주차/prgrms_귤 고르기/Solution.java
+++ April/2주차/prgrms_귤 고르기/Solution.java
@@ -1,6 +1,17 @@
+import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        return answer;
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for(int t : tangerine) {
+            countMap.put(t, countMap.getOrDefault(t, 0) + 1);
+        }
+        List<Integer> countList = new ArrayList<>(countMap.values());
+        Collections.sort(countList, Collections.reverseOrder());
+        int tan = 0;
+        while(k>0) {
+            k-=countList.get(tan);
+            tan++;
+        }
+        return tan;
     }
 }
