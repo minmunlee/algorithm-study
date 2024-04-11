ea5b2e167576517a76e00e86537e3e6e955cebf6
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-11 17:35:14 +0900

    solve: boj_막대기

--- April/2주차/boj_막대기/Main.java
+++ April/2주차/boj_막대기/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int stick = 64;
+        int cnt = 0;
+                
+        while (true){
+            // 막대의 길이가 X보다 큰 경우
+            if(stick > X){
+                stick /= 2;
+            }else{
+                // 남은 길이 계산
+                X -= stick;
+                cnt++;
+            }
+
+            if(X == 0){
+                System.out.println(cnt);
+                break;
+            }
+        }   
+    }
 }
