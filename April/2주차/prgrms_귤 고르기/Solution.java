423ef598f717bed3513f7db2a604b41a47a33876
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-11 17:35:43 +0900

    solve: prgrms_귤 고르기

--- April/2주차/prgrms_귤 고르기/Solution.java
+++ April/2주차/prgrms_귤 고르기/Solution.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for(int n : tangerine){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        for(int key : map.keySet()){
+            list.add(map.get(key));
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for(int i = 0; i < list.size(); i++){
+            if(k <= 0){
+                System.out.println(answer);
+                break;
+            }
+            
+            k-=list.get(i);
+            answer++;
+        }
+        
+        
         return answer;
     }
 }
