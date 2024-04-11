66af4fc1b70607a12625b28212fb710b4670039a
ChaeAg <dydkglel123@gmail.com>
2024-04-11 13:59:26 +0900

    solve: boj_막대기

--- April/2주차/boj_막대기/Main.java
+++ April/2주차/boj_막대기/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        List<Integer> bars = new ArrayList<>();
+        bars.add(64);
+
+        while(true) {
+            int sum = bars.stream().mapToInt(i->i).sum();
+            if(sum == X) break;
+            int min = bars.stream().mapToInt(i->i).min().getAsInt();
+            bars.remove(bars.indexOf(min));
+            bars.add(min/2);
+
+            if(sum - (min/2) < X) {
+                bars.add(min/2);
+            }
+        }
+
+        System.out.print(bars.size());
+    }
 }
