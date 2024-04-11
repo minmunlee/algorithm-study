f6c9e8743a73343084b73985ef8859e3d5b51168
ChaeAg <dydkglel123@gmail.com>
2024-04-11 13:59:51 +0900

    solve: prgrms_연속 부분 수열 합의 개수

--- April/2주차/prgrms_연속 부분 수열 합의 개수/Solution.java
+++ April/2주차/prgrms_연속 부분 수열 합의 개수/Solution.java
@@ -1,6 +1,19 @@
+import java.util.*;
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        Set<Integer> sums = new HashSet<>();
+        int len = elements.length;
+        
+        for(int i=0; i<len; i++) { // 더할 개수
+            for(int j=0; j<len; j++) { // 시작 인덱스
+                int sum = 0;
+                for(int k=0; k<=i; k++) { // 진짜 더하는 for문
+                    sum += elements[(j+k) % len];
+                }
+                sums.add(sum);
+            }
+        }
+        
+        return sums.size();
     }
 }
