6e00de85ba991d464ddda090f5edc11c6eec899e
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-04-11 17:35:03 +0900

    solve: boj_나이순 정렬

--- April/2주차/boj_나이순 정렬/Main.java
+++ April/2주차/boj_나이순 정렬/Main.java
@@ -1,3 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        TreeMap<Integer, List<String>> members = new TreeMap<>(); // {나이 : 이름 리스트}
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            if (members.containsKey(age)) { // 같은 나이가 있는 경우
+                members.get(age).add(name);
+            } else {
+                List<String> names = new ArrayList<>();
+                names.add(name);
+                members.put(age, names);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, List<String>> entry : members.entrySet()) {
+            int age = entry.getKey();
+            List<String> names = entry.getValue();
+            for (String name : names) {
+                sb.append(age).append(" ").append(name).append("\n");
+            }
+        }
+        System.out.print(sb);
+
+    }
 }
