85a072a438c453b2a51f1b197830e1e4eb7b9ffb
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-05-03 11:05:52 +0900

    solve: boj_분수찾기

--- May/1주차/boj_분수찾기/Main.java
+++ May/1주차/boj_분수찾기/Main.java
@@ -1,3 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        int depth = 0;
+
+        while(X - depth > 0){
+            X -= depth; // 몇번째인지
+            depth++;
+        }
+
+        int sum = depth + 1; // 분자 분모 합
+        int bunmo = 0;
+        int bunja = 0;
+
+        // 짝수: 분자가 커지고 분모가 작아짐
+        if(depth % 2 == 0){
+            bunmo = sum - X;
+            bunja = X;
+        }else{
+            // 홀수: 분모가 커기고 분자가 작아짐
+            bunmo = X;
+            bunja = sum - X;
+        }
+
+        System.out.println(bunja + "/" + bunmo);
+    }
   
 }
