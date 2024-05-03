1ba92659211f6262d4a7061f1525012a32103da1
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-05-03 11:06:46 +0900

    solve: prgrms_괄호 회전하기

--- May/1주차/prgrms_괄호 회전하기/Solution.java
+++ May/1주차/prgrms_괄호 회전하기/Solution.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(isCorrect(s)){
+                answer++;
+            }
+            s = s.substring(1) + s.substring(0, 1);
+        }
         return answer;
+    }
+    
+    public static boolean isCorrect(String s){
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(stack.peek() == '(' && s.charAt(i) == ')'){
+                    stack.pop();
+                }else if(stack.peek() == '{' && s.charAt(i) == '}'){
+                    stack.pop();
+                }else if(stack.peek() == '[' && s.charAt(i) == ']'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        } 
+        return stack.isEmpty();
     }
 }
