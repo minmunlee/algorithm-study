ce563e295ab79f86f00d080483651ac21bdabd96
ChaeAg <dydkglel123@gmail.com>
2024-05-03 16:27:53 +0900

    solve: prgrms_괄호 회전하기

--- May/1주차/prgrms_괄호 회전하기/Solution.java
+++ May/1주차/prgrms_괄호 회전하기/Solution.java
@@ -1,6 +1,42 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            String new_str = s.substring(i) + s.substring(0, i);
+            Stack<String> stack = new Stack<String>();
+            boolean isPossible = true;
+            
+            for(String str : new_str.split("")) {
+                if(str.equals("(") || str.equals("{") || str.equals("[")) {
+                    stack.push(str);
+                }
+                else {
+                    if(stack.isEmpty()) {
+                        isPossible = false;
+                        break;
+                    }
+                    String top = stack.pop();
+                    if(str.equals(")") && !top.equals("(")) {
+                        isPossible = false;
+                        break;
+                    }
+                    else if(str.equals("}") && !top.equals("{")) {
+                        isPossible = false;
+                        break;
+                    }
+                    else if(str.equals("]") && !top.equals("[")) {
+                        isPossible = false;
+                        break;
+                    }
+                }
+            }
+            
+            if(!stack.isEmpty()) isPossible = false;
+            if(isPossible) answer++;
+        }
+        
         return answer;
     }
 }
