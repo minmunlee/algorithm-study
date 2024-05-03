365594b2f3b175063247a14adff84f2418724be9
siwon <muncool39@gmail.com>
2024-05-03 18:04:38 +0900

    solve: boj_2xn 타일링

--- May/1주차/boj_2xn 타일링/Main.java
+++ May/1주차/boj_2xn 타일링/Main.java
@@ -1,3 +1,13 @@
+import java.io.*;
 public class Main {
-  
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long sq[] = new long[n+2];
+		sq[0]=0;sq[1]=1;sq[2]=2;
+		for(int i=3;i<=n;i++){
+		    sq[i]=(sq[i-1]+sq[i-2])%10007;
+		}
+		System.out.print(sq[n]);
+	}
 }
