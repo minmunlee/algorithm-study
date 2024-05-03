b0e2de22fd59d20aea4342967904078861d5fc61
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-05-03 11:06:36 +0900

    solve: prgrms_n^2 배열 자르기

--- May/1주차/prgrms_n^2 배열 자르기/Solution.java
+++ May/1주차/prgrms_n^2 배열 자르기/Solution.java
@@ -1,6 +1,12 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
-        int[] answer = {};
+        
+        int[] answer = new int[(int)(right - left) + 1]; // 메모리 초과 문제 해결
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = Math.max((int)((i + left) / n) + 1, (int)((i + left) % n) + 1);
+        }
+
         return answer;
     }
 }
