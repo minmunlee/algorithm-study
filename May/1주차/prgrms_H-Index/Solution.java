81d01304cd3e6379ecd259511e1ca90ffc89ba35
siwon <muncool39@gmail.com>
2024-05-03 11:11:13 +0900

    solve: prgrms_H-Index

--- May/1주차/prgrms_H-Index/Solution.java
+++ May/1주차/prgrms_H-Index/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(citations);
+        int maxsize = citations.length;
+        int max = 0;
+        for(int i=0;i<maxsize;i++) {
+            int h = Math.min(citations[i], maxsize - i);
+            if(max > h) break;
+            max = h;
+        }
+        return max;
     }
 }
