14a9a98700bbd8c973bfc1ec5cca17a758776a62
ChaeAg <dydkglel123@gmail.com>
2024-05-10 14:31:29 +0900

    solve: boj_정수 a를 k로 만들기

--- May/2주차/boj_정수 a를 k로 만들기/Main.java
+++ May/2주차/boj_정수 a를 k로 만들기/Main.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        while(K > A) {
+            if(K / 2 >= A && K % 2 == 0) {
+                K = K / 2 + 1;
+            }
+            count++;
+            K--;
+        }
+
+        System.out.print(count);
+    }
 }
