ed49a568d72296ebbe0ec81e669e37649b7c70a1
ChaeAg <dydkglel123@gmail.com>
2024-05-10 14:31:47 +0900

    solve: prgrms_할인 행사

--- May/2주차/prgrms_할인 행사/Solution.java
+++ May/2주차/prgrms_할인 행사/Solution.java
@@ -1,6 +1,28 @@
+import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        List<String> want_list = Arrays.asList(want);
+        
+        for(int i=0; i<=discount.length-10; i++) {
+            int[] copy_number = Arrays.copyOf(number, number.length);
+            for(int idx=i; idx<i+10; idx++) {
+                if(want_list.contains(discount[idx])) {
+                    int index = want_list.indexOf(discount[idx]);
+                    copy_number[index]--;
+                }
+            }
+            boolean isOk = true;
+            for(int num : copy_number) {
+                if(num > 0) {
+                    isOk = false;
+                    break;
+                } 
+            }
+            
+            if(isOk) answer++;
+        }
+        
         return answer;
     }
 }
