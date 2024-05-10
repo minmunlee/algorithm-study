ac6e2bfced308932088608f2538a675f484c8cc0
ChaeAg <dydkglel123@gmail.com>
2024-05-10 14:31:42 +0900

    solve: prgrms_[1차] 캐시

--- May/2주차/prgrms_[1차] 캐시/Solution.java
+++ May/2주차/prgrms_[1차] 캐시/Solution.java
@@ -1,6 +1,28 @@
+import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+        if(cacheSize == 0) return cities.length * 5;
+        
         int answer = 0;
+        List<String> LRU = new ArrayList<>();
+            
+        for(String city : cities) {
+            String l_city = city.toLowerCase();
+            if(LRU.contains(l_city)) {
+                LRU.remove(l_city);
+                answer += 1;
+            }
+            else {
+                answer += 5;
+            }
+            
+            if(LRU.size() != 0 && LRU.size() >= cacheSize) {
+                LRU.remove(0);
+            }
+            
+            LRU.add(l_city);
+        }
+        
         return answer;
     }
 }
