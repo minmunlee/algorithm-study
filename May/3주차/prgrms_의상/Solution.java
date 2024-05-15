d1798699af92a0cd918496a52c4f086c58296e2f
ChaeAg <dydkglel123@gmail.com>
2024-05-16 03:16:21 +0900

    solve: prgrms_의상

--- May/3주차/prgrms_의상/Solution.java
+++ May/3주차/prgrms_의상/Solution.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
+        HashMap<String, Integer> counts = new HashMap<>();
+        
+        for(String[] cloth : clothes) {
+            counts.put(cloth[1], counts.getOrDefault(cloth[1], 0) + 1);
+        }
+		
+        for(String key : counts.keySet()) {
+            answer *= counts.get(key) + 1; // 안입는 경우까지 고려해서 +1 해서 곱함
+        }
+        
+        // 아무것도 안 입는 경우 빼줌
+        answer -= 1;
+        
         return answer;
     }
 }
