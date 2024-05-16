7808560672c573dc1436111f100f0f26f4373818
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-05-16 17:10:01 +0900

    solve: prgrms_튜플

--- May/3주차/prgrms_튜플/Solution.java
+++ May/3주차/prgrms_튜플/Solution.java
@@ -1,6 +1,29 @@
+import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        Set<String> set = new LinkedHashSet<>();
+
+        // 괄호 없애고 값들을 배열에 넣음
+        String[] str = s.substring(1,s.length()-1).split("},");
+        for(int i = 0;i<str.length;i++){
+            str[i] = str[i].replace("{","").replace("}", "");   
+        }
+        Arrays.sort(str,(a,b)->(a.length() - b.length()));
+        
+        // 중복 제거
+        for(String st : str){
+            String[] arr = st.split(",");
+            for(String ar : arr){
+                set.add(ar);
+            }
+        }
+ 
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for(String d:set){
+            answer[idx++] = Integer.parseInt(d);
+        }
+        
         return answer;
     }
 }
