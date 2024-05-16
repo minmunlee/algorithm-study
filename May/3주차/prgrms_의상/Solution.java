a6cf1bccd5870e3fb5969b130b80e9272aa5a953
siwon <muncool39@gmail.com>
2024-05-16 19:17:18 +0900

    solve: prgrms_의상

--- May/3주차/prgrms_의상/Solution.java
+++ May/3주차/prgrms_의상/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        int answer = 1;
+        HashMap<String, Integer> count = new HashMap<>();
+        for(String[] cloth : clothes) {
+            count.put(cloth[1], count.getOrDefault(cloth[1], 0) + 1);
+        }
+        for(String key : count.keySet()) {
+            answer *= count.get(key) + 1;
+        }
+        return answer-1;
     }
 }
