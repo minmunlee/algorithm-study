bf3d55e4c5784dae4e0164ab913915c9bcc2f6d9
MinSeoYeon <126096318+gitseoyeon@users.noreply.github.com>
2024-05-16 17:09:51 +0900

    solve: prgrms_의상

--- May/3주차/prgrms_의상/Solution.java
+++ May/3주차/prgrms_의상/Solution.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        int answer = 1;
+        Map<String, Integer> hm = new HashMap<>(); // 의상 종류별 개수
+        
+        for(int i = 0; i < clothes.length; i++){
+            if(hm.containsKey(clothes[i][1])){
+                hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
+            }else{
+                hm.put(clothes[i][1], 1);
+            }
+        }
+        
+        for(int i : hm.values()){
+            answer *= i + 1; // 안입기까지 합쳐서 1 더하기
+        }
+        return answer - 1; // 아예 안입는건 안됨 
     }
 }
