abd92c999a29aa330a3e10abcfad21fab0c45dfe
ChaeAg <dydkglel123@gmail.com>
2024-05-22 09:44:33 +0900

    solve: boj_좌표 정렬하기

--- May/4주차/boj_좌표 정렬하기/Main.java
+++ May/4주차/boj_좌표 정렬하기/Main.java
@@ -1,3 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o1[1] - o2[1];
+            return o1[0] - o2[0];
+        });
+        
+        for(int[] a : arr) {
+            sb.append(a[0]).append(" ").append(a[1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
