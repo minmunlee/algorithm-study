fb8d2aeff5d2c1f1c0b824c32b1e0ebae1e0bcf6
ChaeAg <dydkglel123@gmail.com>
2024-05-22 09:44:48 +0900

    solve: prgrms_프로세스

--- May/4주차/prgrms_프로세스/Solution.java
+++ May/4주차/prgrms_프로세스/Solution.java
@@ -1,6 +1,30 @@
+import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        int max = 0;
+        for(int i=0; i<priorities.length; i++) {
+            q.offer(i);
+            if(max < priorities[i]) max = priorities[i];
+        }
+        
+        int before_idx = -1;
+        
+        while(before_idx != location) {
+            int idx = q.poll();
+            
+            if(max > priorities[idx]) {
+                q.offer(idx);
+                continue;
+            }
+            
+            priorities[idx] = 0;
+            max = Arrays.stream(priorities).max().getAsInt();
+            before_idx = idx;
+            answer++;
+        }
+        
         return answer;
     }
 }
