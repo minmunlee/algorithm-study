1cb8f52e01aa555d0391c26b5d78fe7c9111edeb
gitseoyeon <tjdus553926@gmail.com>
2024-05-23 16:55:48 +0900

    solve:prgrms_피로도

--- May/4주차/prgrms_피로도/Solution.java
+++ May/4주차/prgrms_피로도/Solution.java
@@ -1,6 +1,24 @@
 class Solution {
-    public int solution(int k, int[][] dungeons) {
-        int answer = -1;
+    public boolean[] check; // 방문 여부
+    public int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) { 
+        check = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
         return answer;
+    }
+    
+    public void dfs(int level, int k, int[][] dungeons){ // level: 던전 방문 횟수
+        if(level > answer){
+            answer = level;
+        }
+        
+        for(int i = 0; i < dungeons.length; i++){
+            if(!check[i] && dungeons[i][0] <= k){
+                check[i] = true;
+                dfs(level + 1, k - dungeons[i][1], dungeons);
+                check[i] = false;
+            }
+        }
     }
 }
