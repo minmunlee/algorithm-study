18f80fb9de8ac4571a15260d3d8aaa0b535b9e54
gitseoyeon <tjdus553926@gmail.com>
2024-05-23 16:55:04 +0900

    solve:boj_문자열

--- May/4주차/boj_문자열/Main.java
+++ May/4주차/boj_문자열/Main.java
@@ -1,3 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer str = new StringTokenizer(br.readLine());
+
+    String X = str.nextToken();
+    String Y = str.nextToken();
+    int n = 50;
+
+    for(int i = 0; i <= Y.length() - X.length(); i++){
+      int cnt = 0;
+      for(int j = 0; j < X.length(); j++){
+        if(X.charAt(j) != Y.charAt(i + j)){
+          cnt++;
+        }
+      }
+      n = Math.min(n, cnt);
+    }
+    System.out.println(n);
+  }
 }
