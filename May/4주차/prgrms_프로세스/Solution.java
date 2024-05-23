617f8a9dbf4dd4a7b75284a39e995732841ef039
gitseoyeon <tjdus553926@gmail.com>
2024-05-23 16:55:35 +0900

    solve:prgrms_프로세스

--- May/4주차/prgrms_프로세스/Solution.java
+++ May/4주차/prgrms_프로세스/Solution.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); // 우선 순위 큐
+        
+        for(int i : priorities){
+            queue.add(i);
+        }
+        
+        while(!queue.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(priorities[i] == queue.peek()){
+                    if(location == i){
+                        answer++;
+                        return answer;
+                    }else{
+                        queue.poll();
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(queue);
+        
         return answer;
     }
 }
