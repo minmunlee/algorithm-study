944e0dc8d4942d3ff2fb615a54db865d9203393a
gitseoyeon <tjdus553926@gmail.com>
2024-05-23 16:55:15 +0900

    solve:boj_좌표 정렬하기

--- May/4주차/boj_좌표 정렬하기/Main.java
+++ May/4주차/boj_좌표 정렬하기/Main.java
@@ -1,3 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer str;
+
+    int N = Integer.parseInt(br.readLine());
+    int[][] arr = new int[N][2];
+    int idx = 0;
+
+    for(int i = 0; i < N; i++){
+        str = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(str.nextToken());
+        int y = Integer.parseInt(str.nextToken());
+
+        arr[idx][0] = x;
+        arr[idx][1] = y;
+
+        idx++;
+    }
+
+    Arrays.sort(arr, (o1, o2) -> {
+        if(o1[0] == o2[0]){
+            return Integer.compare(o1[1], o2[1]);
+        }else{
+            return Integer.compare(o1[0], o2[0]);
+        }
+    });
+
+    StringBuilder sb = new StringBuilder();
+
+    for(int i = 0; i < N; i++) {
+        sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+    }
+
+    System.out.print(sb);
+
+  }
 }
