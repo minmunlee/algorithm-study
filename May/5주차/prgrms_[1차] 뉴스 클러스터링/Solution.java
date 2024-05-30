9b367edbc84e263050f70d0be8beddaec5496134
ChaeAg <dydkglel123@gmail.com>
2024-05-30 16:08:25 +0900

    solve: prgrms_[1차] 뉴스 클러스터링

--- May/5주차/prgrms_[1차] 뉴스 클러스터링/Solution.java
+++ May/5주차/prgrms_[1차] 뉴스 클러스터링/Solution.java
@@ -1,6 +1,31 @@
+import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
+        List<String> str1_list = new ArrayList<>();
+        List<String> str2_list = new ArrayList<>();
+
+        for(int i=0; i<str1.length()-1; i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i+1);
+            if(!Character.isAlphabetic(c1) || !Character.isAlphabetic(c2)) continue;                
+            str1_list.add(String.valueOf(c1 +""+ c2).toUpperCase());
+        }
+        for(int i=0; i<str2.length()-1; i++) {
+            char c1 = str2.charAt(i);
+            char c2 = str2.charAt(i+1);
+            if(!Character.isAlphabetic(c1) || !Character.isAlphabetic(c2)) continue;
+            str2_list.add(String.valueOf(c1 +""+ c2).toUpperCase());       
+        }
+        
+        List<String> cha = new ArrayList<>(str1_list);
+        for(String s : str2_list) cha.remove(s); // 차집합 구하기
+        
+        int hab_size = str2_list.size() + cha.size();
+        int gyo_size = str1_list.size() - cha.size();
+        if(hab_size == 0) answer = 65536;
+        else answer = (int)(gyo_size / (double)hab_size * 65536);   
+        
         return answer;
     }
 }
