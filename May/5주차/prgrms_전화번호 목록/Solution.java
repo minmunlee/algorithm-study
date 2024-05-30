99bae925e0117f355bded8b8e300ee6febdd29e5
ChaeAg <dydkglel123@gmail.com>
2024-05-30 16:08:39 +0900

    solve: prgrms_전화번호 목록

--- May/5주차/prgrms_전화번호 목록/Solution.java
+++ May/5주차/prgrms_전화번호 목록/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 class Solution {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+    public boolean solution(String[] phone_book) {     
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
