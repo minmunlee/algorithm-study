f20084a831a0b5674f842cb9e37fc86f2f03f8c5
gitseoyeon <tjdus553926@gmail.com>
2024-05-31 19:34:05 +0900

    solve: boj_점프왕 쩰리

--- May/5주차/boj_점프왕 쩰리 (Small)/Main.java
+++ May/5주차/boj_점프왕 쩰리 (Small)/Main.java
@@ -1,3 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        if (bfs(map, N)) {
+            System.out.println("HaruHaru");
+        } else {
+            System.out.println("Hing");
+        }
+    }
+
+    public static boolean bfs(int[][] map, int N) {
+        boolean[][] check = new boolean[N][N];
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[]{0, 0});
+        check[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] loacate = queue.poll();
+            int x = loacate[0];
+            int y = loacate[1];
+            int jump = map[x][y];
+
+            if (map[x][y] == -1) {
+                return true; // 승리 지점 도달
+            }
+
+            // 오른쪽으로 이동
+            int nx = x;
+            int ny = y + jump;
+            if (ny < N && !check[nx][ny]) { // 방문하지 않았고 map 범위를 벗어나지 않았을때
+                queue.add(new int[]{nx, ny});
+                check[nx][ny] = true;
+            }
+
+            // 아래쪽으로 이동
+            nx = x + jump;
+            ny = y;
+            if (nx < N && !check[nx][ny]) {
+                queue.add(new int[]{nx, ny});
+                check[nx][ny] = true;
+            }
+        }
+
+        return false;
+    }
 }
