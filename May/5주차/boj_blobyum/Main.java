e0aed3a0cdbfc7bfa933048abf519ca269faa9fd
gitseoyeon <tjdus553926@gmail.com>
2024-05-31 19:33:01 +0900

    solve: boj_blobyum

--- May/5주차/boj_blobyum/Main.java
+++ May/5주차/boj_blobyum/Main.java
@@ -1,3 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer str = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(str.nextToken());
+    int K = Integer.parseInt(str.nextToken());
+    int maxSum = 0;
+    int sum = 0;
+
+    int[] taste = new int[N];
+    int left = 0;
+    int right = K - 1;
+
+    str = new StringTokenizer(br.readLine());
+    for(int i = 0; i < N; i++){
+        taste[i] = Integer.parseInt(str.nextToken());
+    }
+
+    // 초기 합 계산
+    for(int i = 0; i < K; i++){
+        sum += taste[i];
+    }
+
+    maxSum = sum;
+
+    while(true){
+      sum -= taste[left];
+      left = (left + 1) % N;
+      right = (right + 1) % N;
+      sum += taste[right];
+      if(left == 0){
+        break;
+      }
+      maxSum = Math.max(maxSum, sum);
+     }
+
+    System.out.println(maxSum);
+  }
 }
