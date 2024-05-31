62fd53096d449d45b10ebb447aea0dfa94af3dd9
gitseoyeon <tjdus553926@gmail.com>
2024-05-31 19:34:45 +0900

    solve: prgrms_전화번호 목록

--- May/5주차/prgrms_전화번호 목록/Solution.java
+++ May/5주차/prgrms_전화번호 목록/Solution.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        
+        Arrays.sort(phone_book); // 시간 초과 문제 해결
+        
+        for(int i = 1; i < phone_book.length; i++){
+            if(phone_book[i].startsWith(phone_book[i - 1])){
+                answer = false;
+                break;
+            }
+        }
         return answer;
     }
 }
