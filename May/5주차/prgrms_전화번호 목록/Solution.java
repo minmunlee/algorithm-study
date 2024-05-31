137d08f8ba2e2fda2b6b936e3e58785038e9177a
siwon <muncool39@gmail.com>
2024-05-31 19:25:48 +0900

    solve: prgrms_전화번호 목록

--- May/5주차/prgrms_전화번호 목록/Solution.java
+++ May/5주차/prgrms_전화번호 목록/Solution.java
@@ -1,6 +1,10 @@
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+        Arrays.sort(phone_book);
+        for(int i=1;i<phone_book.length;i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) return false;
+        }
+        return true;
     }
 }
