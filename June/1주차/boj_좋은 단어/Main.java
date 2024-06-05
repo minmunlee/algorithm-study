46f597602ef0dfdf25022e13de0b175474c56643
ChaeAg <dydkglel123@gmail.com>
2024-06-05 10:49:40 +0900

    solve: boj_좋은 단어

--- June/1주차/boj_좋은 단어/Main.java
+++ June/1주차/boj_좋은 단어/Main.java
@@ -1,3 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(int i=0; i<N; i++) {
+            Stack<String> stack = new Stack<>();
+            String str = br.readLine();
+
+            for(int j=0; j<str.length(); j++) {
+                String s = str.substring(j, j+1);
+                if(!stack.isEmpty() && s.equals(stack.peek())) {
+                    stack.pop();
+                    continue;
+                }
+                stack.push(s);
+            }
+
+            if(stack.isEmpty()) count++;
+        }
+
+        System.out.print(count);
+    }
 }
