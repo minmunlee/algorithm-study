7b368a3ee3766e6f5302c367c6638a7c0e141490
ChaeAg <dydkglel123@gmail.com>
2024-06-05 10:50:17 +0900

    solve: prgrms_타겟 넘버

--- June/1주차/prgrms_타겟 넘버/Solution.java
+++ June/1주차/prgrms_타겟 넘버/Solution.java
@@ -1,6 +1,21 @@
 class Solution {
+    static int[] n;
+    static int t;
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        n = numbers;
+        t = target;
+        dfs(0, 0);
         return answer;
+    }
+    
+    static void dfs(int idx, int sum) {
+        if(idx == n.length) {
+            if(t == sum) answer++;
+            return;
+        }
+        
+        dfs(idx+1, sum + n[idx]);
+        dfs(idx+1, sum - n[idx]);
     }
 }
