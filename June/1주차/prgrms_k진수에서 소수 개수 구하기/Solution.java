d5df332b87fa9e0b2a81fa47478186dad218072e
gitseoyeon <tjdus553926@gmail.com>
2024-06-06 20:30:32 +0900

    solve: prgrms_k진수에서 소수 개수 구하기

--- June/1주차/prgrms_k진수에서 소수 개수 구하기/Solution.java
+++ June/1주차/prgrms_k진수에서 소수 개수 구하기/Solution.java
@@ -1,6 +1,35 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = -1;
+        int answer = 0;
+        String kJinsu = Integer.toString(n, k);
+        
+        String[] arr = kJinsu.split("0");
+        
+        for(String s : arr){
+            if(s.equals("")){
+                continue;
+            }
+            
+            long number = Long.parseLong(s); // int로 하면 런타임 에러
+            
+            if(isPrime(number)){
+                answer++;
+            }
+        }
+        
         return answer;
+    }
+    
+    private static boolean isPrime(long number){
+        if(number <= 1){
+            return false;
+        }
+        
+        for(int i = 2; i <= (int)Math.sqrt(number); i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
