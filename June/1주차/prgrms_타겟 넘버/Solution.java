bd175c815386959f2d0f05cc972ce6b513731e9b
gitseoyeon <tjdus553926@gmail.com>
2024-06-06 20:30:58 +0900

    solve: prgrms_타겟 넘버

--- June/1주차/prgrms_타겟 넘버/Solution.java
+++ June/1주차/prgrms_타겟 넘버/Solution.java
@@ -1,6 +1,21 @@
 class Solution {
+    public static int answer;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        
+        dfs(numbers, 0, 0, target);
+        
         return answer;
+    }
+    
+    private void dfs(int[] numbers, int idx, int value, int target){
+        if(numbers.length == idx){
+            if(value == target){
+                answer++;
+            }   
+            return;
+        }
+        
+        dfs(numbers, idx + 1, value + numbers[idx], target); // 더할 경우
+        dfs(numbers, idx + 1, value - numbers[idx], target); // 뺄 경우
     }
 }
