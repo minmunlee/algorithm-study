b22cf13a6563ba0320578ecba8863a3e86f2bc96
ChaeAg <dydkglel123@gmail.com>
2024-06-11 14:25:13 +0900

    solve: boj_누울 자리를 찾아라

--- June/2주차/boj_누울 자리를 찾아라/Main.java
+++ June/2주차/boj_누울 자리를 찾아라/Main.java
@@ -1,3 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] room = new char[N][N];
+        for(int i=0; i<N; i++) {
+            room[i] = br.readLine().toCharArray();
+        }
+
+        int garo = 0, sero = 0;
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(room[i][j] == 'X') continue;
+                if(j+1 < N && room[i][j+1] == '.') {
+                    garo++;
+                    while(j < N && room[i][j] == '.') {
+                        j++;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(room[j][i] == 'X') continue;
+                if(j+1 < N && room[j+1][i] == '.') {
+                    sero++;
+                    while(j < N && room[j][i] == '.') {
+                        j++;
+                    }
+                }
+            }
+        }
+
+        System.out.print(garo + " " + sero);
+    }
 }
