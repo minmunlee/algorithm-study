3526fe3dc839ff34ccca0c7ca2bfe236ffc978b5
ChaeAg <dydkglel123@gmail.com>
2024-06-11 14:25:33 +0900

    solve: prgrms_[3차] 압축

--- June/2주차/prgrms_[3차] 압축/Solution.java
+++ June/2주차/prgrms_[3차] 압축/Solution.java
@@ -1,6 +1,30 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(String msg) {
-        int[] answer = {};
-        return answer;
+        List<Integer> answer = new ArrayList<>();
+        List<String> library = new ArrayList<>();
+
+        for (char c = 'A'; c <= 'Z'; c++) { // 기본 사전 세팅
+            library.add(String.valueOf(c));
+        }
+
+        int len = msg.length();
+
+        for (int i = 0; i < len; i++) {
+            int end = len; 
+            while (!library.contains(msg.substring(i, end))) {
+                end--;
+            } // 일치하는 문자열(w)을 찾았을 경우 빠져나옴
+            answer.add(library.indexOf(msg.substring(i, end)) + 1); // 색인 번호 기록
+
+            if (end == len) break; // 마지막 문자까지 처리 했을 경우
+
+            library.add(msg.substring(i, end + 1)); // w+c 단어 사전에 등록
+            i = end - 1;
+        }
+        
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
