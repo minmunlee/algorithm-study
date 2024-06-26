b7667dc0a2d53edb52329f685baa2260ed671a3f
ChaeAg <dydkglel123@gmail.com>
2024-06-26 16:50:01 +0900

    solve: prgrms_[3차] n진수 게임

--- June/3주차/prgrms_[3차] n진수 게임/Solution.java
+++ June/3주차/prgrms_[3차] n진수 게임/Solution.java
@@ -1,6 +1,27 @@
+import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-        return answer;
+        int num = 0;
+        int count = 0;
+        List<String> list = new ArrayList<>();
+        int idx = 0;
+        int next = p - 1;
+        
+        while(count < t) {
+            for(String str : Integer.toString(num, n).split("")) {
+                list.add(str);
+            }
+            
+            while(list.size() > next && count < t) {
+                answer += list.get(next);
+                count++;
+                next += m;
+            }
+            
+            num++;
+        }
+        
+        return answer.toUpperCase();
     }
 }
