a40d1681e0cbd96248ce2367ec87b46232ae4dfe
ChaeAg <dydkglel123@gmail.com>
2024-06-26 16:49:41 +0900

    solve: boj_인사성 밝은 곰곰이

--- June/3주차/boj_인사성 밝은 곰곰이/Main.java
+++ June/3주차/boj_인사성 밝은 곰곰이/Main.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<String> person = new HashSet<>();
+        int count = 0;
+
+        for(int i=0; i<N; i++) {
+            String str = br.readLine();
+            if(str.equals("ENTER")) {
+                count += person.size();
+                person.clear();
+                continue;
+            }
+            person.add(str);
+        }
+
+        System.out.print(count + person.size());
+    }
 }
