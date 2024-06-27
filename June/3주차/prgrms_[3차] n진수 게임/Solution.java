2f26f687bcd38a7251988a546c3621b4fa0d45d5
gitseoyeon <tjdus553926@gmail.com>
2024-06-27 17:36:22 +0900

    solve: prgrms_[3차] n진수 게임

--- June/3주차/prgrms_[3차] n진수 게임/Solution.java
+++ June/3주차/prgrms_[3차] n진수 게임/Solution.java
@@ -1,6 +1,25 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
+        // 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
         String answer = "";
-        return answer;
+        
+        int num = 0; // 현재 숫자
+        int count = 0; // 말한 숫자의 개수
+        int position = 0; // 현재 순서
+
+        while (count < t) {
+            String str = Integer.toString(num, n);
+            for (int i = 0; i < str.length(); i++) {
+                if (position % m == p - 1) { // 튜브 차례
+                    answer += str.charAt(i);
+                    count++;
+                    if (count == t) break;
+                }
+                position++;
+            }
+            num++;
+        }
+        
+        return answer.toUpperCase();
     }
 }
