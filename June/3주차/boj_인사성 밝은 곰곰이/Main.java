a1135ea1587de5b8556c9ec3797a19ca12fd98ba
gitseoyeon <tjdus553926@gmail.com>
2024-06-27 17:35:43 +0900

    solve: boj_인사성 밝은 곰곰이

--- June/3주차/boj_인사성 밝은 곰곰이/Main.java
+++ June/3주차/boj_인사성 밝은 곰곰이/Main.java
@@ -1,3 +1,27 @@
-public class Main {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        HashSet<String> hs = new HashSet<>(); // list 썼더니 시간초과
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            if(str.equals("ENTER")){
+                cnt += hs.size();
+                hs.clear();
+            }else{
+                hs.add(str);
+            }
+        }
+
+        int total = cnt + hs.size();
+
+        System.out.println(total);
+    }
 }
