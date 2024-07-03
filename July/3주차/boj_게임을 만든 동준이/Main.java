0b5d59cef3ad61155ecc127c31c4eedfa2aad496
ChaeAg <dydkglel123@gmail.com>
2024-07-04 03:01:58 +0900

    solve: boj_게임을 만든 동준이

--- July/3주차/boj_게임을 만든 동준이/Main.java
+++ July/3주차/boj_게임을 만든 동준이/Main.java
@@ -1,3 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] scores = new int[N];
+        for(int i=0; i<N; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for(int i=N-2; i>=0; i--) {
+            while(scores[i] >= scores[i+1]) {
+                scores[i]--;
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
 }
