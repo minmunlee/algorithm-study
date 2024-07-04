9aa2ecf4a06d41c2c0516175782ecca11e59bf2d
ChaeAg <dydkglel123@gmail.com>
2024-07-04 16:21:52 +0900

    solve: prgrms_롤케이크 자르기

--- July/3주차/prgrms_롤케이크 자르기/Solution.java
+++ July/3주차/prgrms_롤케이크 자르기/Solution.java
@@ -1,6 +1,27 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] topping) {
-        int answer = -1;
+        int answer = 0;
+        int[] cheolsu = new int[10001];
+        int[] brother = new int[10001];
+        int c_count = 0;
+        int b_count = 0;
+        for(int t : topping) {
+            if(cheolsu[t] == 0) c_count++;
+            cheolsu[t]++;
+        }
+        
+        for(int i=topping.length-1; i>0; i--) {
+            int t = topping[i];
+            if(brother[t] == 0) b_count++;
+            brother[t]++;
+            cheolsu[t]--;
+            if(cheolsu[t] == 0) c_count--;
+            if(b_count == c_count) answer++;
+        }
+        
         return answer;
     }
 }
