9de915b5cac3013ccdad17346281773eab4720f4
gitseoyeon <tjdus553926@gmail.com>
2024-07-04 15:10:35 +0900

    solve: prgrms_뒤에 있는 큰 수 찾기

--- July/3주차/prgrms_뒤에 있는 큰 수 찾기/Solution.java
+++ July/3주차/prgrms_뒤에 있는 큰 수 찾기/Solution.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = numbers.length - 1; i >= 0; i--){
+            while(!stack.isEmpty() && stack.peek() <= numbers[i]){ // 현재 수보다 작을 경우
+                stack.pop();
+            }
+            
+            if(stack.isEmpty()){
+                answer[i] = -1;
+            }else{
+                answer[i] = stack.peek();
+            }
+            
+            stack.push(numbers[i]);
+        }
+
         return answer;
     }
 }
