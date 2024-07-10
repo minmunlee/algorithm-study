1773a1c036ba50bfdb9dce607ee296250e353ed6
ChaeAg <dydkglel123@gmail.com>
2024-07-11 03:37:01 +0900

    solve: prgrms_땅따먹기

--- July/2주차/prgrms_땅따먹기/Solution.java
+++ July/2주차/prgrms_땅따먹기/Solution.java
@@ -1,8 +1,26 @@
+import java.util.*;
+
 class Solution {
     int solution(int[][] land) {
-        int answer = 0;
-        System.out.println("Hello Java");
-
-        return answer;
+        int[][] dp = new int[land.length][4];
+        dp[0] = land[0];
+        
+        for(int i=1; i<land.length; i++) {
+            for(int j=0; j<4; j++) {
+                dp[i][j] = land[i][j];
+                int max = 0;
+                
+                for(int k=0; k<4; k++) {
+                    if(k == j) continue;
+                    max = max > dp[i-1][k] ? max : dp[i-1][k];
+                }
+                
+                dp[i][j] += max;
+            }
+        }
+        
+        return Arrays.stream(dp[land.length-1])
+            .max()
+            .getAsInt();
     }
 }
