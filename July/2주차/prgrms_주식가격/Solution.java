e40382dcb83ca8bee446f66a0da07d1332630d16
ChaeAg <dydkglel123@gmail.com>
2024-07-11 03:37:12 +0900

    solve: prgrms_주식가격

--- July/2주차/prgrms_주식가격/Solution.java
+++ July/2주차/prgrms_주식가격/Solution.java
@@ -1,6 +1,29 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
+        Stack<Integer> idx_stack = new Stack<>();
+        idx_stack.push(0);
+
+        int idx = 1;
+        boolean done = false;
+        int count = 0;
+        while(count < prices.length) {
+            if(idx_stack.isEmpty() || !done && idx < prices.length && prices[idx_stack.peek()] <= prices[idx]) {
+                idx_stack.push(idx);
+                idx++;
+                continue;
+            }
+            if(idx == prices.length) {
+                done = true;
+                idx--;
+            }
+            int pop_idx = idx_stack.pop();
+            int un_down_time = idx - pop_idx;
+            answer[pop_idx] = un_down_time;
+            count++;
+        }
+        
         return answer;
     }
 }
