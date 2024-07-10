d6d510618405c2217b01ff42e54ca705b1113558
ChaeAg <dydkglel123@gmail.com>
2024-07-11 03:36:50 +0900

    solve: boj_피보나치 비스무리한 수열

--- July/2주차/boj_피보나치 비스무리한 수열/Main.java
+++ July/2주차/boj_피보나치 비스무리한 수열/Main.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] fibo = new long[117];
+        fibo[1] = 1;
+        fibo[2] = 1;
+        fibo[3] = 1;
+
+        for(int i=4; i<=n; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-3];
+        }
+
+        System.out.print(fibo[n]);
+    }
 }
