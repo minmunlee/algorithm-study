10000306d1118bfca9195f7caa52589d52ae6902
gitseoyeon <tjdus553926@gmail.com>
2024-07-11 20:15:35 +0900

    sovle: prgrms_땅따먹기

--- July/2주차/prgrms_땅따먹기/Solution.java
+++ July/2주차/prgrms_땅따먹기/Solution.java
@@ -1,7 +1,17 @@
+import java.util.*;
+
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-        System.out.println("Hello Java");
+        
+        for(int i = 0; i < land.length - 1; i++){
+            land[i + 1][0] += Math.max(Math.max(land[i][1], land[i][2]), land[i][3]);
+            land[i + 1][1] += Math.max(Math.max(land[i][0], land[i][2]), land[i][3]);
+            land[i + 1][2] += Math.max(Math.max(land[i][0], land[i][1]), land[i][3]);
+            land[i + 1][3] += Math.max(Math.max(land[i][0], land[i][1]), land[i][2]);
+        }
+        
+        answer = Arrays.stream(land[land.length - 1]).max().getAsInt();
 
         return answer;
     }
