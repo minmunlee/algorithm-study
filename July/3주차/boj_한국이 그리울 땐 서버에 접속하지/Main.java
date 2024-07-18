e61ef5402606fbd97bf1e9f2e1fea834d96b6065
gitseoyeon <tjdus553926@gmail.com>
2024-07-18 14:19:27 +0900

    solve: boj_한국이 그리울 땐 서버에 접속하지

--- July/3주차/boj_한국이 그리울 땐 서버에 접속하지/Main.java
+++ July/3주차/boj_한국이 그리울 땐 서버에 접속하지/Main.java
@@ -1,3 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] pattern = br.readLine().split("\\*");
+
+
+        for(int i = 0; i < N; i++){
+            String s = br.readLine();
+            if((s.length() < pattern[0].length() + pattern[1].length())){
+                System.out.println("NE");
+            }else{
+                if(s.substring(0, pattern[0].length()).equals(pattern[0]) && s.substring(s.length() - pattern[1].length()).equals(pattern[1])){
+                    System.out.println("DA");
+                }else{
+                    System.out.println("NE");
+                }
+            }
+        }
+    }
     
 }
