70ac514947ad6cb2ac35e30e8542938195b2651f
ChaeAg <dydkglel123@gmail.com>
2024-07-18 10:20:30 +0900

    solve: prgrms_스킬트리

--- July/3주차/prgrms_스킬트리/Solution.java
+++ July/3주차/prgrms_스킬트리/Solution.java
@@ -1,6 +1,26 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
+        int answer = 0;
+        
+        for(String skill_tree : skill_trees) {
+            int idx = 0;
+            String word = skill.substring(idx, idx+1);
+            boolean isFalse = false;
+            for(String s : skill_tree.split("")) {
+                if(!word.equals(s) && skill.contains(s)) {
+                    isFalse = true;
+                    break;
+                }
+                if(word.equals(s)) {
+                    idx++;
+                    if(idx >= skill.length()-1) break;
+                    word = skill.substring(idx, idx+1);
+                }
+            }
+            
+            if(!isFalse) answer++;
+        }
+        
         return answer;
     }
 }
