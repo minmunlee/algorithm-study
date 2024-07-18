e53d0ace24e05a2cba9e2160ef0b2ab74e97f74c
ChaeAg <dydkglel123@gmail.com>
2024-07-18 10:20:22 +0900

    solve: prgrms_더 맵게

--- July/3주차/prgrms_더 맵게/Solution.java
+++ July/3주차/prgrms_더 맵게/Solution.java
@@ -1,6 +1,27 @@
+import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for(int i : scoville) {
+            queue.add(i);
+        }
+        
+        while(true) {
+            int num1 = queue.poll();
+            if(num1 >= K) break;
+            int num2 = queue.poll();
+            
+            queue.add(num1 + num2*2);
+            answer++;
+            
+            if(queue.size() == 1) {
+                if(queue.poll() < K) answer = -1;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
