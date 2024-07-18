14a290b266e917353ec1f7af9e542d8ac290c8f9
gitseoyeon <tjdus553926@gmail.com>
2024-07-18 14:19:52 +0900

    solve: prgrms_스킬트리

--- July/3주차/prgrms_스킬트리/Solution.java
+++ July/3주차/prgrms_스킬트리/Solution.java
@@ -1,6 +1,30 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
+        int answer = skill_trees.length;
+        
+        for(String s : skill_trees){
+            String[] st = s.split("");
+            String first = skill.substring(0, 1);
+            String rest = skill.substring(1);
+            
+            for(int i = 0; i < st.length; i++){
+                if(rest.contains(st[i])){
+                    answer--;
+                    break;
+                }
+                
+                if(st[i].equals(first)){
+                    if(rest.length() > 1){
+                        first = rest.substring(0, 1);
+                        rest = rest.substring(1);
+                    }else{
+                        first = rest;
+                        rest = "";
+                    }
+                }
+            }
+        }
+        
         return answer;
     }
 }
