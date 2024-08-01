d4634ab6e3dd3419c6001bee3d632873f2f78b8c
gitseoyeon <tjdus553926@gmail.com>
2024-08-01 15:05:21 +0900

    solve: prgrms_택배상자

--- August/1주차/prgrms_택배상자/Solution.java
+++ August/1주차/prgrms_택배상자/Solution.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
+        Stack<Integer> bojo = new Stack<>();
+        int idx = 0;
+        
+        for(int i = 0; i < order.length; i++){
+            bojo.push(i + 1);
+            while(!bojo.isEmpty()){
+                if(bojo.peek() == order[idx]){
+                    bojo.pop();
+                    idx++;
+                    answer++;
+                }else{
+                    break;
+                }
+            }
+        }
         return answer;
     }
 }
