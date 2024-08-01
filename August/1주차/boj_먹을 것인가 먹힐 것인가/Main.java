c6ff68889f4c8f16fc73870bfecce638634f748e
ChaeAg <dydkglel123@gmail.com>
2024-08-01 15:40:34 +0900

    solve: boj_먹을 것인가 먹힐 것인가

--- August/1주차/boj_먹을 것인가 먹힐 것인가/Main.java
+++ August/1주차/boj_먹을 것인가 먹힐 것인가/Main.java
@@ -1,3 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int[] A = new int[N];
+            int[] B = new int[M];
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                A[j] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                B[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            int result = 0;
+
+            for(int n1 : A) {
+                for(int n2 : B) {
+                    if(n1 <= n2) break;
+                    result += 1;
+                }
+            }
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
