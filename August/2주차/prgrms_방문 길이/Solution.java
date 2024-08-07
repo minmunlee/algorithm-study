4213e750d100b802b2010f905e897b5ea3e643e5
gitseoyeon <tjdus553926@gmail.com>
2024-08-07 14:13:28 +0900

    solve: prgrms_방문 길이

--- August/2주차/prgrms_방문 길이/Solution.java
+++ August/2주차/prgrms_방문 길이/Solution.java
@@ -1,6 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int solution(String dirs) {
-        int answer = 0;
-        return answer;
+        Set<String> set = new HashSet<>();
+
+        int x = 0;
+        int y = 0;
+
+        String[] str = dirs.split("");
+
+        for(String s : str){
+            String before = Integer.toString(x) + Integer.toString(y);
+            switch (s) {
+                case "U":
+                    if(y + 1 > 5)
+                        continue;
+                    y++;
+                    break;
+                case "D":
+                    if(y - 1 < -5)
+                        continue;
+                    y--;
+                    break;
+                case "R":
+                    if(x + 1 > 5)
+                        continue;
+                    x++;
+                    break;
+                case "L":
+                    if(x - 1 < -5)
+                        continue;
+                    x--;
+                    break;
+            }
+
+            String after = Integer.toString(x) + Integer.toString(y);
+
+            set.add(before + after);
+            set.add(after + before);
+        }
+        return set.size() / 2;
     }
 }
