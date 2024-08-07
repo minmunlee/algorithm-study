29ce8b8f07e5d5961a29d689efa714d3c5537c1a
gitseoyeon <tjdus553926@gmail.com>
2024-08-07 14:13:00 +0900

    solve: boj_노드사이의 거리

--- August/2주차/boj_노드사이의 거리/Main.java
+++ August/2주차/boj_노드사이의 거리/Main.java
@@ -1,3 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    static int[][] node;
+    static boolean[] visit;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+        node = new int[N + 1][N + 1];
+
+        for(int i = 1; i < N; i++){
+            str = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(str.nextToken());
+            int e = Integer.parseInt(str.nextToken());
+            int distance = Integer.parseInt(str.nextToken());
+
+            node[s][e] = distance;
+            node[e][s] = distance;
+
+        }
+
+        // 거리를 알고 싶은 M개의 노드 쌍
+        for(int i = 0; i < M; i++){
+            visit = new boolean[N + 1];
+            str = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(str.nextToken());
+            int e = Integer.parseInt(str.nextToken());
+
+            System.out.println(bfs(s, e, visit));
+        }
+
+    }
+
+    public static int bfs(int s, int e, boolean[] visit){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{s, 0}); // 현재 노드 위치, 거리 저장
+        visit[s] = true;
+
+        while(!queue.isEmpty()){
+            int[] arr = queue.poll();
+            int currentNode = arr[0];
+            int distance = arr[1];
+
+            if(currentNode == e){
+                return distance;
+            }
+
+            for(int i = 1; i <= N; i++){
+                if(node[currentNode][i] != 0 && !visit[i]){
+                    queue.add(new int[]{i, (distance + node[currentNode][i])}); // 시작 노드에서 i 노드까지의 거리 합
+                    visit[i] = true;
+                }
+            }
+        }
+        return -1;
+    }
 }
