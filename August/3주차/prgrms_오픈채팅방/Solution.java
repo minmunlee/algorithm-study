2c7104a72333360dd7a7858d1d56f45dd24f09a6
ChaeAg <dydkglel123@gmail.com>
2024-08-14 16:32:20 +0900

    solve: prgrms_오픈채팅방

--- August/3주차/prgrms_오픈채팅방/Solution.java
+++ August/3주차/prgrms_오픈채팅방/Solution.java
@@ -1,6 +1,31 @@
+import java.util.*;
 class Solution {
+    String[] format = {"%s님이 들어왔습니다.", "%s님이 나갔습니다."};
+    
     public String[] solution(String[] record) {
-        String[] answer = {};
-        return answer;
+        Map<String, String> nick = new HashMap<>();
+        
+        for(String str : record) {
+            String[] temp = str.split(" ");
+            
+            if(!temp[0].equals("Leave")) {
+                nick.put(temp[1], temp[2]);
+            }
+        }
+    
+        List<String> result = new ArrayList<>();
+        
+        for(String str : record) {
+            String[] temp = str.split(" ");
+            
+            if(temp[0].equals("Enter")) {
+                result.add(String.format(format[0], nick.get(temp[1])));
+            }
+            else if(temp[0].equals("Leave")) {
+                result.add(String.format(format[1], nick.get(temp[1])));
+            }
+        }
+        
+        return result.toArray(new String[result.size()]);
     }
 }
