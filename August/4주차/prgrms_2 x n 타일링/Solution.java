ff2e0270b2337d9001fb35fe1e722da1ac16f790
ChaeAg <dydkglel123@gmail.com>
2024-08-22 02:24:10 +0900

    solve: prgrms_2 x n 타일링

--- August/4주차/prgrms_2 x n 타일링/Solution.java
+++ August/4주차/prgrms_2 x n 타일링/Solution.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int[] dp = new int[60001];
+        dp[1] = 1; dp[2] = 2;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        
+        return dp[n];  
     }
 }
