06d3691d1f4a80fdb20dd08c6e013e1748c96444
ChaeAg <dydkglel123@gmail.com>
2024-08-30 16:34:25 +0900

    solve: boj_수열 정렬

--- August/5주차/boj_수열 정렬/Main.java
+++ August/5주차/boj_수열 정렬/Main.java
@@ -1,3 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] result = new int[N];
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[][] idxResister = new int[N][2];
+
+        for(int i=0; i<N; i++) {
+            idxResister[i][0] = arr[i];
+            idxResister[i][1] = i;
+        }
+
+        Arrays.sort(idxResister, (o1, o2) -> {
+            if(o1 == o2) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        for(int i=0; i<N; i++) {
+            result[idxResister[i][1]] = i;
+        }
+
+        for(int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
