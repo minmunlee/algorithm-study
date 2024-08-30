790e6942fffcdd590f2dc148c073d10b0ee7ac64
ChaeAg <dydkglel123@gmail.com>
2024-08-30 16:34:55 +0900

    solve: prgrms_2개 이하로 다른 비트

--- August/5주차/prgrms_2개 이하로 다른 비트/Solution.java
+++ August/5주차/prgrms_2개 이하로 다른 비트/Solution.java
@@ -1,6 +1,27 @@
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = {};
+        long[] answer = new long[numbers.length];
+
+        for(int i=0; i<numbers.length; i++) {
+            String[] strs = Long.toBinaryString(numbers[i]).split("");
+            String result = "";
+            
+            for(int j=strs.length-1; j>=0; j--) {
+                if(strs[j].equals("0")) {
+                    strs[j] = "1";
+                    if(j < strs.length-1) strs[j+1] = "0"; // 더 작은 수를 만들어주기 위함
+                    result = String.join("", strs);
+                    break;
+                }
+
+                if(j == 0) { // 2진수의 모든 비트가 1일 경우에 여기 도달
+                    strs[j] = "0";
+                    result = "1" + String.join("", strs);
+                }
+            }
+            answer[i] = Long.parseLong(result, 2);
+        }
+        
         return answer;
     }
 }
