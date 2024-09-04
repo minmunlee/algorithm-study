0a4e7e713fdf2ed078df208770c1d30414d13cb0
ChaeAg <dydkglel123@gmail.com>
2024-09-04 17:21:08 +0900

    solve: boj_붙임성 좋은 총총이

--- September/1주차/boj_붙임성 좋은 총총이/Main.java
+++ September/1주차/boj_붙임성 좋은 총총이/Main.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
+
+        for(int i=0; i<N; i++) {
+            String[] str = br.readLine().split(" ");
+            if(set.contains(str[0])) {
+                set.add(str[1]);
+            }
+            else if(set.contains(str[1])) {
+                set.add(str[0]);
+            }
+        }
+
+        System.out.print(set.size());
+    }
 }
