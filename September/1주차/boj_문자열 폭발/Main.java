e2796cdc4d80c152a84bc12cc12ac8e75d46c5c0
gitseoyeon <tjdus553926@gmail.com>
2024-09-04 15:21:44 +0900

    solve: boj_문자열 폭발

--- September/1주차/boj_문자열 폭발/Main.java
+++ September/1주차/boj_문자열 폭발/Main.java
@@ -1,3 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String bomb = br.readLine();
+
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            str.append(s.charAt(i));
+            if (str.length() >= bomb.length() && str.substring(str.length() - bomb.length()).equals(bomb)) {
+                str.delete(str.length() - bomb.length(), str.length());
+            }
+        }
+
+        if (str.length() == 0) {
+            System.out.println("FRULA");
+        } else {
+            System.out.println(str.toString());
+        }
+    }
 }
