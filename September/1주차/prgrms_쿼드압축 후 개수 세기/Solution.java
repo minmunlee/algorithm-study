c8912b127538cc0088865ff2d43de7a0a1447396
gitseoyeon <tjdus553926@gmail.com>
2024-09-04 15:22:23 +0900

    solve: prgrms_쿼드압축 후 개수 세기

--- September/1주차/prgrms_쿼드압축 후 개수 세기/Solution.java
+++ September/1주차/prgrms_쿼드압축 후 개수 세기/Solution.java
@@ -1,6 +1,37 @@
 class Solution {
+    static int[] answer;
+  
     public int[] solution(int[][] arr) {
-        int[] answer = {};
+        answer = new int[2];
+        divide(0, 0, arr.length, arr);
         return answer;
+
     }
+    public static void divide(int startX, int startY, int l, int[][] arr){
+         if(check(startX, startY, l, arr)){
+            if(arr[startX][startY] == 0){
+                answer[0]++;
+            }else{
+                answer[1]++;
+            }
+             return;
+        }
+        
+        divide(startX, startY, l/2, arr);
+        divide(startX, startY + l/2, l/2, arr);
+        divide(startX + l/2, startY, l/2, arr);
+        divide(startX + l/2, startY + l/2, l/2 , arr);
+    }
+    public static boolean check(int startX, int startY, int l, int[][] arr){
+        int n = arr[startX][startY];
+        for(int i = startX; i < startX + l; i++){
+            for(int j = startY; j < startY + l; j++){
+                if(arr[i][j] != n)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
 }
