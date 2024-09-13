e3cbbf22d8cc1614eef8c5c8b3fc6759f2e19cd0
gitseoyeon <tjdus553926@gmail.com>
2024-09-13 13:38:16 +0900

    solve: prgrms_다리를 지나는 트럭

--- September/2주차/prgrms_다리를 지나는 트럭/Solution.java
+++ September/2주차/prgrms_다리를 지나는 트럭/Solution.java
@@ -1,6 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        int w = 0; // 다리 위 트럭 무게
+        int idx = 0; // 대기 중인 트럭
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.add(0);
+        }
+        
+        while (idx < truck_weights.length || w > 0) {
+            answer++;
+            w -= bridge.poll();
+
+            if (idx < truck_weights.length && w + truck_weights[idx] <= weight) {
+                bridge.add(truck_weights[idx]);
+                w += truck_weights[idx];
+                idx++;
+            } else {
+                bridge.add(0);
+            }
+        }
+        
         return answer;
     }
 }
