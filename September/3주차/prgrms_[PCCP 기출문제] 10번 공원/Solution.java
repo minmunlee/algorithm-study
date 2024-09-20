9ad81b2220ca4f37b17d99f646f05bd66a65f267
ChaeAg <dydkglel123@gmail.com>
2024-09-20 20:47:33 +0900

    solve: prgrms_[PCCP 기출문제] 10번 공원

--- September/3주차/prgrms_[PCCP 기출문제] 10번 공원/Solution.java
+++ September/3주차/prgrms_[PCCP 기출문제] 10번 공원/Solution.java
@@ -1,6 +1,29 @@
+import java.util.*;
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        int answer = -1;
+        Arrays.sort(mats);
+
+        for(int i=0; i<park.length; i++) {
+            for(int j=0; j<park[0].length; j++) {
+                for(int k=0; k<mats.length; k++) {
+                    boolean isOk = true;
+                    for(int k1=0; k1<mats[k]; k1++) {
+                        for(int k2=0; k2<mats[k]; k2++) {
+                            if(i+k1 >= park.length || j+k2 >= park[0].length || !park[i+k1][j+k2].equals("-1")) {
+                                isOk = false;
+                                break;
+                            }
+                        }
+                        if(!isOk) break;
+                    }
+                    if(!isOk) break;
+
+                    answer = answer > mats[k] ? answer : mats[k]; // 이 돗자리 성공
+                }
+            }
+        }
+
         return answer;
     }
 }
