8651a2d43c7effb78424cd34c5d61561f2b0571a
ChaeAg <dydkglel123@gmail.com>
2024-09-20 20:47:51 +0900

    solve: prgrms_[PCCP 기출문제] 9번 지폐 접기

--- September/3주차/prgrms_[PCCP 기출문제] 9번 지폐 접기/Solution.java
+++ September/3주차/prgrms_[PCCP 기출문제] 9번 지폐 접기/Solution.java
@@ -1,6 +1,36 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
+
+        if(wallet[0] < wallet[1]) {
+            int temp = wallet[0];
+            wallet[0] = wallet[1];
+            wallet[1] = temp;
+        }
+
+        if(bill[0] < bill[1]) {
+            int temp = bill[0];
+            bill[0] = bill[1];
+            bill[1] = temp;
+        }
+
+        while(wallet[0] < bill[0] || wallet[1] < bill[1]) {
+            if(bill[0] > bill[1]) {
+                bill[0] /= 2;
+            }
+            else {
+                bill[1] /= 2;
+            }
+            
+            answer++;
+            
+            if(bill[0] < bill[1]) {
+                int temp = bill[0];
+                bill[0] = bill[1];
+                bill[1] = temp;
+            }
+        }
+        
         return answer;
     }
 }
