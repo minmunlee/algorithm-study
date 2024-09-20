122008a67687054f7126ce2e9da96d65be1c8071
gitseoyeon <tjdus553926@gmail.com>
2024-09-20 17:07:54 +0900

    solve: prgrms_[PCCP 기출문제] 10번 공원

--- September/3주차/prgrms_[PCCP 기출문제] 10번 공원/Solution.java
+++ September/3주차/prgrms_[PCCP 기출문제] 10번 공원/Solution.java
@@ -1,6 +1,41 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        int answer = -1;
+
+        Arrays.sort(mats);
+        for (int i = mats.length - 1; i >= 0; i--) {
+            int size = mats[i];
+            
+            for (int r = 0; r < park.length; r++) {
+                for (int c = 0; c < park[0].length; c++) {
+                    if (park[r][c].equals("-1")) {
+                        if (checkMat(park, r, c, size)) {
+                            return size;
+                        }
+                    }
+                }
+            }
+        }
+
         return answer;
+    }
+
+    public static boolean checkMat(String[][] park, int r, int c, int size) {
+        if (r + size > park.length || c + size > park[0].length) {
+            return false;
+        }
+
+        // 돗자리가 모두 빈 공간에 놓일 수 있는지 확인
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                if (!park[i][j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
