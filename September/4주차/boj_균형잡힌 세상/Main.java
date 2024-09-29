a22c113c154b37f0309a63eab23fbbceca14459b
ChaeAg <dydkglel123@gmail.com>
2024-09-29 16:27:20 +0900

    solve: boj_균형잡힌 세상

--- September/4주차/boj_균형잡힌 세상/Main.java
+++ September/4주차/boj_균형잡힌 세상/Main.java
@@ -1,3 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        while(!str.equals(".")) {
+            Stack<String> stack = new Stack<>();
+            boolean isGood = true;
+
+            for(String s : str.split("")) {
+                if(s.equals(".")) break;
+
+                if(s.equals("(") || s.equals("[")) {
+                    stack.add(s);
+                }
+                else if(s.equals(")")) {
+                    if(stack.isEmpty() || !stack.pop().equals("(")) { // 매칭
+                        isGood = false;
+                        break;
+                    }
+                }
+                else if(s.equals("]")) {
+                    if(stack.isEmpty() || !stack.pop().equals("[")) { // 매칭
+                        isGood = false;
+                        break;
+                    }
+                }
+            }
+
+            sb.append(stack.isEmpty() && isGood ? "yes" : "no").append("\n");
+
+            str = br.readLine();
+        }
+
+        System.out.print(sb);
+    }
 }
