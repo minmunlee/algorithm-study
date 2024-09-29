b7339e98b2c44b94050d7663d75fe64c476ae35b
gitseoyeon <tjdus553926@gmail.com>
2024-09-29 16:57:19 +0900

    solve: prgrms_큰 수 만들기

--- September/4주차/prgrms_큰 수 만들기/Solution.java
+++ September/4주차/prgrms_큰 수 만들기/Solution.java
@@ -1,6 +1,28 @@
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        int length = number.length() - k; // 구할 숫자의 길이
+        int start = 0;
+
+        for (int i = 0; i < length; i++) {
+            int end = number.length() - (length - i) + 1; // 탐색할 숫자의 범위
+            int idx = getMaxIndex(number, start, end);
+            answer.append(number.charAt(idx));
+            start = idx + 1; // 구한 큰 수 다음 인덱스 부터 다음 큰 수 구함
+        }
+
+        return answer.toString();
+    }
+
+    private int getMaxIndex(String number, int start, int end) {
+        int maxIdx = start;
+
+        for (int i = start; i < end; i++) {
+            if (number.charAt(i) > number.charAt(maxIdx)) {
+                maxIdx = i;
+            }
+        }
+
+        return maxIdx;
     }
 }
