940517e4ab62348730dc6f909ae8210e8b65ab0f
gitseoyeon <tjdus553926@gmail.com>
2024-09-29 16:56:43 +0900

    solve: boj_균형 잡힌 세상

--- September/4주차/boj_균형잡힌 세상/Main.java
+++ September/4주차/boj_균형잡힌 세상/Main.java
@@ -1,3 +1,42 @@
-public class Main {
-    
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = br.readLine();
+            if (str.equals(".")) {
+                break;
+            }
+
+            Stack<Character> stack = new Stack<>();
+            boolean isBalanced = true;
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c == '[' || c == '(') {
+                    stack.push(c);
+                }
+                else if (c == ']' || c == ')') {
+                    if (stack.isEmpty()) {
+                        isBalanced = false;
+                        break;
+                    }
+                    char top = stack.pop();
+                    if ((c == ']' && top != '[') || (c == ')' && top != '(')) {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isBalanced && stack.isEmpty()) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
 }
