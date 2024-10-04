ee1ba2a0464c2bbb7030f4aba89badb5472b809d
gitseoyeon <tjdus553926@gmail.com>
2024-10-04 15:50:18 +0900

    solve: prgrms_연속된 부분 수열의 합

--- October/1주차/prgrms_연속된 부분 수열의 합/Solution.java
+++ October/1주차/prgrms_연속된 부분 수열의 합/Solution.java
@@ -1,6 +1,29 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {};
+        int start = 0;
+        int end = 0;
+        int minLength = 1000001;
+        int[] answer = new int[2];
+        int sum = 0;
+
+        while (end < sequence.length) {
+            sum += sequence[end];
+
+            while (sum >= k) {
+                if (sum == k) {
+                    int length = end - start + 1;
+                    if (length < minLength) {
+                        minLength = length;
+                        answer[0] = start;
+                        answer[1] = end;
+                    }
+                }
+                sum -= sequence[start]; // start 값 빼기
+                start++;
+            }
+            end++;
+        }
+
         return answer;
     }
 }
