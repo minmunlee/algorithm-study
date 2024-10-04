1c1678defae298a693509c4b9e2245a00f3df244
gitseoyeon <tjdus553926@gmail.com>
2024-10-04 15:50:08 +0900

    solve: boj_그룹 단어 체커

--- October/1주차/boj_그룹 단어 체커/Main.java
+++ October/1주차/boj_그룹 단어 체커/Main.java
@@ -1,3 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++){
+            String[] s = br.readLine().split("");
+            List<String> str = new ArrayList<>();
+            boolean check = true;
+
+            if(s.length == 1){
+                cnt++;
+                continue;
+            }
+
+            String now = s[0];
+            str.add(s[0]);
+
+            for(int j = 1; j < s.length; j++){
+                if(!now.equals(s[j])){
+                    if(str.contains(s[j])){
+                        check = false;
+                        break;
+                    }
+                    str.add(s[j]);
+                    now = s[j];
+                }
+            }
+            if(check) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
 }
