32712c45696c058197065ad7b1227af716802db9
ChaeAg <dydkglel123@gmail.com>
2024-10-11 18:38:44 +0900

    solve: prgrms_마법의 엘리베이터

--- October/2주차/prgrms_마법의 엘리베이터/Solution.java
+++ October/2주차/prgrms_마법의 엘리베이터/Solution.java
@@ -1,6 +1,26 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
+        
+        while(storey > 0) {
+            int one = storey % 10;
+            storey /= 10;
+            
+            if(one > 5) { // 올림
+                answer += (10 - one);
+                storey++;
+            }
+            else if(one < 5) { // 내림
+                answer += one;
+            }
+            else {
+                if(storey % 10 >= 5) {
+                    storey++;
+                }
+                answer += one;
+            }
+        }
+        
         return answer;
     }
 }
