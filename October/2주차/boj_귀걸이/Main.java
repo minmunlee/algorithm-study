0cd04eb226338d8071f6931fab2da3726661180e
ChaeAg <dydkglel123@gmail.com>
2024-10-11 18:38:28 +0900

    solve: boj_귀걸이

--- October/2주차/boj_귀걸이/Main.java
+++ October/2주차/boj_귀걸이/Main.java
@@ -1,3 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int scenarios = 1;
+        int n = Integer.parseInt(br.readLine());
+
+        while(n != 0) {
+            String[] students = new String[n];
+            List<String> memory = new ArrayList<>();
+
+            for(int i=0; i<n; i++) {
+                students[i] = br.readLine();
+            }
+
+            for(int i=0; i<2*n-1; i++) {
+                String idx = br.readLine().split(" ")[0];
+                if(memory.contains(idx)) memory.remove(idx);
+                else memory.add(idx);
+            }
+
+            sb.append(scenarios).append(" ").append(students[Integer.parseInt(memory.get(0))-1]).append("\n");
+
+            n = Integer.parseInt(br.readLine());
+            scenarios++;
+        }
+
+        System.out.print(sb);
+    }
 }
