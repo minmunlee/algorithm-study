64a5210a11af3d132932503d26c8917805ed1db5
gitseoyeon <tjdus553926@gmail.com>
2024-10-11 16:10:29 +0900

    solve: prgrms_마법의 엘리베이터

--- October/2주차/prgrms_마법의 엘리베이터/Solution.java
+++ October/2주차/prgrms_마법의 엘리베이터/Solution.java
@@ -1,6 +1,27 @@
 class Solution {
+
     public int solution(int storey) {
         int answer = 0;
+
+        while (storey > 0) {
+            int num = storey % 10;
+            storey /= 10;
+
+            if (num < 5) {
+                answer += num;
+            } else if (num > 5) {
+                answer += 10 - num;
+                storey++;
+            } else {
+                if (storey % 10 >= 5) {
+                    answer += num;
+                    storey++;
+                } else {
+                    answer += num;
+                }
+            }
+        }
+
         return answer;
     }
 }
