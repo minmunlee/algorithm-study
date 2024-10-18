7d38bb849b54bf0dfb56d22b8bfa8b644d3fa139
gitseoyeon <tjdus553926@gmail.com>
2024-10-18 12:13:39 +0900

    solve: boj_폴리오미노

--- October/3주차/boj_폴리오미노/Main.java
+++ October/3주차/boj_폴리오미노/Main.java
@@ -1,3 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] board = br.readLine().split("");
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        boolean check = true;
+
+        for (String s : board) {
+            if (s.equals("X")) {
+                cnt++;
+            } else {
+                if (cnt % 2 != 0) {
+                    check = false;
+                    break;
+                } else {
+                    int A = cnt / 4;
+                    int B = cnt % 4;
+
+                    for (int i = 0; i < A; i++) {
+                        sb.append("AAAA");
+                    }
+                    for (int i = 0; i < B / 2; i++) {
+                        sb.append("BB");
+                    }
+                    cnt = 0;
+                }
+                sb.append(".");
+            }
+        }
+
+        if (cnt > 0) {
+            if (cnt % 2 != 0) {
+                check = false;
+            } else {
+                int A = cnt / 4;
+                int B = cnt % 4;
+
+                for (int i = 0; i < A; i++) {
+                    sb.append("AAAA");
+                }
+                for (int i = 0; i < B / 2; i++) {
+                    sb.append("BB");
+                }
+            }
+        }
+
+        if (check) {
+            System.out.println(sb);
+        } else {
+            System.out.println(-1);
+        }
+    }
 }
