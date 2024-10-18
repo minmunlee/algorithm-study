6fd998ae96a50a00e282b1521c949f18a033ac44
gitseoyeon <tjdus553926@gmail.com>
2024-10-18 12:13:23 +0900

    solve: boj_수 고르기

--- October/3주차/boj_수 고르기/Main.java
+++ October/3주차/boj_수 고르기/Main.java
@@ -1,3 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(str.nextToken());
+        int M = Integer.parseInt(str.nextToken());
+        int[] arr = new int[N];
+        int num = 2000000000;
+
+        for(int i= 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N - 1; i++){
+            int tmp = arr[i] + M;
+            for(int j = i + 1; j < N; j++){
+                if(arr[j] >= tmp){
+                    num = Math.min(num, arr[j] - arr[i]);
+                }
+            }
+        }
+
+        System.out.println(num);
+    }
 }
