97118e28f2c5a36f271bb87c21d66dee8aee3a3a
ChaeAg <dydkglel123@gmail.com>
2024-11-21 15:08:35 +0900

    solve: prgrms_124 나라의 숫자

--- November/2주차/prgrms_123 나라의 숫자/Solution.java
+++ November/2주차/prgrms_123 나라의 숫자/Solution.java
@@ -1,6 +1,21 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            int a = n % 3;
+
+            if(a == 0) {
+                sb.insert(0, 4);
+                n--;
+            }
+            else {
+                sb.insert(0, a);
+            }
+            
+            n /= 3;
+        }
+        
+        return sb.toString();
     }
 }
