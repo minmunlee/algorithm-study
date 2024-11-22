80ae3cb50530bfd963b3364f549d5b21b3786c47
gitseoyeon <tjdus553926@gmail.com>
2024-11-22 16:43:10 +0900

    solve: boj_괄호의 값

--- November/2주차/boj_괄호의 값/Main.java
+++ November/2주차/boj_괄호의 값/Main.java
@@ -1,3 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] str = s.split("");
+        Stack<String> stack = new Stack<>();
+        int sum = 1;
+        int answer = 0;
+        boolean check = true;
+
+        for(int i = 0; i < str.length; i++){
+            if(str[i].equals("(")){
+                stack.add(str[i]);
+                sum *= 2;
+            }else if(str[i].equals("[")){
+                stack.add(str[i]);
+                sum *= 3;
+            }
+            else if(str[i].equals(")")){
+                if(stack.isEmpty() || !stack.peek().equals("(")){
+                    check = false;
+                    break;
+                }
+                if(str[i - 1].equals("(")){
+                    answer += sum;
+                }
+                sum /= 2;
+                stack.pop();
+            }
+            else if(str[i].equals("]")){
+                if(stack.isEmpty() || !stack.peek().equals("[")){
+                    check = false;
+                    break;
+                }
+                if(str[i - 1].equals("[")){
+                    answer += sum;
+                }
+                sum /= 3;
+                stack.pop();
+            }
+        }
+
+        System.out.println(!stack.isEmpty() || !check ? 0 : answer);
+    }
 }
