f922c178dc66f854ec36ef18064f6447a69d32bd
ChaeAg <dydkglel123@gmail.com>
2024-11-29 21:56:36 +0900

    solve: boj_스네이크버드

--- November/3주차/boj_스네이크버드/Main.java
+++ November/3주차/boj_스네이크버드/Main.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        st.nextToken();
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] fruits = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        for(int fruit : fruits) {
+            if(fruit > L) {
+                break;
+            }
+            L++;
+        }
+
+        System.out.print(L);
+    }
 }
