6770e9c196bd9864ca68d44991192b35e88f3e46
gitseoyeon <tjdus553926@gmail.com>
2024-12-06 20:39:26 +0900

    solve: prgrms_연간 평가 점수에 해당하는 평가 등급 및 성과금 조회하기

---
+++ December/1주차/sql_prgrms_연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/Solution.sql
@@ -0,0 +1,27 @@
+-- 코드를 작성해주세요
+SELECT 
+    E.EMP_NO,
+    E.EMP_NAME,
+    CASE
+        WHEN G.AVG_SCORE >= 96 THEN 'S'
+        WHEN G.AVG_SCORE >= 90 THEN 'A'
+        WHEN G.AVG_SCORE >= 80 THEN 'B'
+        ELSE 'C'
+    END AS GRADE,
+    CASE
+        WHEN G.AVG_SCORE >= 96 THEN E.SAL * 0.2
+        WHEN G.AVG_SCORE >= 90 THEN E.SAL * 0.15
+        WHEN G.AVG_SCORE >= 80 THEN E.SAL * 0.1
+        ELSE 0
+    END AS BONUS
+FROM 
+    HR_EMPLOYEES E
+JOIN 
+    (SELECT EMP_NO, AVG(SCORE) AS AVG_SCORE 
+     FROM HR_GRADE 
+     WHERE YEAR = 2022 
+     GROUP BY EMP_NO) G
+ON 
+    E.EMP_NO = G.EMP_NO
+ORDER BY 
+    E.EMP_NO;
